f60374103790409d9109b0bea9999fd8aa2d6c61
TheDonsky <nikadon.net@gmail.com>
2021-01-08 18:35:37 +0400

    Add test subproject

--- __Source__/main.cpp
+++ __Source__/main.cpp
@@ -1,6 +1,17 @@
-#include "Core/Event.h"
-#include "Core/Object.h"
+//#include "Core/Event.h"
+//#include "Core/Object.h"
+//#include <gtest/gtest.h>
 
-int main() {
+//TEST(ReferenceTest, ReferenceValueTest) {
+//	Jimara::Object a, b;
+//	{
+//		Jimara::Reference<Jimara::Object> refA(&a), refB(&b);
+		//EXPECT_EQ(refA, refB);
+//	}
+//}
+
+int main(int argc, char** argv) {
 	return 0;
+	//testing::InitGoogleTest(&argc, argv);
+	//return RUN_ALL_TESTS();
 }
