cc7113c318653c5f21aade506533c863a6298475
TheDonsky <nikadon.net@gmail.com>
2021-01-10 14:57:09 +0400

    Expand tests a bit

--- __Tests__/OS/LoggerTest.cpp
+++ __Tests__/OS/LoggerTest.cpp
@@ -12,8 +12,13 @@ namespace Jimara {
 					int exitCode;
 				};
 
+			private:
 				std::vector<Info> infos[static_cast<uint8_t>(LogLevel::FATAL) + 1];
 
+			public:
+				inline const std::vector<Info>& operator[](LogLevel level)const {
+					return infos[static_cast<uint8_t>(level)];
+				}
 
 			protected:
 				inline virtual void Log(const LogInfo& info) {
@@ -28,22 +33,21 @@ namespace Jimara {
 			
 			const char* FIRST_MESSAGE = "This shit is a debug message";
 			logger.Debug(FIRST_MESSAGE);
-			uint8_t index = static_cast<uint8_t>(Logger::LogLevel::DEBUG);
 #ifndef NDEBUG
-			EXPECT_EQ(logger.infos[index].size(), 1);
-			EXPECT_EQ(logger.infos[index][0].message, FIRST_MESSAGE);
+			EXPECT_EQ(logger[Logger::LogLevel::DEBUG].size(), 1);
+			EXPECT_EQ(logger[Logger::LogLevel::DEBUG][0].message, FIRST_MESSAGE);
 #else
-			EXPECT_EQ(logger.infos[index].size(), 0);
+			EXPECT_EQ(logger[Logger::LogLevel::DEBUG].size(), 0);
 #endif
 			
 			const std::string SECOND_MESSAGE = "This is another message";
 			logger.Debug(SECOND_MESSAGE);
 #ifndef NDEBUG
-			EXPECT_EQ(logger.infos[index].size(), 2);
-			EXPECT_EQ(logger.infos[index][0].message, FIRST_MESSAGE);
-			EXPECT_EQ(logger.infos[index][1].message, SECOND_MESSAGE);
+			EXPECT_EQ(logger[Logger::LogLevel::DEBUG].size(), 2);
+			EXPECT_EQ(logger[Logger::LogLevel::DEBUG][0].message, FIRST_MESSAGE);
+			EXPECT_EQ(logger[Logger::LogLevel::DEBUG][1].message, SECOND_MESSAGE);
 #else
-			EXPECT_EQ(logger.infos[index].size(), 0);
+			EXPECT_EQ(logger[Logger::LogLevel::DEBUG].size(), 0);
 #endif
 		}
 
