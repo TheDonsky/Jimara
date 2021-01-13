81dba5b435823374720785cc28ed74c12a68a8b4
TheDonsky <nikadon.net@gmail.com>
2021-01-13 18:02:24 +0400

    Who's gonna document this?

--- __Source__/Jimara/Graphics/Vulkan/VulkanPhysicalDevice.h
+++ __Source__/Jimara/Graphics/Vulkan/VulkanPhysicalDevice.h
@@ -8,33 +8,64 @@
 namespace Jimara {
 	namespace Graphics {
 		namespace Vulkan {
+			/// <summary>
+			/// Vulkan physical device descriptor
+			/// </summary>
 			class VulkanPhysicalDevice : public PhysicalDevice {
 			public:
+				/// <summary>
+				/// Constructor
+				/// </summary>
+				/// <param name="instance"> Owning instance </param>
+				/// <param name="device"> Physical device </param>
+				/// <param name="index"> Physical device index </param>
 				VulkanPhysicalDevice(VulkanInstance* instance, VkPhysicalDevice device, size_t index);
 
+				/// <summary> Virtual destructor </summary>
 				virtual ~VulkanPhysicalDevice();
 
+				/// <summary> Type cast to API object </summary>
 				operator VkPhysicalDevice()const;
 
+				/// <summary> Full Vulkan device features structure </summary>
 				const VkPhysicalDeviceFeatures& DeviceFeatures()const;
 
+				/// <summary> Full Vulkan device properties structure </summary>
 				const VkPhysicalDeviceProperties& DeviceProperties()const;
 
+				/// <summary> Full Vulkan device memory properties struct </summary>
 				const VkPhysicalDeviceMemoryProperties& MemoryProperties()const;
 
+				/// <summary> Number of available queue families </summary>
 				size_t QueueFamilyCount()const;
 
+				/// <summary>
+				/// Queue family properties by index
+				/// </summary>
+				/// <param name="index"> Queue family index </param>
+				/// <returns> Family properties </returns>
 				const VkQueueFamilyProperties& QueueFamilyProperties(size_t index)const;
 
+				/// <summary> Graphics queue id (does not have value if there's no graphics capability) </summary>
 				std::optional<uint32_t> GraphicsQueueId()const;
 
+				/// <summary> Compute queue id (synchronous(same as GraphicsQueueId), if possible; does not have value if there's no compute capability) </summary>
 				std::optional<uint32_t> ComputeQueueId()const;
 
+				/// <summary> Number of available asynchronous compute queues </summary>
 				size_t AsynchComputeQueueCount()const;
 
+				/// <summary>
+				/// Asynchronous compute queue index(global) from asynchronous index(ei 0 - AsynchComputeQueueCount)
+				/// </summary>
+				/// <param name="asynchQueueId"> Asynchronous queue index (0 - AsynchComputeQueueCount) </param>
+				/// <returns> Asynchronous compute queue index </returns>
 				uint32_t AsynchComputeQueueId(size_t asynchQueueId)const;
 
+				/// <summary> Device extension version (will not have value if extension is not supported by the device) </summary>
 				std::optional<uint32_t> DeviceExtensionVerison(const std::string& extensionName)const;
+
+
 
 				/// <summary> Physical device type </summary>
 				virtual DeviceType Type()const override;
@@ -51,24 +82,49 @@ namespace Jimara {
 				/// <summary> Instantiates a logical device </summary>
 				virtual Reference<LogicalDevice> CreateLogicalDevice() override;
 
+
+
 			private:
+				// Underlying physical device
 				const VkPhysicalDevice m_device;
+
+				// Underlying physical device index
 				const size_t m_index;
 
+				// Queue indices
 				struct {
+					// Main graphics queue
 					std::optional<uint32_t> graphics;
+
+					// Main compute queue (synchronous, if possible)
 					std::optional<uint32_t> compute;
+
+					// Asynchronous compute queue indices
 					std::vector<uint32_t> asynchronous_compute;
 				} m_queueIds;
 
+				// Device type
 				PhysicalDevice::DeviceType m_type;
+
+				// Device features
 				uint64_t m_features;
+				
+				// Device VRAM capacity
 				size_t m_vramCapacity;
 
+				// Vulkan-reported device features
 				VkPhysicalDeviceFeatures m_deviceFeatures;
+
+				// Vulkan-reported device properties
 				VkPhysicalDeviceProperties m_deviceProperties;
+
+				// Vulkan-reported device memory properties
 				VkPhysicalDeviceMemoryProperties m_memoryProps;
+
+				// Vulkan-reported device queue family properties
 				std::vector<VkQueueFamilyProperties> m_queueFamilies;
+
+				// Vulkan-reported device extensions
 				std::unordered_map<std::string, uint32_t> m_availableExtensions;
 			};
 		}
