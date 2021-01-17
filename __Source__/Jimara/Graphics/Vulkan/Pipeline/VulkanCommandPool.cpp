db6d46aeb4d8ca84ecde5d5f516ea6a09def0c86
TheDonsky <nikadon.net@gmail.com>
2021-01-17 17:52:54 +0400

    Import implementation of VulkanCommandPool wrapper

---
+++ __Source__/Jimara/Graphics/Vulkan/Pipeline/VulkanCommandPool.cpp
@@ -0,0 +1,86 @@
+#include "VulkanCommandPool.h"
+
+
+namespace Jimara {
+	namespace Graphics {
+		namespace Vulkan {
+			VulkanCommandPool::VulkanCommandPool(VulkanDevice* device, uint32_t queueFamilyId, VkCommandPoolCreateFlags createFlags)
+				: m_device(device), m_queueFamilyId(queueFamilyId), m_createFlags(createFlags), m_commandPool(VK_NULL_HANDLE), m_queue(VK_NULL_HANDLE) {
+				VkCommandPoolCreateInfo poolInfo = {};
+				{
+					poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+					poolInfo.queueFamilyIndex = m_queueFamilyId;
+					poolInfo.flags = m_createFlags;
+				}
+				if (vkCreateCommandPool(*device, &poolInfo, nullptr, &m_commandPool) != VK_SUCCESS)
+					m_device->Log()->Fatal("VulkanCommandPool - Failed to create command pool!");
+
+				vkGetDeviceQueue(*m_device, m_queueFamilyId, 0, &m_queue);
+			}
+
+			VulkanCommandPool::VulkanCommandPool(VulkanDevice* device, VkCommandPoolCreateFlags createFlags)
+				: VulkanCommandPool(device, device->PhysicalDeviceInfo()->GraphicsQueueId().value(), createFlags) { }
+
+			VulkanCommandPool::VulkanCommandPool(VulkanDevice* device)
+				: VulkanCommandPool(device, device->PhysicalDeviceInfo()->GraphicsQueueId().value(), VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT) { }
+
+			VulkanCommandPool::~VulkanCommandPool() {
+				if (m_commandPool != VK_NULL_HANDLE) {
+					vkDestroyCommandPool(*m_device, m_commandPool, nullptr);
+					m_commandPool = VK_NULL_HANDLE;
+				}
+			}
+
+			VulkanDevice* VulkanCommandPool::Device()const { return m_device; }
+
+			uint32_t VulkanCommandPool::QueueFamilyId()const { return m_queueFamilyId; }
+
+			VkQueue VulkanCommandPool::Queue()const { return m_queue; }
+
+			VkCommandPoolCreateFlags VulkanCommandPool::CreateFlags()const { return m_createFlags; }
+
+			VulkanCommandPool::operator VkCommandPool()const { return m_commandPool; }
+
+			namespace {
+#pragma warning(disable: 26812)
+				inline static void AllocateCommandBuffers(const VulkanCommandPool* commandPool, VkCommandBufferLevel level, size_t count, VkCommandBuffer* buffers) {
+					VkCommandBufferAllocateInfo allocInfo = {};
+					{
+						allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+						allocInfo.commandPool = *commandPool;
+						allocInfo.level = level;
+						allocInfo.commandBufferCount = static_cast<uint32_t>(count);
+					}
+					if (vkAllocateCommandBuffers(*commandPool->Device(), &allocInfo, buffers) != VK_SUCCESS)
+						commandPool->Device()->Log()->Fatal("VulkanCommandPool - Failed to allocate command buffers!");
+				}
+#pragma warning(default: 26812)
+			}
+
+			std::vector<VkCommandBuffer> VulkanCommandPool::CreateCommandBuffers(size_t count, VkCommandBufferLevel level)const {
+				std::vector<VkCommandBuffer> commandBuffers(count);
+				AllocateCommandBuffers(this, level, count, commandBuffers.data());
+				return commandBuffers;
+			}
+
+			VkCommandBuffer VulkanCommandPool::CreateCommandBuffer(VkCommandBufferLevel level)const {
+				VkCommandBuffer commandBuffer = VK_NULL_HANDLE;
+				AllocateCommandBuffers(this, level, 1, &commandBuffer);
+				return commandBuffer;
+			}
+
+			void VulkanCommandPool::DestroyCommandBuffers(VkCommandBuffer* buffers, size_t count)const {
+				if (buffers != nullptr)
+					vkFreeCommandBuffers(*m_device, m_commandPool, static_cast<uint32_t>(count), buffers);
+			}
+
+			void VulkanCommandPool::DestroyCommandBuffer(std::vector<VkCommandBuffer>& buffers)const {
+				DestroyCommandBuffers(buffers.data(), buffers.size());
+			}
+
+			void VulkanCommandPool::DestroyCommandBuffer(VkCommandBuffer buffer)const {
+				DestroyCommandBuffers(&buffer, 1);
+			}
+		}
+	}
+}
---
+++ __Source__/Jimara/Graphics/Vulkan/Pipeline/VulkanCommandPool.h
@@ -0,0 +1,142 @@
+#pragma once
+namespace Jimara {
+	namespace Graphics {
+		namespace Vulkan {
+			class VulkanCommandPool;
+		}
+	}
+}
+#include "../VulkanDevice.h"
+
+
+namespace Jimara {
+	namespace Graphics {
+		namespace Vulkan {
+			/// <summary>
+			/// Wrapper on top of VkCommandPool
+			/// </summary>
+			class VulkanCommandPool : public virtual Object {
+			public:
+				/// <summary>
+				/// Constructor
+				/// </summary>
+				/// <param name="device"> Logical device </param>
+				/// <param name="queueFamilyId"> Queue family id </param>
+				/// <param name="createFlags"> Command pool create flags </param>
+				VulkanCommandPool(VulkanDevice* device, uint32_t queueFamilyId, VkCommandPoolCreateFlags createFlags);
+
+				/// <summary>
+				/// Constructor
+				/// Note: queueFamilyId defaults to main graphics queue
+				/// </summary>
+				/// <param name="device"> Logical device </param>
+				/// <param name="createFlags"> Command pool create flags </param>
+				VulkanCommandPool(VulkanDevice* device, VkCommandPoolCreateFlags createFlags);
+
+				/// <summary>
+				/// Constructor
+				/// Note: queueFamilyId defaults to main graphics queue; createFlags defaults to VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT.
+				/// </summary>
+				/// <param name="device"> Logical device </param>
+				VulkanCommandPool(VulkanDevice* device);
+
+				/// <summary> Virtual destructor </summary>
+				virtual ~VulkanCommandPool();
+
+				/// <summary> "Owner" device </summary>
+				VulkanDevice* Device()const;
+
+				/// <summary> Target queue family id </summary>
+				uint32_t QueueFamilyId()const;
+
+				/// <summary> Target queue </summary>
+				VkQueue Queue()const;
+
+				/// <summary> Command pool create flags used during creation </summary>
+				VkCommandPoolCreateFlags CreateFlags()const;
+
+				/// <summary> Type cast to underlying API object </summary>
+				operator VkCommandPool()const;
+
+				/// <summary>
+				/// Creates command buffers
+				/// </summary>
+				/// <param name="count"> Number of buffers to create </param>
+				/// <param name="level"> Created buffer level </param>
+				/// <returns> Vector of newly created buffers </returns>
+				std::vector<VkCommandBuffer> CreateCommandBuffers(size_t count, VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_PRIMARY)const;
+
+				/// <summary>
+				/// Creates a single command buffer
+				/// </summary>
+				/// <param name="level"> Created buffer level </param>
+				/// <returns> New command buffer </returns>
+				VkCommandBuffer CreateCommandBuffer(VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_PRIMARY)const;
+
+				/// <summary>
+				/// Destroys command buffers
+				/// </summary>
+				/// <param name="buffers"> Buffers to destroy (should be created "togather" with single CreateCommandBuffers() call) </param>
+				/// <param name="count"> Number of buffers within buffer array </param>
+				void DestroyCommandBuffers(VkCommandBuffer* buffers, size_t count)const;
+
+				/// <summary>
+				/// Destroys a single command buffer
+				/// </summary>
+				/// <param name="buffers"> Command buffers to destroy (should be created "togather" with single CreateCommandBuffers() call) </param>
+				void DestroyCommandBuffer(std::vector<VkCommandBuffer>& buffers)const;
+
+				/// <summary>
+				/// Destroys a single command buffer
+				/// </summary>
+				/// <param name="buffer"> Command buffer to destroy </param>
+				void DestroyCommandBuffer(VkCommandBuffer buffer)const;
+
+				/// <summary>
+				/// Creates and runs a single-time command buffer (introduces sync point, so use this one with an amount of caution)
+				/// </summary>
+				/// <typeparam name="FunctionType"> Some function type that gets a VkCommandBuffer as parameter, records some commands to it and returns </typeparam>
+				/// <param name="recordCallback"> Some function that gets a VkCommandBuffer as parameter, records some commands to it and returns </param>
+				template<typename FunctionType>
+				inline void SubmitSingleTimeCommandBuffer(FunctionType recordCallback) {
+					VkCommandBuffer commandBuffer = CreateCommandBuffer();
+					{
+						VkCommandBufferBeginInfo beginInfo = {};
+						beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+						beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+						vkBeginCommandBuffer(commandBuffer, &beginInfo);
+					}
+					recordCallback(commandBuffer);
+					vkEndCommandBuffer(commandBuffer);
+					{
+						VkSubmitInfo submitInfo = {};
+						submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+						submitInfo.commandBufferCount = 1;
+						submitInfo.pCommandBuffers = &commandBuffer;
+						vkQueueSubmit(Queue(), 1, &submitInfo, VK_NULL_HANDLE);
+					}
+					vkQueueWaitIdle(Queue());
+					DestroyCommandBuffer(commandBuffer);
+				}
+
+
+
+			private:
+				// "Owener" device
+				Reference<VulkanDevice> m_device;
+				
+				// Target queue family id
+				uint32_t m_queueFamilyId;
+
+				// Pool create flags
+				VkCommandPoolCreateFlags m_createFlags;
+
+				// Underlying command pool
+				VkCommandPool m_commandPool;
+
+				// Target queue
+				VkQueue m_queue;
+			};
+		}
+	}
+}
