718ad3da7086588425056a9dda9866f7352ced3a
TheDonsky <nikadon.net@gmail.com>
2021-01-24 17:19:58 +0400

    Fix memory location flags

--- __Source__/Jimara/Graphics/Vulkan/Memory/VulkanDeviceResidentBuffer.cpp
+++ __Source__/Jimara/Graphics/Vulkan/Memory/VulkanDeviceResidentBuffer.cpp
@@ -53,7 +53,7 @@ namespace Jimara {
 				std::unique_lock<std::mutex> lock(m_bufferLock);
 				m_dataBuffer = Object::Instantiate<VulkanBuffer>(m_device, m_objectSize, m_objectCount, true
 					, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT
-					, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+					, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 
 				commandRecorder->RecordBufferDependency(m_dataBuffer);
 
