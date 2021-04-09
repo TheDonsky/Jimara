2c946daf5e7f120252acfd17cc8e4b85d40c0b80
TheDonsky <nikadon.net@gmail.com>
2021-04-09 14:45:28 +0400

    Update shader merger

--- __Scripts__/jimara_file_tools.py
+++ __Scripts__/jimara_file_tools.py
@@ -1,28 +1,48 @@
 import os, sys
 
-def find_by_suffix(folder_path, file_extensions, recursive = True):
+def get_file_names(file_paths):
+	return [os.path.basename(file) for file in file_paths]
+
+def strip_file_extension(file_name):
+	return os.path.splitext(file_name)[0]
+
+def strip_file_extensions(file_names):
+	return [strip_file_extension(file) for file in file_names]
+
+def get_file_extension(file_name):
+	return os.path.splitext(file_name)[1]
+
+def get_file_extensions(file_names):
+	return [get_file_extension(file) for file in file_names]
+	
+def find_with_filter(folder_path, filter, recursive = True):
 	files = []
 	for file in os.listdir(folder_path):
 		file_path = os.path.join(folder_path, file)
 		if os.path.isdir(file_path):
 			if recursive:
-				files += find_by_suffix(file_path, file_extensions, True)
+				files += find_with_filter(file_path, filter, True)
 		else:
-			for extension in file_extensions:
-				if file.endswith(extension):
-					files.append(file_path)
-					break
+			if filter(file_path):
+				files.append(file_path)
 	return files
 
-def get_file_names(file_paths):
-	return [os.path.basename(file) for file in file_paths]
+def find_by_suffix(folder_path, file_suffixes, recursive = True):
+	def filter(file_path):
+		for extension in file_suffixes:
+			if file_path.endswith(extension):
+				return True
+		return False
+	return find_with_filter(folder_path, filter, recursive)
 
-def strip_file_extensions(file_names):
-	return [os.path.splitext(file)[0] for file in file_names]
-
-def get_file_extensions(file_names):
-	return [os.path.splitext(file)[1] for file in file_names]
+def find_by_extension(folder_path, file_extensions, recursive = True):
+	extensions = []
+	for extension in file_extensions:
+		extensions.append(extension if (len(extension) > 0 and extension[0] == '.') else ('.' + extension))
+	def filter(file_path):
+		return get_file_extension(file_path) in extensions
+	return find_with_filter(folder_path, filter, recursive)
 
 if __name__ == "__main__":
-	for file in find_by_suffix("." if len(sys.argv) <= 1 else sys.argv[1], [""] if len(sys.argv) <= 2 else sys.argv[2:]):
+	for file in find_by_extension("." if len(sys.argv) <= 1 else sys.argv[1], [""] if len(sys.argv) <= 2 else sys.argv[2:]):
 		print(file)
--- __Scripts__/jimara_merge_light_shaders.py
+++ __Scripts__/jimara_merge_light_shaders.py
@@ -5,10 +5,6 @@ def get_type_names(shader_paths):
 	return jimara_file_tools.strip_file_extensions(jimara_file_tools.get_file_names(shader_paths))
 
 
-def light_type_id_name(type_name):
-	return type_name + "_LightTypeId"
-
-
 def merge_light_shaders(shader_paths):
 	code = (
 		"// COMMON PARAMETERS:\n"
@@ -21,9 +17,14 @@ def merge_light_shaders(shader_paths):
 		"#ifndef LIGHT_BUFFER_BINDING_ID\n" + 
 		"#define LIGHT_BUFFER_BINDING_ID 0\n" + 
 		"#endif\n\n" + 
+		"// ILLUMINATED POINT DEFINITION:\n" + 
+		"#ifndef HIT_POINT_INFO_DEFINED\n" +
+		"struct HitPoint { vec3 position; vec3 normal; };\n" +
+		"#define HIT_POINT_INFO_DEFINED\n" +
+		"#endif\n\n" +
 		"// PHOTON DEFINITION:\n"
 		"#ifndef PHOTON_DEFINED\n" +
-		"struct Photon { vec3 origin; vec3 color };\n" +
+		"struct Photon { vec3 origin; vec3 color; };\n" +
 		"#define PHOTON_DEFINED\n" +
 		"#endif\n\n")
 
@@ -31,19 +32,20 @@ def merge_light_shaders(shader_paths):
 
 	buffer_attachment_name = lambda type_name: type_name + "_lightBuffer"
 	light_data_type = lambda type_name: type_name + "_Data"
+	light_type_id_name = lambda type_name: type_name + "_LightTypeId"
 	light_function_name = lambda type_name: type_name + "_GetSamples"
 
 	code += "\n// TYPE ID-s:\n"
 	for i, type_name in enumerate(type_names):
 		code += "#define " + light_type_id_name(type_name) + " " + str(i) + "\n"
 
-	code += "___________________________________________________________________________________________\n\n"
+	code += "// ___________________________________________________________________________________________\n\n"
 	for i, file in enumerate(shader_paths):
 		with open(file, "r") as fl:
 			code += "// LIGHT TYPE: " + type_names[i] + " <SOURCE: \"" + file + "\">\n" + fl.read() + "\n"
 
 	code += (
-		"___________________________________________________________________________________________\n\n" + 
+		"// ___________________________________________________________________________________________\n\n" + 
 		"// ATTACHMENTS:\n")
 	for type_name in type_names:
 		code += (
@@ -52,12 +54,12 @@ def merge_light_shaders(shader_paths):
 
 	code += (
 		"\n// Computes sample photons coming to the hit point from light defined by light buffer index and light type index\n" +
-		"uint Jimara_GetLightSamples(uint lightBufferId, uint lightTypeId, in vec3 hitPoint, in vec3 hitNormal, out Photon samples[MAX_SAMPLES]) {\n")
+		"uint Jimara_GetLightSamples(uint lightBufferId, uint lightTypeId, in HitPoint hitPoint, out Photon samples[MAX_SAMPLES]) {\n")
 	def search_type(types, tab):
 		if len(types) < 1:
-			return
+			return tab + "return 0;\n"
 		elif len(types) == 1:
-			return tab + "return " + light_function_name(types[0]) + "(hitPoint, hitNormal, " + buffer_attachment_name(types[0]) + "[lightBufferId], samples);\n"
+			return tab + "return " + light_function_name(types[0]) + "(hitPoint, " + buffer_attachment_name(types[0]) + "[lightBufferId], samples);\n"
 		else:
 			return (
 				tab + "if (lightTypeId < " + light_type_id_name(types[int(len(types)/2)]) + ") {\n" +
@@ -66,31 +68,51 @@ def merge_light_shaders(shader_paths):
 				search_type(types[int(len(types)/2):], tab + "\t") +
 				tab + "}\n")
 	code += search_type(type_names, "\t")
-	code += (
-		"\treturn 0;\n" + 
-		"}\n")
+	code += "}\n#define JIMARA_GetLightSamples_FN Jimara_GetLightSamples\n"
 
 	return code
 
 
-def generate_engine_type_indices(shader_paths, namespaces = ["Jimara", "LightRegistry"], inset = ""):
-	if len(namespaces) > 0:
-		return inset + "namespace " + namespaces[0] + " {\n" + generate_engine_type_indices(shader_paths, namespaces[1:], inset + "\t") + inset + "}\n"
+def generate_engine_type_indices(shader_paths, set_name, namespaces = ["Jimara", "LightRegistry"], inset = "", generate_includes = True):
+	if generate_includes:
+		code = (
+			inset + "#include <unordered_map>\n" + 
+			inset + "#include <string>\n\n")
+	else:
+		code = ""
 
-	code = inset + "typedef unsigned int LightTypeId;\n"
+	if len(namespaces) > 0:
+		return code + inset + "namespace " + namespaces[0] + " {\n" + generate_engine_type_indices(shader_paths, set_name, namespaces[1:], inset + "\t", False) + inset + "}\n"
+
+	escape = lambda type_name: repr(type_name)[1:(len(repr(type_name)) - 1)]
+
+	code += (
+		inset + "typedef unsigned int LightTypeId;\n" +
+		inset + "static const std::unordered_map<std::string, LightTypeId> " + set_name + " = {")
 	for i, type_name in enumerate(get_type_names(shader_paths)):
-		code += inset + "const LightTypeId " + light_type_id_name(type_name) + " = " + str(i) + ";\n"
+		code += ("\n" if i <= 0 else ",\n") + inset + "\t{ \"" + escape(type_name) + "\", " + str(i) + " }"
+	code += "\n" + inset + "};\n"
 
 	return code
 
 
 if __name__ == "__main__":
-	source_dir = "." if len(sys.argv) <= 1 else sys.argv[1]
-	glsl_output_file = "Lights.lights" if len(sys.argv) <= 2 else sys.argv[2]
-	cpp_output_file = "LightTypes.cpp" if len(sys.argv) <= 3 else sys.argv[3]
-	light_extensions = [".light"] if len(sys.argv) <= 4 else sys.argv[4:]
-	paths = jimara_file_tools.find_by_suffix(source_dir, light_extensions)
-	with open(glsl_output_file, "w") as file:
-		file.write(merge_light_shaders(paths))
-	with open(cpp_output_file, "w") as file:
-		file.write(generate_engine_type_indices(paths))
+	if len(sys.argv) < 4:
+		print(
+			"Usage: python jimara_merge_light_shaders.py source_directory glsl_output_file cpp_output_file <type_id_map> <extensions...>\n" +
+			"    source_directory - Light shaders will be searched in this directory, as well as it's subfolders;\n" +
+			"    glsl_output_file - Merged light shader code will be stored in this \"output\" header file containing Jimara_GetLightSamples function;\n" +
+			"    cpp_output_file  - glsl_output_file will depend on \"Light Type identifiers\" to decide which light shader to run. Those identifiers will be stored in this header as <type_id_map> unordered_map;\n" + 
+			"    type_id_map      - Optional name for the unordered_map stored in cpp_output_file (defaults to JIMARA_LIGHT_TYPE_IDS);\n" + 
+			"    extensions       - Optional list of light shader extensions to find the shader files in source_directory (defaults to \"jls\"<stands for \"Jimara Light Shader\">).")
+	else:
+		source_directory = sys.argv[1]
+		glsl_output_file = sys.argv[2]
+		cpp_output_file = sys.argv[3]
+		type_id_map = "JIMARA_LIGHT_TYPE_IDS" if len(sys.argv) <= 4 else sys.argv[4]
+		light_extensions = ["jls"] if len(sys.argv) <= 5 else sys.argv[5:]
+		paths = jimara_file_tools.find_by_extension(source_directory, light_extensions)
+		with open(glsl_output_file, "w") as file:
+			file.write(merge_light_shaders(paths))
+		with open(cpp_output_file, "w") as file:
+			file.write(generate_engine_type_indices(paths, type_id_map))
