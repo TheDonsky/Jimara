19ba1adf4024b31860969069db5a44f422c121d0
TheDonsky <nikadon.net@gmail.com>
2021-05-26 15:32:54 +0400

    Fix PhysX compile issue

--- __Source__/Jimara-Tests/Components/PhysicsPlayground.cpp
+++ __Source__/Jimara-Tests/Components/PhysicsPlayground.cpp
@@ -12,6 +12,7 @@
 #pragma warning(disable: 26495)
 #pragma warning(disable: 26451)
 #pragma warning(disable: 33010)
+#define PX_PHYSX_STATIC_LIB
 #include "PxPhysicsAPI.h"
 #include "PxPhysicsVersion.h"
 
@@ -63,28 +64,103 @@ namespace Jimara {
 					}
 					m_logger->Log(level, "[PxErrorCodes: ", stream.str(), "] file: <", file, ">; line: ", line, "; message: <", message, ">");
 				}
+
+				inline OS::Logger* Log()const { return m_logger; }
 			} m_errorCallback;
 			physx::PxDefaultAllocator m_allocator;
 			physx::PxFoundation* m_foundation = nullptr;
+			physx::PxPvd* m_pvd = nullptr;
+			physx::PxPhysics* m_physx = nullptr;
 
 		public:
 			inline PhysicXFoundation(OS::Logger* logger) : m_errorCallback(logger) {
 				m_foundation = PxCreateFoundation(PX_PHYSICS_VERSION, m_allocator, m_errorCallback);
-				if (m_foundation == nullptr) logger->Fatal("PhysicXFoundation - Failed to create foundation!");
+				if (m_foundation == nullptr) {
+					logger->Fatal("PhysicXFoundation - Failed to create foundation!");
+					return;
+				}
+				
+				m_pvd = physx::PxCreatePvd(*m_foundation);
+				if (m_pvd == nullptr) {
+					logger->Fatal("PhysicXFoundation - Failed to create PhysX visual debugger!");
+					return;
+				}
+				
+				physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+				if (transport == nullptr)
+					logger->Error("PhysicXFoundation - Failed to create transport!");
+				else m_pvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+
+				m_physx = PxCreatePhysics(PX_PHYSICS_VERSION, *m_foundation, physx::PxTolerancesScale(), true, m_pvd);
+				if (m_physx == nullptr)
+					logger->Fatal("PhysicXFoundation - Failed to create physX instance!");
 			}
 
 			inline virtual ~PhysicXFoundation() {
+				if (m_physx != nullptr) {
+					m_physx->release();
+					m_physx = nullptr;
+				}
+				if (m_pvd != nullptr) {
+					physx::PxPvdTransport* transport = m_pvd->getTransport();
+					m_pvd->release();
+					m_pvd = nullptr;
+					if (transport != nullptr) transport->release();
+				}
 				if (m_foundation != nullptr) {
 					m_foundation->release();
 					m_foundation = nullptr;
 				}
 			}
+
+			inline OS::Logger* Log()const { return m_errorCallback.Log(); }
+
+			inline physx::PxFoundation* Foundation()const { return m_foundation; }
+
+			inline physx::PxPhysics* PhysX()const { return m_physx; }
+		};
+
+		class PhysicXScene : public virtual Object {
+		private:
+			const Reference<PhysicXFoundation> m_foundation;
+			physx::PxDefaultCpuDispatcher* m_dispatcher = nullptr;
+			physx::PxScene* m_scene = nullptr;
+
+		public:
+			inline PhysicXScene(PhysicXFoundation* foundation) : m_foundation(foundation) {
+				m_dispatcher = physx::PxDefaultCpuDispatcherCreate(max(std::thread::hardware_concurrency() / 4, 1));
+				if (m_dispatcher == nullptr) {
+					m_foundation->Log()->Fatal("PhysicXScene - Failed to create the dispatcher!");
+				}
+				
+				physx::PxSceneDesc sceneDesc(m_foundation->PhysX()->getTolerancesScale());
+				sceneDesc.gravity = physx::PxVec3(0.0f, -9.81f, 0.0f);
+				sceneDesc.cpuDispatcher = m_dispatcher;
+				sceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
+				m_scene = m_foundation->PhysX()->createScene(sceneDesc);
+				if(m_scene == nullptr)
+					m_foundation->Log()->Fatal("PhysicXScene - Failed to create the scene!");
+			}
+
+			inline virtual ~PhysicXScene() {
+				if (m_scene != nullptr) {
+					m_scene->release();
+					m_scene = nullptr;
+				}
+				if (m_dispatcher != nullptr) {
+					m_dispatcher->release();
+					m_dispatcher = nullptr;
+				}
+			}
+
+			physx::PxScene* Scene()const { return m_scene; }
 		};
 	}
 
 	TEST(PhysicsPlayground, Playground) {
 		Jimara::Test::TestEnvironment environment("PhysicsPlayground");
 		Reference<PhysicXFoundation> foundation = Object::Instantiate<PhysicXFoundation>(environment.RootObject()->Context()->Log());
+		Reference<PhysicXScene> scene = Object::Instantiate<PhysicXScene>(foundation);
 	}
 }
 
