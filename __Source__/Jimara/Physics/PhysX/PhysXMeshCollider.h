0a9e0ba77551fe3f7bf7b5fa7dd63c3f80477268
TheDonsky <nikadon.net@gmail.com>
2021-06-20 17:09:39 +0400

    Add missing comments

--- __Source__/Jimara/Physics/PhysX/PhysXMeshCollider.h
+++ __Source__/Jimara/Physics/PhysX/PhysXMeshCollider.h
@@ -6,8 +6,12 @@ namespace Jimara {
 	namespace Physics {
 		namespace PhysX {
 #pragma warning(disable: 4250)
+			/// <summary>
+			/// PhysX-backed mesh collider
+			/// </summary>
 			class PhysXMeshCollider : public virtual SingleMaterialPhysXCollider, public virtual PhysicsMeshCollider {
 			public:
+				/// <summary> Virtual destructor </summary>
 				virtual ~PhysXMeshCollider();
 
 				/// <summary>
@@ -28,17 +32,27 @@ namespace Jimara {
 				virtual void Update(const MeshShape& newShape) override;
 
 			private:
+				// Lock for internal state protection
 				std::mutex m_lock;
+
+				// Dynamic geometry handler, associated with the currently set mesh
 				Reference<Object> m_shapeObject;
+
+				// Underlying physX mesh
 				PhysXReference<physx::PxTriangleMesh> m_triangleMesh;
+				
+				// Current scale
 				Vector3 m_scale;
 
+				// Constructor
 				PhysXMeshCollider(
 					PhysXBody* body, physx::PxShape* shape, PhysXMaterial* material, PhysicsCollider::EventListener* listener, bool active, 
 					Object* shapeObject, const MeshShape& mesh, physx::PxTriangleMesh* physMesh);
 
+				// Changes m_shapeObject
 				void SetShapeObject(Object* shapeObject);
 
+				// Whenever m_shapeObject 'understands' that m_triangleMesh is dirty, it sends this notification
 				void ShapeDirty(Object* shapeObject);
 			};
 #pragma warning(default: 4250)
