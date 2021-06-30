8162b4f714da65672b74112a7ac0c109e0ef6b84
TheDonsky <nikadon.net@gmail.com>
2021-06-30 13:00:55 +0400

    Add controller-controlled zoom

--- __Source__/Jimara-Tests/Components/TestEnvironment/TestEnvironment.cpp
+++ __Source__/Jimara-Tests/Components/TestEnvironment/TestEnvironment.cpp
@@ -41,7 +41,10 @@ namespace Jimara {
 						m_rotationX = max(-80.0f, min(m_rotationX + deltaTime * SENSITIVITY * (delta.x), 80.0f));
 						m_rotationY += deltaTime * SENSITIVITY * delta.y;
 
-						m_zoom = max(-1.0f, min(m_zoom - 0.2f * Context()->Input()->GetAxis(OS::Input::Axis::MOUSE_SCROLL_WHEEL), 8.0f));
+						m_zoom = max(-1.0f, min(
+							m_zoom - 0.2f * Context()->Input()->GetAxis(OS::Input::Axis::MOUSE_SCROLL_WHEEL) + 
+							(deltaTime * (Context()->Input()->GetAxis(OS::Input::Axis::CONTROLLER_LEFT_TRIGGER) - Context()->Input()->GetAxis(OS::Input::Axis::CONTROLLER_RIGHT_TRIGGER)))
+							, 8.0f));
 					}
 
 					float time = m_stopwatch.Elapsed();
