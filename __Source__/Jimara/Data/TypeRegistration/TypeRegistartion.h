afea1298a72cbc10364371f503ed34495dc29301
TheDonsky <nikadon.net@gmail.com>
2021-11-15 16:29:25 +0400

    Add TypeId for consistent type identification

--- __Source__/Jimara/Data/TypeRegistration/TypeRegistartion.h
+++ __Source__/Jimara/Data/TypeRegistration/TypeRegistartion.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../../Core/Object.h"
+#include <type_traits>
 
 /**
 Here are a few macros for defining and implementing projet-wide type registries:
@@ -100,9 +101,145 @@ For integration, one should follow these steps:
 	void RegisteredClassType::UnregisterType() { UnregisterCallbackBody; }
 
 namespace Jimara {
+	/// <summary>
+	/// Basic information about types:
+	/// </summary>
+	class TypeId {
+	private:
+		// Type name
+		std::string_view m_typeName = "void";
+
+		// Type check function
+		typedef bool(*CheckTypeFn)(const Object*);
+
+		// Type check function
+		CheckTypeFn m_checkType = [](const Object*) -> bool { return true; };
+
+		// Type check function for classes
+		template<typename Type>
+		inline static constexpr typename std::enable_if<std::is_class<Type>::value, CheckTypeFn>::type CheckType() {
+			return [](const Object* object)->bool { return dynamic_cast<const Type*>(object) != nullptr; };
+		}
+
+		// Type check function for non-classes
+		template<typename Type>
+		inline static constexpr typename std::enable_if<!std::is_class<Type>::value, CheckTypeFn>::type CheckType() {
+			return [](const Object*) { return std::is_same<Type, void>::value; };
+		}
+
+		// Constructor (a private one, to prevent incorrect assignment)
+		inline constexpr TypeId(const std::string_view& typeName, CheckTypeFn checkType) : m_typeName(typeName), m_checkType(checkType) {}
+
+		// In order to get type name, we'll need some random string that contains it only once; this will generally suffice:
+		template<typename Type>
+		inline static constexpr std::string_view TemplateSignature() {
+#ifdef __clang__
+			return __PRETTY_FUNCTION__;
+#elif defined(__GNUC__)
+			return __PRETTY_FUNCTION__;
+#elif defined(_MSC_VER)
+			return __FUNCSIG__;
+#else
+#error "Jimara::TypeId - Unknown compiler type!"
+			return "";
+#endif
+		}
+
+	public:
+		/// <summary> Default constructor </summary>
+		inline constexpr TypeId() = default;
+
+		/// <summary> Copy-Constructor </summary>
+		inline constexpr TypeId(const TypeId&) = default;
+
+		/// <summary> Copy-Assignment </summary>
+		inline constexpr TypeId& operator=(const TypeId&) = default;
+
+		/// <summary> Type name (full path) </summary>
+		inline constexpr std::string_view Name()const { return m_typeName; }
+
+		/// <summary>
+		/// Checks if the object is derived from this type
+		/// </summary>
+		/// <param name="object"> Object to check </param>
+		/// <returns> True, if the object is of this type </returns>
+		inline bool CheckType(const Object* object)const { return m_checkType(object); }
+
+		/// <summary> Compares two TypeId objects (returns true, if this is 'less than' other) </summary>
+		inline constexpr bool operator<(const TypeId& other)const { return m_typeName < other.m_typeName; }
+
+		/// <summary> Compares two TypeId objects (returns true, if this is 'less than' or equal to other) </summary>
+		inline constexpr bool operator<=(const TypeId& other)const { return m_typeName <= other.m_typeName; }
+
+		/// <summary> Compares two TypeId objects (returns true, if this is equal to other) </summary>
+		inline constexpr bool operator==(const TypeId& other)const { return m_typeName == other.m_typeName; }
+
+		/// <summary> Compares two TypeId objects (returns true, if this is not equal to other) </summary>
+		inline constexpr bool operator!=(const TypeId& other)const { return m_typeName != other.m_typeName; }
+
+		/// <summary> Compares two TypeId objects (returns true, if this is 'greater than' or equal to other) </summary>
+		inline constexpr bool operator>=(const TypeId& other)const { return m_typeName >= other.m_typeName; }
+
+		/// <summary> Compares two TypeId objects (returns true, if this is 'greater than' other) </summary>
+		inline constexpr bool operator>(const TypeId& other)const { return m_typeName > other.m_typeName; }
+
+		/// <summary>
+		/// Generates TypeId for given type
+		/// </summary>
+		/// <typeparam name="Type"> Type to generate TypeId of </typeparam>
+		/// <returns> TypeId for Type </returns>
+		template<typename Type>
+		inline static constexpr TypeId Of() {
+			// Find type name definition:
+			constexpr const std::string_view VOID_NAME = "void";
+			constexpr const std::string_view VOID_SIGNATURE = TemplateSignature<void>();
+			constexpr const size_t PREFFIX_LEN = VOID_SIGNATURE.find(VOID_NAME);
+			constexpr const std::string_view SIGNATURE = TemplateSignature<Type>();
+			constexpr const std::string_view TYPE_DEFINITION = SIGNATURE.substr(PREFFIX_LEN, SIGNATURE.length() + VOID_NAME.length() - VOID_SIGNATURE.length());
+			
+			// Remove 'class'/'struct' preffix to find real type name:
+			constexpr const std::string_view CLASS_PREFFIX = "class ";
+			constexpr const size_t CLASS_OFFSET = (TYPE_DEFINITION.find(CLASS_PREFFIX) == 0) ? CLASS_PREFFIX.length() : (size_t)0;
+			constexpr const std::string_view STRUCT_PREFFIX = "struct ";
+			constexpr const size_t STRUCT_OFFSET = (TYPE_DEFINITION.find(STRUCT_PREFFIX) == 0) ? STRUCT_PREFFIX.length() : (size_t)0;
+			constexpr const std::string_view TYPE_NAME = TYPE_DEFINITION.substr(CLASS_OFFSET + STRUCT_OFFSET);
+
+			// Type check function:
+			constexpr const CheckTypeFn checkType = CheckType<Type>();
+
+			// Create type id:
+			return TypeId(TYPE_NAME, checkType);
+		}
+	};
+
+	// A few static asserts to make sure TypeId::Of<...>.Name() works as intended
+	static_assert(TypeId::Of<void>().Name() == "void");
+	static_assert(TypeId::Of<int>().Name() == "int");
+	static_assert(TypeId::Of<float>().Name() == "float");
+	static_assert(TypeId::Of<TypeId>().Name() == "Jimara::TypeId");
+	static_assert(TypeId::Of<Object>().Name() == "Jimara::Object");
+
+
 	/** 
 	Built-in type registrator, all internal Jimara objects rely on 
 	(Instance only needed to make accessing various type registries possible; the runtime will work fine without this one) 
 	*/
 	JIMARA_DEFINE_TYPE_REGISTRATION_CLASS(BuiltInTypeRegistrator);
+}
+
+namespace std {
+	/// <summary>
+	/// Standsrd library hash overload for TypeId
+	/// </summary>
+	template<>
+	struct hash<Jimara::TypeId> {
+		/// <summary>
+		/// Standsrd library hash overload for TypeId
+		/// </summary>
+		/// <param name="typeId"> Type id </param>
+		/// <returns> Hash of the identifier </returns>
+		inline std::size_t operator()(const Jimara::TypeId& typeId)const {
+			return std::hash<std::string_view>()(typeId.Name());
+		}
+	};
 }
