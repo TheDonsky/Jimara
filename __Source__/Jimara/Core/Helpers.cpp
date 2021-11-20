aa8a5e503435ed92c16c0b66aa3d133f4199020d
TheDonsky <nikadon.net@gmail.com>
2021-11-20 18:51:16 +0400

    Make sure initialisation order is good

--- __Source__/Jimara/Core/Helpers.cpp
+++ __Source__/Jimara/Core/Helpers.cpp
@@ -12,12 +12,15 @@
 namespace Jimara {
 #pragma warning(disable: 4996)
 	namespace {
-		static std::mutex wstring_convert_Lock;
+		static std::mutex& wstring_convert_Lock() {
+			static std::mutex lock;
+			return lock;
+		}
 	}
 
 	template<>
 	std::wstring Convert<std::wstring>(const std::string_view& text) {
-		std::unique_lock<std::mutex> lock(wstring_convert_Lock);
+		std::unique_lock<std::mutex> lock(wstring_convert_Lock());
 		return std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>>().from_bytes(text.data());
 	}
 
@@ -28,7 +31,7 @@ namespace Jimara {
 
 	template<>
 	std::string Convert<std::string>(const std::wstring_view& text) {
-		std::unique_lock<std::mutex> lock(wstring_convert_Lock);
+		std::unique_lock<std::mutex> lock(wstring_convert_Lock());
 		return std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>>().to_bytes(text.data());
 	}
 
