22435b812db0a85ee5cef0d500baa34b5b3a4955
TheDonsky <nikadon.net@gmail.com>
2022-01-11 17:22:47 +0400

    Document existing functionality

--- __Source__/Jimara/Environment/Refactor/Logic/LogicContext.h
+++ __Source__/Jimara/Environment/Refactor/Logic/LogicContext.h
@@ -10,28 +10,60 @@ namespace Jimara {
 #ifndef USE_REFACTORED_SCENE
 namespace Refactor_TMP_Namespace {
 #endif
+	/// <summary>
+	/// Main scene context
+	/// Note: 
+	///		Gives access to the engine internals to the components, 
+	///		without exposing anything that would harm the runtime and main update cycle
+	/// </summary>
 	class SceneContext : public virtual Object {
 	public:
+		/// <summary> Scene update cycle clock </summary>
 		inline Scene::Clock* Time()const { return m_time; }
 
+		/// <summary> Index of the current update cycle since start </summary>
 		inline uint64_t FrameIndex()const { return m_frameIndex.load(); }
 
+		/// <summary> Main logger </summary>
 		inline OS::Logger* Log()const { return m_logger; }
 
+		/// <summary> Input module </summary>
 		inline const OS::Input* Input()const { return m_input; }
 
+		/// <summary> Sub-context for graphics-related stuff </summary>
 		inline Scene::GraphicsContext* Graphics()const { return m_graphics; }
 
+		/// <summary> Sub-context for physics-related stuff </summary>
 		inline Scene::PhysicsContext* Physics()const { return m_physics; }
 
+		/// <summary> Sub-context for audio-related stuff </summary>
 		inline Scene::AudioContext* Audio()const { return m_audio; }
 
+		/// <summary>
+		/// Update lock
+		/// Notes:
+		///		0. This lock is automatically aquired during the update callbacks, physics uppdate and graphics synch point;
+		///		1. Any of the asynchronous job systems that are part of the synch points or the main update cycle will naturally deadlock if they try to use this lock;
+		///		2. Under most circumstances no component, job or any other scene object should access this lock; it's mostly for the external runtime stuff, 
+		///		like the editor and what not;
+		///		3. Object creation/destruction, as well as enabling/disabling will lock this mutex, so doing those from job threads is also not viable.
+		/// </summary>
 		inline std::recursive_mutex& UpdateLock()const { return m_updateLock; }
 
+		/// <summary> 
+		/// Root component 
+		/// Notes:
+		///		0. Any component will have this one on top of their parent heirarchy, unless the user creates some custom object that is not a 'normal' part of the heirarchy;
+		///		1. Destroying the root object while the scene still exists will cause the entire tree to be deleted and the root being replaced.
+		/// </summary>
 		Reference<Component> RootObject()const;
 
+		/// <summary>
+		/// Component that updates each scene update cycle
+		/// </summary>
 		class UpdatingComponent : public virtual Component {
 		protected:
+			/// <summary> Updates component </summary>
 			virtual void Update() = 0;
 
 		private:
@@ -39,34 +71,73 @@ namespace Refactor_TMP_Namespace {
 			friend class SceneContext;
 		};
 
+		/// <summary> Invoked right after UpdatingComponents get updated </summary>
 		inline Event<>& OnUpdate() { return m_onUpdate; }
 
+		/// <summary>
+		/// Stores arbitrary object as a part of the scene data
+		/// Note: This is mostly useful to keep references alive, since there's no way to get the objects stored here.
+		/// </summary>
+		/// <param name="object"> Object to keep alive </param>
 		void StoreDataObject(const Object* object);
+
+		/// <summary>
+		/// Removes arbitrary object stored as a part of the scene data
+		/// Note: Use this to undo previous StoreDataObject() calls
+		/// </summary>
+		/// <param name="object"> Object to keep alive </param>
 		void EraseDataObject(const Object* object);
 
 
 	private:
+		// Scene update cycle clock
 		const Reference<Scene::Clock> m_time;
+
+		// Current frame index
 		std::atomic<uint64_t> m_frameIndex = 0;
+
+		// Main logger
 		const Reference<OS::Logger> m_logger;
+
+		// Main input module
 		const Reference<OS::Input> m_input;
+
+		// Graphics context
 		const Reference<Scene::GraphicsContext> m_graphics;
+
+		// Physics context
 		const Reference<Scene::PhysicsContext> m_physics;
+
+		// Audio context
 		const Reference<Scene::AudioContext> m_audio;
 		
+		// Update lock
 		mutable std::recursive_mutex m_updateLock;
+
+		// OnUpdate() event
 		EventInstance<> m_onUpdate;
 
+		// Flushes any new/removed/enabled/disabled component
 		void FlushComponentSets();
+
+		// Updates component
 		void Update(float deltaTime);
+
+		// Invoked by each component when it gets created
 		void ComponentCreated(Component* component);
+
+		// Invoked by each component when it gets destroyed
 		void ComponentDestroyed(Component* component);
+
+		// Invoked by each component when it gets enabled or disabled
 		void ComponentEnabledStateDirty(Component* component);
 
+		// Constructor
 		inline SceneContext(OS::Logger* logger, OS::Input* input, Scene::GraphicsContext* graphics, Scene::PhysicsContext* physics, Scene::AudioContext* audio)
 			: m_time([]() -> Reference<Scene::Clock> { Reference<Scene::Clock> clock = new Scene::Clock(); clock->ReleaseRef(); return clock; }())
 			, m_logger(logger), m_input(input), m_graphics(graphics), m_physics(physics), m_audio(audio) {}
 
+		// Scene data that lives only while the scene itself is alive and well
 		struct Data : public virtual Object {
 			Data(OS::Logger* logger, OS::Input* input, Scene::GraphicsContext* graphics, Scene::PhysicsContext* physics, Scene::AudioContext* audio);
 			virtual void OnOutOfScope()const final override;
--- __Source__/Jimara/Environment/Refactor/Scene.h
+++ __Source__/Jimara/Environment/Refactor/Scene.h
@@ -18,40 +18,74 @@ namespace Refactor_TMP_Namespace {
 #endif
 	class SceneContext;
 
+	/// <summary>
+	/// Main Scene class with full lifecycle control
+	/// </summary>
 	class Scene : public virtual Object {
 	public:
+		/// <summary> Scene logic context </summary>
 		typedef SceneContext LogicContext;
 
+		/// <summary> Scene sub-context for graphics-related routines and storage </summary>
 		class GraphicsContext;
 
+		/// <summary> Data necessary for the graphics context to be created </summary>
 		struct GraphicsConstants;
 
+		/// <summary> Scene sub-context for physics-related routines and storage </summary>
 		class PhysicsContext;
 
+		/// <summary> Scene sub-context for audio-related routines and storage </summary>
 		class AudioContext;
 
+		/// <summary> Simple clock for various scene contexts </summary>
 		class Clock;
 
 	public:
+		/// <summary>
+		/// Creates a new instance of a scene
+		/// Note: Most arguments can be null, but it's highly adviced to provide each and every one of them for safety.
+		/// </summary>
+		/// <param name="inputModule"> Input module for the scene context </param>
+		/// <param name="graphicsConfiguration"> Data for graphics context creation </param>
+		/// <param name="physicsInstance"> Physics instance to use </param>
+		/// <param name="audioDevice"> Audio device to use </param>
+		/// <returns> New instance of a scene if successful, nullptr otherwise </returns>
 		static Reference<Scene> Create(
 			OS::Input* inputModule,
 			GraphicsConstants* graphicsConfiguration,
 			Physics::PhysicsInstance* physicsInstance,
 			Audio::AudioDevice* audioDevice);
 
+		/// <summary> Scene logic context </summary>
 		LogicContext* Context()const;
 
+		/// <summary> Same as Context()->RootObject() </summary>
 		Reference<Component> RootObject()const;
 
+		/// <summary>
+		/// Updates the scene
+		/// </summary>
+		/// <param name="deltaTime"> Elapsed time from the last update to simulate (Yep, the Scene has no internal clock) </param>
 		void Update(float deltaTime);
 
 	private:
+		// Type registry for built-in types
 		const Reference<const BuiltInTypeRegistrator> m_builtInTypeRegistry = BuiltInTypeRegistrator::Instance();
+
+		// LogicContext::Data
 		const Reference<Object> m_logicScene;
+
+		// GraphicsContext::Data
 		const Reference<Object> m_graphicsScene;
+
+		// PhysicsContext::Data
 		const Reference<Object> m_physicsScene;
+
+		// AudioContext::Data
 		const Reference<Object> m_audioScene;
 
+		// Basic pointer and lock for data-context relationships
 		template<typename DataType>
 		struct DataWeakReference {
 			mutable SpinLock lock;
@@ -64,8 +98,10 @@ namespace Refactor_TMP_Namespace {
 			}
 		};
 
+		// Constructor
 		Scene(Reference<Object> logic, Reference<Object> graphics, Reference<Object> physics, Reference<Object> audio);
 
+		// So far, SceneContext is not a nested class and it needs to access the internals...
 		friend class SceneContext;
 	};
 
@@ -77,4 +113,3 @@ namespace Refactor_TMP_Namespace {
 #include "SceneClock.h"
 #include "Logic/LogicContext.h"
 #include "../../Components/Component.h"
-
