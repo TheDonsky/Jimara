d6e0c61532d4fc2d7e2547e431518607dbe59c7b
TheDonsky <nikadon.net@gmail.com>
2022-01-26 10:17:20 +0400

    Make sure there can be multiple GameView instances without image stretching

--- __Source__/Jimara-Editor/EditorWindows/Scene/GameView.cpp
+++ __Source__/Jimara-Editor/EditorWindows/Scene/GameView.cpp
@@ -14,13 +14,36 @@ namespace Jimara {
 			Reference<EditorScene> editorScene = GetOrCreateScene();
 			auto toVec2 = [](const ImVec2& v) { return Jimara::Vector2(v.x, v.y); };
 			const Vector2 viewport = toVec2(ImGui::GetItemRectSize());
-			const Vector2 windowStart = toVec2(ImGui::GetWindowPos()) + Jimara::Vector2(0.0f, viewport.y);
 			const Vector2 windowSize = toVec2(ImGui::GetWindowSize()) - Jimara::Vector2(0.0f, viewport.y);
-			const Vector2 windowEnd = (windowStart + windowSize);
+			
 			editorScene->RequestResolution(Size2((uint32_t)windowSize.x, (uint32_t)windowSize.y));
 			Reference<Graphics::TextureView> texture = editorScene->RootObject()->Context()->Graphics()->Renderers().TargetTexture();
-			if (texture != nullptr)
+			if (texture != nullptr && (windowSize.x * windowSize.y) > 0.0f) {
+				const Vector2 textureSize = [&]() {
+					const Size3 size = texture->TargetTexture()->Size();
+					return Vector2(size.x, size.y);
+				}();
+				if ((textureSize.x * textureSize.y) <= 0.0f) return;
+
+				Vector2 windowStart = toVec2(ImGui::GetWindowPos()) + Jimara::Vector2(0.0f, viewport.y);
+				Vector2 windowEnd = (windowStart + windowSize);
+
+				const float windowAspect = (windowSize.x / windowSize.y);
+				const float textureAspect = (textureSize.x / textureSize.y);
+
+				if (windowAspect > textureAspect) {
+					float widthDiff = (windowSize.x - (textureAspect * windowSize.y)) * 0.5f;
+					windowStart.x += widthDiff;
+					windowEnd.x -= widthDiff;
+				}
+				else {
+					float heightDiff = (windowSize.y - (windowSize.x / textureAspect)) * 0.5f;
+					windowStart.y += heightDiff;
+					windowEnd.y -= heightDiff;
+				}
+
 				ImGuiRenderer::Texture(texture->TargetTexture(), Jimara::Rect(windowStart, windowEnd));
+			}
 		}
 
 		namespace {
