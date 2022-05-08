04f0708ce24fe8d5c236950763588bfef13c762c
TheDonsky <nikadon.net@gmail.com>
2022-05-08 12:20:40 +0400

    Add basic tests for clear flags

--- __Source__/Jimara-Tests/Components/GraphicsObjects/CameraSettingsTest.cpp
+++ __Source__/Jimara-Tests/Components/GraphicsObjects/CameraSettingsTest.cpp
@@ -127,4 +127,102 @@ namespace Jimara {
 				});
 		}
 	}
+
+	TEST(CameraSettingsTest, ClearFlags) {
+		Jimara::Test::TestEnvironment environment("Clear Flags Test");
+
+		environment.ExecuteOnUpdateNow([&]() {
+			Reference<Transform> sun = Object::Instantiate<Transform>(environment.RootObject(), "Sun", Vector3(0.0f), Vector3(64.0f, 32.0f, 0.0f));
+			Object::Instantiate<DirectionalLight>(sun, "Sun Light", Vector3(0.85f, 0.85f, 0.856f));
+			Reference<Transform> back = Object::Instantiate<Transform>(environment.RootObject(), "Back");
+			back->LookTowards(-sun->Forward());
+			Object::Instantiate<DirectionalLight>(back, "Back Light", Vector3(0.125f, 0.125f, 0.125f));
+			});
+
+		static const constexpr GraphicsLayer layerA = 0;
+		static const constexpr GraphicsLayer layerB = 1;
+		static const constexpr int sleepInterval = 3;
+
+		Reference<MeshRenderer> rendererA;
+		Reference<MeshRenderer> rendererB;
+		Reference<Camera> cameraA;
+		Reference<Camera> cameraB;
+
+		// Create objects:
+		environment.ExecuteOnUpdateNow([&]() {
+			Transform* meshBaseTransform = Object::Instantiate<Transform>(environment.RootObject(), "meshBaseTransform");
+			static void(*rotateGeometry)(Transform*) = [](Transform* transform) {
+				transform->SetLocalEulerAngles(Math::Up() * (transform->LocalEulerAngles().y + transform->Context()->Time()->ScaledDeltaTime() * 180.0f));
+			};
+			meshBaseTransform->Context()->OnUpdate() += Callback<>(rotateGeometry, meshBaseTransform);
+
+			auto createRenderer = [&](auto createMesh, const Vector3& color, const Vector3& offset) {
+				const Reference<TriMesh> mesh = createMesh();
+				const Reference<const Material::Instance> material = SampleDiffuseShader::MaterialInstance(
+					environment.RootObject()->Context()->Graphics()->Device(), color);
+				const Reference<MeshRenderer> renderer = Object::Instantiate<MeshRenderer>(
+					Object::Instantiate<Transform>(meshBaseTransform, "rendererA", offset), "rendererA", mesh);
+				renderer->SetMaterialInstance(material);
+				return renderer;
+			};
+			rendererA = createRenderer(
+				[]() { return GenerateMesh::Tri::Cone(Vector3(0.0f, -0.5f, 0.0f), 1.0f, 0.5f, 16); },
+				Vector3(0.0f, 1.0f, 0.0f), Vector3(-0.75f, 0.0f, 0.0f));
+			rendererB = createRenderer(
+				[]() { return GenerateMesh::Tri::Cylinder(Vector3(0.0f), 0.5f, 1.0f, 16); },
+				Vector3(0.0f, 0.0f, 1.0f), Vector3(0.75f, 0.0f, 0.0f));
+			
+
+			cameraA = environment.RootObject()->GetComponentInChildren<Camera>();
+			cameraB = Object::Instantiate<Camera>(cameraA, "CameraB");
+			static void(*updateCameraB)(Camera * cameraB) = [](Camera* camera) {
+				const Camera* parentCamera = camera->GetComponentInParents<Camera>(false);
+				camera->SetFieldOfView(parentCamera->FieldOfView());
+				camera->SetClearColor(Vector4(0.125f));
+				camera->SetClosePlane(parentCamera->ClosePlane());
+				camera->SetFarPlane(parentCamera->FarPlane());
+			};
+			cameraB->Context()->OnUpdate() += Callback<>(updateCameraB, cameraB.operator->());
+
+			rendererA->SetLayer(layerA);
+			cameraA->SetRendererPriority(1);
+			cameraA->RenderLayers(GraphicsLayerMask(layerA));
+
+			rendererB->SetLayer(layerB);
+			cameraB->RenderLayers(GraphicsLayerMask(layerB));
+			});
+
+
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(sleepInterval));
+			environment.SetWindowName("Do not clear color on B (Cylinder will appear on top of the cone)");
+			environment.ExecuteOnUpdateNow([&]() { 
+				cameraB->SetRendererFlags(Graphics::RenderPass::Flags::CLEAR_DEPTH | Graphics::RenderPass::Flags::RESOLVE_COLOR);
+				});
+		}
+
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(sleepInterval));
+			environment.SetWindowName("Do not clear depth on B (Only cyllinder will appera, but the cone will cut it out where it would have been)");
+			environment.ExecuteOnUpdateNow([&]() {
+				cameraB->SetRendererFlags(Graphics::RenderPass::Flags::CLEAR_COLOR | Graphics::RenderPass::Flags::RESOLVE_COLOR);
+				});
+		}
+
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(sleepInterval));
+			environment.SetWindowName("Do not clear color or depth (Will appear as if there's only a single camera)");
+			environment.ExecuteOnUpdateNow([&]() {
+				cameraB->SetRendererFlags(Graphics::RenderPass::Flags::RESOLVE_COLOR);
+				});
+		}
+
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(sleepInterval));
+			environment.SetWindowName("Do not resolve (Only triangle will be visible)");
+			environment.ExecuteOnUpdateNow([&]() {
+				cameraB->SetRendererFlags(Graphics::RenderPass::Flags::CLEAR_COLOR | Graphics::RenderPass::Flags::CLEAR_DEPTH);
+				});
+		}
+	}
 }
