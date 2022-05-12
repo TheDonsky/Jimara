1183e7a3679eefbd6c579c7638f7b29fdb47c82c
TheDonsky <nikadon.net@gmail.com>
2022-05-12 23:45:31 +0400

    Implement visual part of the scale handles

---
+++ __Source__/Jimara-Editor/Gizmos/Handles/Compound/TripleAxisScaleHandle.cpp
@@ -0,0 +1,187 @@
+#include "TripleAxisScalehandle.h"
+
+
+namespace Jimara {
+	namespace Editor {
+		struct TripleAxisScalehandle::Helpers {
+			static const constexpr float SHAPE_SIZE = 0.075f;
+			static const constexpr float ARROW_SIZE = 0.55f;
+
+			inline static TriMesh* Shape() {
+				static const Reference<TriMesh> shape = GenerateMesh::Tri::Box(-Vector3(SHAPE_SIZE * 0.5f), Vector3(SHAPE_SIZE * 0.5f));
+				return shape;
+			}
+
+			inline static void Initialize(const ScaleHandle& handle, Vector3 color) {
+				auto materialInstance = SampleDiffuseShader::MaterialInstance(handle.handle->Context()->Graphics()->Device(), color);
+				if (handle.handleConnector != nullptr) {
+					handle.handleConnector->SetParent(handle.handle);
+					Reference<MeshRenderer> renderer = Object::Instantiate<MeshRenderer>(handle.handleConnector, "Renderer", Shape());
+					renderer->SetMaterialInstance(materialInstance);
+					renderer->SetLayer(static_cast<GraphicsLayer>(GizmoLayers::HANDLE));
+				}
+				{
+					Reference<MeshRenderer> renderer = Object::Instantiate<MeshRenderer>(handle.handle, "Renderer", Shape());
+					renderer->SetMaterialInstance(materialInstance);
+					renderer->SetLayer(static_cast<GraphicsLayer>(GizmoLayers::HANDLE));
+				}
+				Helpers::SetLocalPosition(handle, handle.defaultPosition);
+			};
+
+			inline static void SetWorldPosition(const ScaleHandle& handle, Vector3 position) {
+				handle.handle->SetWorldPosition(position);
+				if (handle.handleConnector != nullptr) {
+					handle.handleConnector->SetLocalPosition(-handle.handle->LocalPosition() * 0.5f);
+					Vector3 rawScale = -handle.handleConnector->LocalPosition() / (SHAPE_SIZE * 0.5f) + 0.25f;
+					handle.handleConnector->SetLocalScale(Vector3(std::abs(rawScale.x), std::abs(rawScale.y), std::abs(rawScale.z)));
+				}
+				else {
+					const float baseSqrSize = Math::SqrMagnitude(handle.defaultPosition);
+					if (baseSqrSize > 0.0001f) {
+						Vector3 scale = handle.handle->LocalPosition() * (2.0f / SHAPE_SIZE);
+						scale.x = max(std::abs(scale.x), 0.1f);
+						scale.y = max(std::abs(scale.y), 0.1f);
+						scale.z = max(std::abs(scale.z), 0.1f);
+						handle.handle->SetLocalScale(scale);
+					}
+				}
+			}
+
+			inline static void SetLocalPosition(const ScaleHandle& handle, Vector3 position) {
+				SetWorldPosition(handle, handle.handle->GetComponentInParents<Transform>(false)->LocalToWorldPosition(position));
+			}
+
+			template<typename CallType>
+			inline static bool ForAllHandles(const TripleAxisScalehandle* self, const CallType& call) {
+				if (call(self->m_center)) return true;
+				else if (call(self->m_xHandle)) return true;
+				else if (call(self->m_yHandle)) return true;
+				else if (call(self->m_zHandle)) return true;
+				else if (call(self->m_xyHandle)) return true;
+				else if (call(self->m_xzHandle)) return true;
+				else if (call(self->m_yzHandle)) return true;
+				else return false;
+			}
+		};
+
+		TripleAxisScalehandle::TripleAxisScalehandle(Component* parent, const std::string_view& name, float size)
+			: Component(parent, name), Transform(parent, name)
+			, m_center(ScaleHandle{ Object::Instantiate<DragHandle>(this, "XYZ"), Vector3(0.0f), nullptr })
+			, m_xHandle(ScaleHandle{
+			Object::Instantiate<DragHandle>(this, "X", DragHandle::Flags::DRAG_X),
+			Vector3(Helpers::ARROW_SIZE, 0.0f, 0.0f), Object::Instantiate<Transform>(this, "X_Connector") })
+			, m_yHandle(ScaleHandle{
+			Object::Instantiate<DragHandle>(this, "Y", DragHandle::Flags::DRAG_Y),
+			Vector3(0.0f, Helpers::ARROW_SIZE, 0.0f), Object::Instantiate<Transform>(this, "Y_Connector") })
+			, m_zHandle(ScaleHandle{
+			Object::Instantiate<DragHandle>(this, "Z", DragHandle::Flags::DRAG_Z),
+			Vector3(0.0f, 0.0f, Helpers::ARROW_SIZE), Object::Instantiate<Transform>(this, "Z_Connector") })
+			, m_xyHandle(ScaleHandle{
+			Object::Instantiate<DragHandle>(this, "XY", DragHandle::Flags::DRAG_XY),
+			Vector3(Helpers::SHAPE_SIZE, Helpers::SHAPE_SIZE, 0.0f), nullptr })
+			, m_xzHandle(ScaleHandle{
+			Object::Instantiate<DragHandle>(this, "XZ", DragHandle::Flags::DRAG_XZ),
+			Vector3(Helpers::SHAPE_SIZE, 0.0f, Helpers::SHAPE_SIZE), nullptr })
+			, m_yzHandle(ScaleHandle{
+			Object::Instantiate<DragHandle>(this, "YZ", DragHandle::Flags::DRAG_YZ),
+			Vector3(0.0f, Helpers::SHAPE_SIZE, Helpers::SHAPE_SIZE), nullptr })
+			, m_hover(GizmoViewportHover::GetFor(GizmoScene::GetContext(parent->Context())->Viewport()))
+			, m_size(size) {
+
+			Helpers::Initialize(m_center, Vector3(1.0f, 1.0f, 1.0f));
+
+			Helpers::Initialize(m_xHandle, Vector3(1.0f, 0.0f, 0.0f));
+			Helpers::Initialize(m_yHandle, Vector3(0.0f, 1.0f, 0.0f));
+			Helpers::Initialize(m_zHandle, Vector3(0.0f, 0.0f, 1.0f));
+
+			Helpers::Initialize(m_xyHandle, Vector3(0.0f, 0.0f, 1.0f));
+			Helpers::Initialize(m_xzHandle, Vector3(0.0f, 1.0f, 0.0f));
+			Helpers::Initialize(m_yzHandle, Vector3(1.0f, 0.0f, 0.0f));
+
+			Helpers::ForAllHandles(this, [&](const ScaleHandle& handle) {
+				handle.handle->OnHandleActivated() += Callback(&TripleAxisScalehandle::HandleActivated, this);
+				handle.handle->OnHandleUpdated() += Callback(&TripleAxisScalehandle::HandleUpdated, this);
+				handle.handle->OnHandleDeactivated() += Callback(&TripleAxisScalehandle::HandleDisabled, this);
+				return false;
+				});
+			HandleDisabled(nullptr);
+			UpdateScale();
+		}
+
+		TripleAxisScalehandle::~TripleAxisScalehandle() {}
+
+		bool TripleAxisScalehandle::HandleActive()const {
+			return Helpers::ForAllHandles(this, [](const ScaleHandle& handle) { return handle.handle->HandleActive(); });
+		}
+
+		void TripleAxisScalehandle::Update() { UpdateScale(); }
+
+		void TripleAxisScalehandle::OnComponentDestroyed() {
+			Transform::OnComponentDestroyed();
+			Scene::LogicContext::UpdatingComponent::OnComponentDestroyed();
+			Helpers::ForAllHandles(this, [&](const ScaleHandle& handle) {
+				handle.handle->OnHandleActivated() -= Callback(&TripleAxisScalehandle::HandleActivated, this);
+				handle.handle->OnHandleUpdated() -= Callback(&TripleAxisScalehandle::HandleUpdated, this);
+				handle.handle->OnHandleDeactivated() -= Callback(&TripleAxisScalehandle::HandleDisabled, this);
+				return false;
+				});
+		}
+
+
+		void TripleAxisScalehandle::HandleActivated(Handle*) {
+			m_hoverOrigin = m_hover->HandleGizmoHover().objectPosition;
+			m_delta = Vector3(1.0f);
+			m_onHandleActivated(this);
+		}
+
+		void TripleAxisScalehandle::HandleUpdated(Handle* handle) {
+			const ScaleHandle* scaleHandle = nullptr;
+			Helpers::ForAllHandles(this, [&](const ScaleHandle& ref) {
+				if (ref.handle == handle) {
+					scaleHandle = &ref;
+					return true;
+				}
+				else return false;
+				});
+			if (scaleHandle == nullptr) {
+				Context()->Log()->Error("TripleAxisScalehandle::HandleUpdated - Got input from unknown handle!");
+				return;
+			}
+
+			Vector3 rawDelta = dynamic_cast<DragHandle*>(handle)->Delta();
+			if (scaleHandle == &m_center) {
+				const Vector3 curScale = scaleHandle->handle->LocalScale();
+				const Vector3 initialDelta = m_hoverOrigin - WorldPosition();
+				const Vector3 deltaScale = Vector3(Math::Dot(rawDelta, initialDelta) / Math::SqrMagnitude(initialDelta));
+				const Vector3 finalScale = curScale + deltaScale;
+				scaleHandle->handle->SetLocalScale(finalScale);
+				m_delta = (finalScale / curScale);
+			}
+			else {
+				const Vector3 scaleDirection = Math::Normalize(LocalToWorldDirection(scaleHandle->defaultPosition));
+				auto scaleFactor = [&]() { return Math::Dot(scaleHandle->handle->WorldPosition() - WorldPosition(), scaleDirection); };
+				float curVal = scaleFactor();
+				Helpers::SetWorldPosition(*scaleHandle,
+					scaleHandle->handle->WorldPosition() +
+					(scaleDirection * Math::Dot(rawDelta, scaleDirection)));
+				float newValue = scaleFactor();
+				m_delta = (scaleDirection * ((newValue / curVal) - 1.0f)) + 1.0f;
+			}
+			m_onHandleUpdated(this);
+		}
+
+		void TripleAxisScalehandle::HandleDisabled(Handle*) {
+			m_delta = Vector3(1.0f);
+			Helpers::ForAllHandles(this, [](const ScaleHandle& handle) {
+				Helpers::SetLocalPosition(handle, handle.defaultPosition);
+				return false;
+				});
+			m_center.handle->SetLocalScale(Vector3(2.0f));
+			m_onHandleDeactivated(this);
+		}
+
+		void TripleAxisScalehandle::UpdateScale() {
+			SetLocalScale(Vector3(m_size * m_center.handle->GizmoContext()->Viewport()->GizmoSizeAt(WorldPosition())));
+		}
+	}
+}
--- __Source__/Jimara-Editor/Gizmos/Handles/Compound/TripleAxisScalehandle.h
+++ __Source__/Jimara-Editor/Gizmos/Handles/Compound/TripleAxisScalehandle.h
@@ -15,59 +15,13 @@ namespace Jimara {
 			/// <param name="parent"> Parent comonent </param>
 			/// <param name="name"> Gizmo group name </param>
 			/// <param name="size"> Gizmo size multiplier </param>
-			inline TripleAxisScalehandle(Component* parent, const std::string_view& name, float size = 1.0f) 
-				: Component(parent, name), Transform(parent, name)
-				, m_center(ScaleHandle{ Object::Instantiate<DragHandle>(this, "XYZ"), Vector3(0.0f, 0.0f, 0.0f), nullptr })
-				, m_xHandle(ScaleHandle{ 
-				Object::Instantiate<DragHandle>(this, "X", DragHandle::Flags::DRAG_X), 
-				Vector3(0.5f, 0.0f, 0.0f), Object::Instantiate<Transform>(this, "X_Connector") })
-				, m_yHandle(ScaleHandle{ 
-				Object::Instantiate<DragHandle>(this, "Y", DragHandle::Flags::DRAG_Y), 
-				Vector3(0.0f, 0.5f, 0.0f), Object::Instantiate<Transform>(this, "Y_Connector") })
-				, m_zHandle(ScaleHandle{ 
-				Object::Instantiate<DragHandle>(this, "Z", DragHandle::Flags::DRAG_Z), 
-				Vector3(0.0f, 0.0f, 0.5f), Object::Instantiate<Transform>(this, "Z_Connector") })
-				, m_xyHandle(ScaleHandle{ Object::Instantiate<DragHandle>(this, "XY", DragHandle::Flags::DRAG_XY), Vector3(0.0f, 0.0f, 0.0f), nullptr })
-				, m_xzHandle(ScaleHandle{ Object::Instantiate<DragHandle>(this, "XZ", DragHandle::Flags::DRAG_XZ), Vector3(0.0f, 0.0f, 0.0f), nullptr })
-				, m_yzHandle(ScaleHandle{ Object::Instantiate<DragHandle>(this, "YZ", DragHandle::Flags::DRAG_YZ), Vector3(0.0f, 0.0f, 0.0f), nullptr })
-				, m_hover(GizmoViewportHover::GetFor(GizmoScene::GetContext(parent->Context())->Viewport()))
-				, m_size(size) {
-				UpdateScale();
-				auto initialize = [&](const ScaleHandle& handle, Vector3 color) {
-					auto materialInstance = SampleDiffuseShader::MaterialInstance(Context()->Graphics()->Device(), color);
-					if (handle.handleConnector != nullptr) {
-						handle.handleConnector->SetParent(handle.handle); 
-						Reference<MeshRenderer> renderer = Object::Instantiate<MeshRenderer>(handle.handleConnector, "Renderer", Shape());
-						renderer->SetMaterialInstance(materialInstance);
-						renderer->SetLayer(static_cast<GraphicsLayer>(GizmoLayers::HANDLE));
-					}
-					{
-						Reference<MeshRenderer> renderer = Object::Instantiate<MeshRenderer>(handle.handle, "Renderer", Shape());
-						renderer->SetMaterialInstance(materialInstance);
-						renderer->SetLayer(static_cast<GraphicsLayer>(GizmoLayers::HANDLE));
-					}
-					handle.SetLocalPosition(handle.defaultPosition);
-				};
-				initialize(m_center, Vector3(1.0f, 1.0f, 1.0f));
-				initialize(m_xHandle, Vector3(1.0f, 0.0f, 0.0f));
-				initialize(m_yHandle, Vector3(0.0f, 1.0f, 0.0f));
-				initialize(m_zHandle, Vector3(0.0f, 0.0f, 1.0f));
-				ForAllHandles([&](const ScaleHandle& handle) {
-					handle.handle->OnHandleActivated() += Callback(&TripleAxisScalehandle::HandleActivated, this);
-					handle.handle->OnHandleUpdated() += Callback(&TripleAxisScalehandle::HandleUpdated, this);
-					handle.handle->OnHandleDeactivated() += Callback(&TripleAxisScalehandle::HandleDisabled, this);
-					return false;
-					});
-				HandleDisabled(nullptr);
-			}
+			TripleAxisScalehandle(Component* parent, const std::string_view& name, float size = 1.0f);
 
 			/// <summary>  Virtual destructor </summary>
-			virtual ~TripleAxisScalehandle() {}
+			virtual ~TripleAxisScalehandle();
 
 			/// <summary> Tells, if the underlying handles are active or not </summary>
-			inline bool HandleActive()const {
-				return ForAllHandles([](const ScaleHandle& handle) { return handle.handle->HandleActive(); });
-			}
+			bool HandleActive()const;
 
 			/// <summary> Sum of all underlying giozmo deltas </summary>
 			inline Vector3 Delta()const { return m_delta; }
@@ -83,19 +37,10 @@ namespace Jimara {
 
 		protected:
 			/// <summary> Updates component </summary>
-			inline virtual void Update()override { UpdateScale(); }
+			virtual void Update()override;
 
 			/// <summary> Invoked, when destroyed </summary>
-			inline virtual void OnComponentDestroyed() override {
-				Transform::OnComponentDestroyed();
-				Scene::LogicContext::UpdatingComponent::OnComponentDestroyed();
-				ForAllHandles([&](const ScaleHandle& handle) {
-					handle.handle->OnHandleActivated() -= Callback(&TripleAxisScalehandle::HandleActivated, this);
-					handle.handle->OnHandleUpdated() -= Callback(&TripleAxisScalehandle::HandleUpdated, this);
-					handle.handle->OnHandleDeactivated() -= Callback(&TripleAxisScalehandle::HandleDisabled, this);
-					return false;
-					});
-			}
+			virtual void OnComponentDestroyed()override;
 
 		private:
 			// Underlying handles
@@ -103,19 +48,6 @@ namespace Jimara {
 				Reference<DragHandle> handle;
 				Vector3 defaultPosition;
 				Reference<Transform> handleConnector;
-
-				inline void SetLocalPosition(Vector3 position)const {
-					SetWorldPosition(handle->GetComponentInParents<Transform>(false)->LocalToWorldPosition(position));
-				}
-
-				inline void SetWorldPosition(Vector3 position)const {
-					handle->SetWorldPosition(position);
-					if (handleConnector != nullptr) {
-						handleConnector->SetLocalPosition(-handle->LocalPosition() * 0.5f);
-						Vector3 rawScale = -handleConnector->LocalPosition() / (SHAPE_SIZE * 0.5f) + 0.25f;
-						handleConnector->SetLocalScale(Vector3(std::abs(rawScale.x), std::abs(rawScale.y), std::abs(rawScale.z)));
-					}
-				}
 			};
 			const ScaleHandle m_center;
 			const ScaleHandle m_xHandle;
@@ -124,6 +56,9 @@ namespace Jimara {
 			const ScaleHandle m_xyHandle;
 			const ScaleHandle m_xzHandle;
 			const ScaleHandle m_yzHandle;
+
+			// Some helper functions reside here...
+			struct Helpers;
 
 			// Hover query
 			const Reference<GizmoViewportHover> m_hover;
@@ -141,80 +76,12 @@ namespace Jimara {
 			EventInstance<TripleAxisScalehandle*> m_onHandleDeactivated;
 
 			// Underlying handle events
-			inline void HandleActivated(Handle*) { 
-				m_hoverOrigin = m_hover->HandleGizmoHover().objectPosition;
-				m_delta = Vector3(1.0f); 
-				m_onHandleActivated(this); 
-			}
-			inline void HandleUpdated(Handle* handle) { 
-				const ScaleHandle* scaleHandle = nullptr;
-				ForAllHandles([&](const ScaleHandle& ref) {
-					if (ref.handle == handle) {
-						scaleHandle = &ref;
-						return true;
-					}
-					else return false;
-					});
-				if (scaleHandle == nullptr) {
-					Context()->Log()->Error("TripleAxisScalehandle::HandleUpdated - Got input from unknown handle!");
-					return;
-				}
-
-				Vector3 rawDelta = dynamic_cast<DragHandle*>(handle)->Delta();
-				if (scaleHandle == &m_center) {
-					const Vector3 curScale = scaleHandle->handle->LocalScale();
-					const Vector3 initialDelta = m_hoverOrigin - WorldPosition();
-					const Vector3 deltaScale = Vector3(Math::Dot(rawDelta, initialDelta) / Math::SqrMagnitude(initialDelta));
-					const Vector3 finalScale = curScale + deltaScale;
-					scaleHandle->handle->SetLocalScale(finalScale);
-					m_delta = (finalScale / curScale);
-				}
-				else {
-					// __TODO__: Implement for XY/XZ/YZ...
-					const Vector3 scaleDirection = Math::Normalize(LocalToWorldDirection(scaleHandle->defaultPosition));
-					auto scaleFactor = [&]() { return Math::Dot(scaleHandle->handle->WorldPosition() - WorldPosition(), scaleDirection); };
-					float curVal = scaleFactor();
-					scaleHandle->SetWorldPosition(
-						scaleHandle->handle->WorldPosition() +
-						(scaleDirection * Math::Dot(rawDelta, scaleDirection)));
-					float newValue = scaleFactor();
-					m_delta = (scaleDirection * ((newValue / curVal) - 1.0f)) + 1.0f;
-				}
-				m_onHandleUpdated(this); 
-			}
-			inline void HandleDisabled(Handle*) { 
-				m_delta = Vector3(1.0f);
-				ForAllHandles([](const ScaleHandle& handle) { 
-					handle.SetLocalPosition(handle.defaultPosition);
-					return false;
-					});
-				m_center.handle->SetLocalScale(Vector3(2.0f));
-				m_onHandleDeactivated(this); 
-			}
-
-			template<typename CallType>
-			inline bool ForAllHandles(const CallType& call)const {
-				if (call(m_center)) return true;
-				else if (call(m_xHandle)) return true;
-				else if (call(m_yHandle)) return true;
-				else if (call(m_zHandle)) return true;
-				else if (call(m_xyHandle)) return true;
-				else if (call(m_xzHandle)) return true;
-				else if (call(m_yzHandle)) return true;
-				else return false;
-			}
+			void HandleActivated(Handle*);
+			void HandleUpdated(Handle* handle);
+			void HandleDisabled(Handle*);
 
 			// Actual update function
-			inline void UpdateScale() {
-				SetLocalScale(Vector3(m_size * m_center.handle->GizmoContext()->Viewport()->GizmoSizeAt(WorldPosition())));
-			}
-
-			static const constexpr float SHAPE_SIZE = 0.075f;
-
-			inline static TriMesh* Shape() {
-				static const Reference<TriMesh> shape = GenerateMesh::Tri::Box(-Vector3(SHAPE_SIZE * 0.5f), Vector3(SHAPE_SIZE * 0.5f));
-				return shape;
-			}
+			void UpdateScale();
 		};
 	}
 }
