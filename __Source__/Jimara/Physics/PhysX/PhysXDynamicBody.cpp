902a9eb729ca3a3fe8a63ca6eb39e547161a75bc
TheDonsky <nikadon.net@gmail.com>
2022-06-24 15:08:37 +0400

    Take advantage of the fact, that lock flag enumeration from physX is a binary equivalent of the one from our engine

--- __Source__/Jimara/Physics/PhysX/PhysXDynamicBody.cpp
+++ __Source__/Jimara/Physics/PhysX/PhysXDynamicBody.cpp
@@ -40,38 +40,23 @@ namespace Jimara {
 
 			void PhysXDynamicBody::MoveKinematic(const Matrix4& transform) { PhysXScene::WriteLock lock(Scene()); operator->()->setKinematicTarget(physx::PxTransform(Translate(transform))); }
 
-			namespace {
-				static const uint8_t LOCK_MASK_PAIR_COUNT = 6;
-
-				static const std::pair<physx::PxRigidDynamicLockFlag::Enum, DynamicBody::LockFlag> LOCK_MASK_PAIRS[LOCK_MASK_PAIR_COUNT] = {
-					std::make_pair(physx::PxRigidDynamicLockFlag::eLOCK_LINEAR_X, DynamicBody::LockFlag::MOVEMENT_X),
-					std::make_pair(physx::PxRigidDynamicLockFlag::eLOCK_LINEAR_Y, DynamicBody::LockFlag::MOVEMENT_Y),
-					std::make_pair(physx::PxRigidDynamicLockFlag::eLOCK_LINEAR_Z, DynamicBody::LockFlag::MOVEMENT_Z),
-					std::make_pair(physx::PxRigidDynamicLockFlag::eLOCK_ANGULAR_X, DynamicBody::LockFlag::ROTATION_X),
-					std::make_pair(physx::PxRigidDynamicLockFlag::eLOCK_ANGULAR_Y, DynamicBody::LockFlag::ROTATION_Y),
-					std::make_pair(physx::PxRigidDynamicLockFlag::eLOCK_ANGULAR_Z, DynamicBody::LockFlag::ROTATION_Z)
-				};
-			}
+			// Let's make sure the enumerations match (will make logic somewhat simpler)
+			static_assert(physx::PxRigidDynamicLockFlag::eLOCK_LINEAR_X == (physx::PxRigidDynamicLockFlag::Enum)DynamicBody::LockFlag::MOVEMENT_X);
+			static_assert(physx::PxRigidDynamicLockFlag::eLOCK_LINEAR_Y == (physx::PxRigidDynamicLockFlag::Enum)DynamicBody::LockFlag::MOVEMENT_Y);
+			static_assert(physx::PxRigidDynamicLockFlag::eLOCK_LINEAR_Z == (physx::PxRigidDynamicLockFlag::Enum)DynamicBody::LockFlag::MOVEMENT_Z);
+			static_assert(physx::PxRigidDynamicLockFlag::eLOCK_ANGULAR_X == (physx::PxRigidDynamicLockFlag::Enum)DynamicBody::LockFlag::ROTATION_X);
+			static_assert(physx::PxRigidDynamicLockFlag::eLOCK_ANGULAR_Y == (physx::PxRigidDynamicLockFlag::Enum)DynamicBody::LockFlag::ROTATION_Y);
+			static_assert(physx::PxRigidDynamicLockFlag::eLOCK_ANGULAR_Z == (physx::PxRigidDynamicLockFlag::Enum)DynamicBody::LockFlag::ROTATION_Z);
 
 			DynamicBody::LockFlagMask PhysXDynamicBody::GetLockFlags()const {
 				PhysXScene::ReadLock lock(Scene());
 				physx::PxRigidDynamicLockFlags flags = operator->()->getRigidDynamicLockFlags();
-				LockFlagMask mask = 0;
-				for (size_t i = 0; i < LOCK_MASK_PAIR_COUNT; i++) {
-					const std::pair<physx::PxRigidDynamicLockFlag::Enum, DynamicBody::LockFlag>& pair = LOCK_MASK_PAIRS[i];
-					if (((physx::PxU8)flags & pair.first) != 0) mask |= LockFlags(pair.second);
-				}
-				return mask;
+				return (LockFlagMask)flags;
 			}
 
 			void PhysXDynamicBody::SetLockFlags(LockFlagMask mask) {
-				physx::PxRigidDynamicLockFlags flags(0);
-				for (size_t i = 0; i < LOCK_MASK_PAIR_COUNT; i++) {
-					const std::pair<physx::PxRigidDynamicLockFlag::Enum, DynamicBody::LockFlag>& pair = LOCK_MASK_PAIRS[i];
-					if ((LockFlags(pair.second) & mask) != 0) flags |= pair.first;
-				}
 				PhysXScene::WriteLock lock(Scene());
-				operator->()->setRigidDynamicLockFlags(flags);
+				operator->()->setRigidDynamicLockFlags((physx::PxRigidDynamicLockFlags)mask);
 			}
 
 			void PhysXDynamicBody::SetPose(const Matrix4& transform) {
