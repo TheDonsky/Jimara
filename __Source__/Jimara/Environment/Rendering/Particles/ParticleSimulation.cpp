55e60d57c009a63dce1838777574f15ecef6ddf1
TheDonsky <nikadon.net@gmail.com>
2022-10-28 21:46:09 +0400

    Document the implementation so far to make it somewhat easier to understand when the future ME wants to inspect or edit something

--- __Source__/Jimara/Environment/Rendering/Particles/ParticleSimulation.cpp
+++ __Source__/Jimara/Environment/Rendering/Particles/ParticleSimulation.cpp
@@ -3,12 +3,20 @@
 
 
 namespace Jimara {
-
 	struct ParticleSimulation::Helpers {
+		/// <summary>
+		/// During the normal Update cycle, Components can add or remove tasks to the particle simulation;
+		/// This is the collection that holds references to the particle simulation tasks per scene and is update immediately on add/remove
+		/// </summary>
 		class TaskSet : public virtual Object {
 		public:
+			/// <summary>
+			/// Adds a task to the collection
+			/// </summary>
+			/// <param name="task"> Task to add </param>
+			/// <returns> 1 if the task is inserted and number of current entries is 1 (otherwise undefined) </returns>
 			inline size_t AddTask(Task* task) {
-				if (task == nullptr) return m_entries.size();
+				if (task == nullptr) return ~size_t(0u);
 				std::unique_lock<std::shared_mutex> lock(m_lock);
 				{
 					decltype(m_entries)::iterator it = m_entries.find(task);
@@ -22,6 +30,11 @@ namespace Jimara {
 				return m_entries.size();
 			}
 
+			/// <summary>
+			/// Removes a task from the simulation
+			/// </summary>
+			/// <param name="task"> Task to remove </param>
+			/// <returns> Number of remaining elements inside the collection </returns>
 			inline size_t RemoveTask(Task* task) {
 				if (task == nullptr) return m_entries.size();
 				std::unique_lock<std::shared_mutex> lock(m_lock);
@@ -39,8 +52,13 @@ namespace Jimara {
 				return m_entries.size();
 			}
 
+			/// <summary>
+			/// Iterates over all tasks within the collection
+			/// </summary>
+			/// <typeparam name="GetTaskFn"> Any function that receives tasks array and count as arguments </typeparam>
+			/// <param name="inspectTasks"></param>
 			template<typename GetTaskFn>
-			inline void GetTasks(const GetTaskFn& inspectTask) {
+			inline void GetTasks(const GetTaskFn& inspectTasks) {
 				std::shared_lock<std::shared_mutex> lock(m_lock);
 				if (m_entries.size() != m_taskList.size()) {
 					m_taskList.clear();
@@ -48,9 +66,8 @@ namespace Jimara {
 						m_taskList.push_back(it->first);
 				}
 				size_t count = m_taskList.size();
-				Task** tasks = m_taskList.data();
-				for (size_t i = 0; i < count; i++)
-					inspectTask(tasks[i]);
+				Task* const* tasks = m_taskList.data();
+				inspectTasks(tasks, count);
 			}
 
 		private:
@@ -63,26 +80,34 @@ namespace Jimara {
 
 
 
+		/// <summary>
+		/// A simple description of a task, alongside it's dependants and number of dependencies
+		/// </summary>
 		struct TaskWithDependencies {
+			// Task reference
 			Reference<Task> task;
-			mutable std::atomic<size_t> dependencies;
+
+			// Number of 'unscheduled' dependencies of the task
+			mutable size_t dependencies;
+
+			// Tasks that depend on this one
 			mutable std::vector<Task*> dependants;
 
 			inline TaskWithDependencies(Task* t = nullptr) 
 				: task(t), dependencies(0u) { }
 			inline TaskWithDependencies(TaskWithDependencies&& other) noexcept 
-				: task(other.task), dependencies(other.dependencies.load()), dependants(std::move(other.dependants)) { }
+				: task(other.task), dependencies(other.dependencies), dependants(std::move(other.dependants)) { }
 			inline TaskWithDependencies& operator=(TaskWithDependencies&& other) noexcept {
 				task = other.task;
-				dependencies = other.dependencies.load();
+				dependencies = other.dependencies;
 				dependants = std::move(other.dependants);
 				return (*this);
 			}
 			inline TaskWithDependencies(const TaskWithDependencies& other) noexcept 
-				: task(other.task), dependencies(other.dependencies.load()), dependants(other.dependants) { }
+				: task(other.task), dependencies(other.dependencies), dependants(other.dependants) { }
 			inline TaskWithDependencies& operator=(const TaskWithDependencies& other) noexcept {
 				task = other.task;
-				dependencies = other.dependencies.load();
+				dependencies = other.dependencies;
 				dependants = other.dependants;
 				return (*this);
 			}
@@ -92,25 +117,35 @@ namespace Jimara {
 
 
 
-
-
+		/// <summary>
+		/// Each graphics synch point, the first step taken by particle simulation takes all tasks from the scene
+		/// and collects them alongside their recursive dependencies inside a single buffer which later steps use and operate on.
+		/// </summary>
 		class TaskCollectionJob : public virtual JobSystem::Job {
 		public:
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="taskSet"> TaskSet of some scene </param>
 			inline TaskCollectionJob(TaskSet* taskSet) : m_taskSet(taskSet) {}
 
+			/// <summary> Virtual destructor </summary>
 			inline virtual ~TaskCollectionJob() {}
 
-			inline TaskBuffer& SchedulingBuffer() { return m_taskBuffer; }
-
-			inline const std::vector<Reference<Task>>& AllTasks()const { return m_allTasks; }
+			/// <summary> Task buffer that is regenerated after each job execution </summary>
+			inline const TaskBuffer& SchedulingBuffer()const { return m_taskBuffer; }
 
 		protected:
+			/// <summary>
+			/// Regenerates the task buffer
+			/// </summary>
 			inline virtual void Execute() override {
 				m_taskBuffer.Clear();
-				m_allTasks.clear();
 
 				// Collect all base jobs:
-				m_taskSet->GetTasks([&](Task* task) { m_taskBuffer.Add(task); });
+				m_taskSet->GetTasks([&](Task* const* tasks, size_t count) {
+					m_taskBuffer.Add(tasks, count);
+					});
 
 				// Add all dependencies to task buffer:
 				for (size_t i = 0; i < m_taskBuffer.Size(); i++) {
@@ -127,17 +162,18 @@ namespace Jimara {
 						if (dep != nullptr) dep->dependants.push_back(task);
 						else m_taskBuffer.Add(&dependency, 1, [&](const TaskWithDependencies* added, size_t) { added->dependants.push_back(task); });
 					}
-					m_allTasks.push_back(task);
 					m_dependencyBuffer.clear();
 				}
 			}
 
+			/// <summary>
+			/// This is the first synch point step and does not have any dependencies
+			/// </summary>
 			inline virtual void CollectDependencies(Callback<Job*>) override {}
 
 		private:
 			const Reference<TaskSet> m_taskSet;
 
-			std::vector<Reference<Task>> m_allTasks;
 			TaskBuffer m_taskBuffer;
 			std::unordered_set<Reference<Task>> m_dependencyBuffer;
 		};
@@ -146,29 +182,47 @@ namespace Jimara {
 
 
 
+		/// <summary>
+		/// Once the tasks are collected by TaskCollectionJob above, they need to be synchronized with the scene state.
+		/// To do that, we have a 'swarm' of SynchJob objects that will collectively invoke Task::Synchronize() for all tasks found by TaskCollectionJob.
+		/// </summary>
 		class SynchJob : public virtual JobSystem::Job {
 		public:
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="collectionJob"> TaskCollectionJob that collects tasks </param>
+			/// <param name="synchJobIndex"> Index of this SynchJob within the 'swarm' </param>
+			/// <param name="synchJobCount"> Number of SynchJob objects within the 'swarm' </param>
 			inline SynchJob(TaskCollectionJob* collectionJob, size_t synchJobIndex, size_t synchJobCount) 
 				: m_collectionJob(collectionJob), m_index(synchJobIndex), m_synchJobCount(synchJobCount) {}
 
+			/// <summary> Virtual destructor </summary>
 			inline virtual ~SynchJob() {}
 
 		protected:
+			/// <summary>
+			/// Each SynchJob within the swarm invokes Synchronize() method for an even subset of the tasks found by the TaskCollectionJob
+			/// </summary>
 			inline virtual void Execute() override {
-				const std::vector<Reference<Task>>& tasks = m_collectionJob->AllTasks();
-				const size_t tasksPerJob = (tasks.size() + m_synchJobCount - 1) / m_synchJobCount;
+				const TaskBuffer& tasks = m_collectionJob->SchedulingBuffer();
+				const size_t tasksPerJob = (tasks.Size() + m_synchJobCount - 1) / m_synchJobCount;
 				
 				const size_t firstTaskIndex = m_index * tasksPerJob;
-				if (firstTaskIndex >= tasks.size()) return;
+				if (firstTaskIndex >= tasks.Size()) return;
 
-				const Reference<Task>* ptr = tasks.data() + firstTaskIndex;
-				const Reference<Task>* const end = tasks.data() + Math::Min(firstTaskIndex + tasksPerJob, tasks.size());
+				const TaskWithDependencies* ptr = tasks.Data() + firstTaskIndex;
+				const TaskWithDependencies* const end = tasks.Data() + Math::Min(firstTaskIndex + tasksPerJob, tasks.Size());
 				while (ptr < end) {
-					(*ptr)->Synchronize();
+					ptr->task->Synchronize();
 					ptr++;
 				}
 			}
 
+			/// <summary>
+			/// SynchJob-s should get executed after TaskCollectionJob
+			/// </summary>
+			/// <param name="addDependency"> TaskCollectionJob is reported through this callback </param>
 			inline virtual void CollectDependencies(Callback<Job*> addDependency) override {
 				addDependency(m_collectionJob);
 			}
@@ -182,28 +236,68 @@ namespace Jimara {
 
 
 
+
+		/// <summary>
+		/// __TODO__: Implement this crap!
+		/// </summary>
+		class SimulationKernel : public virtual JobSystem::Job {
+		public:
+			/// __TODO__: Implement this crap!
+		};
+
+		/// <summary>
+		/// Particle simulation is done on graphics render job system in several simulation steps;
+		/// Each simulation step comes right after the one before it and consists of multiple unrelated simulation kernel executions that are reported as task dependencies;
+		/// RenderSchedulingJob is a synch point job responsible for creating and managing simulation steps.
+		/// </summary>
 		class SimulationStep : public virtual JobSystem::Job {
 		public:
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="previous"> Previous simulation step (nullptr for the first one) </param>
 			inline SimulationStep(SimulationStep* previous) 
 				: m_previous(previous) {}
 
+			/// <summary>
+			/// Virtual destructor
+			/// </summary>
 			inline virtual ~SimulationStep() {}
 
+			/// <summary>
+			/// Removes tasks from the internal task list used for scheduling (invoked by RenderSchedulingJob)
+			/// </summary>
 			inline void Clear() { m_tasks.clear(); }
 
+			/// <summary>
+			/// Adds task to the internal task list used for scheduling (invoked by RenderSchedulingJob)
+			/// </summary>
+			/// <param name="task"> Task to include </param>
 			inline void AddTask(Task* task) { m_tasks.push_back(task); }
 
+			/// <summary>
+			/// Analizes internal task list and creates or populates simulation kernels which are later reported as dependencies;
+			/// Executed on a separate thread right after the internal task list is filled by RenderSchedulingJob.
+			/// </summary>
 			inline void ScheduleKernelSubtasks() {
 				// __TODO__: Implement this crap!
 			}
 
 		protected:
+			/// <summary>
+			/// Cleans up the task list, but could also do nothing, as far as I'm concerned;
+			/// Invoked by the graphics render job system, right after the previous simulation step and the kernels generated by ScheduleKernelSubtasks() call
+			/// </summary>
 			inline virtual void Execute() override {
 				// __TODO__: Implement this crap!
+				m_tasks.clear();
 			}
 
+			/// <summary>
+			/// Reports the kernels scheduled by ScheduleKernelSubtasks as dependencies
+			/// </summary>
+			/// <param name="addDependency"> Kernel jobs are reported through this callback </param>
 			inline virtual void CollectDependencies(Callback<Job*> addDependency) override {
-				addDependency(m_previous);
 				// __TODO__: Implement this crap!
 			}
 
@@ -212,16 +306,46 @@ namespace Jimara {
 			std::vector<Reference<Task>> m_tasks;
 		};
 
+
+
+
+
+		/// <summary>
+		/// After TaskCollectionJob collects all tasks and their dependencies into a nice buffer,
+		/// SynchJob swarm and RenderSchedulingJob are executed;
+		/// RenderSchedulingJob, specifically, is responsible for creating a job graph that will be executed on the render job system;
+		/// As discussed in the description of SimulationStep, this one divides tasks into interdependent simulation steps and adds those steps to the render job system.
+		/// </summary>
 		class RenderSchedulingJob : public virtual JobSystem::Job {
 		public:
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="collectionJob"> Task collection job after which the RenderSchedulingJob is executed </param>
+			/// <param name="context"> Scene context </param>
 			inline RenderSchedulingJob(TaskCollectionJob* collectionJob, SceneContext* context) 
 				: m_collectionJob(collectionJob), m_context(context) {}
 
-			inline virtual ~RenderSchedulingJob() {}
+			/// <summary>
+			/// Destructor;
+			/// Removes all simulation steps from the render job.
+			/// </summary>
+			inline virtual ~RenderSchedulingJob() {
+				while (!m_simulationSteps.empty()) {
+					m_context->Graphics()->RenderJobs().Remove(m_simulationSteps.back());
+					m_simulationSteps.pop_back();
+				}
+			}
 
 		protected:
+			/// <summary>
+			/// Builds the sequence of simulation steps from the scheduling buffer constructed by TaskCollectionJob;
+			/// For further clarity, the tasks that have no dependencies are put in the firts simulation step; 
+			/// Ones dependent only on the tasks from the firts step are combined into the second step;
+			/// Tasks that depend only on the ones from the first two steps are put into the third one and so on... 
+			/// </summary>
 			inline virtual void Execute() override {
-				TaskBuffer& taskBuffer = m_collectionJob->SchedulingBuffer();
+				const TaskBuffer& taskBuffer = m_collectionJob->SchedulingBuffer();
 				m_stepTaskBuffer.clear();
 				m_stepTaskBackBuffer.clear();
 				size_t numSimulationSteps = 0u;
@@ -313,6 +437,10 @@ namespace Jimara {
 				}
 			}
 
+			/// <summary>
+			/// Reports TaskCollectionJob as it's only dependency
+			/// </summary>
+			/// <param name="addDependency"> TaskCollectionJob is reported through this callback </param>
 			inline virtual void CollectDependencies(Callback<Job*> addDependency) override {
 				addDependency(m_collectionJob);
 			}
@@ -336,16 +464,37 @@ namespace Jimara {
 
 
 
+		/// <summary>
+		/// Particle simulation is created per scene and consists of these elements:
+		/// 0. TaskSet that contains all manually registered tasks within the simulation system;
+		/// 1. TaskCollectionJob that reads TaskSet, extracts all dependencies recursively and provides the buffer to the other synch point jobs;
+		/// 2. A 'swarm' of SynchJob-s that execute after TaskCollectionJob on the graphics synch point and synchronize all the tasks found by it;
+		/// 3. RenderSchedulingJob that executes after TaskCollectionJob alongside SynchJob-s and constructs a chain of simulation steps for the graphics render job system;
+		/// 4. Multiple SimulationStep-s generated and managed internally by RenderSchedulingJob that execute after each other on the graphics render job system;
+		/// 5. SimulationKernel objects created and managed internally by each SimulationStep per individual simulation kernel that execute right before each SimulationStep.
+		/// </summary>
 		class Simulation : public virtual ObjectCache<Reference<SceneContext>>::StoredObject {
 		public:
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="context"> Scene context </param>
 			inline Simulation(SceneContext* context) : m_context(context) {
 				assert(m_context != nullptr);
 			}
 
+			/// <summary>
+			/// Virtual destructor
+			/// </summary>
 			inline virtual ~Simulation() {
 				RemoveTask(nullptr);
+				RemoveAllJobs();
 			}
 
+			/// <summary>
+			/// Adds simulation task and if it's the first one, creates and activates the synch point jobs
+			/// </summary>
+			/// <param name="task"> Task to add to the system </param>
 			inline void AddTask(Task* task) {
 				std::unique_lock<std::mutex> lock(m_taskLock);
 				size_t taskCount = m_taskSet->AddTask(task);
@@ -370,19 +519,15 @@ namespace Jimara {
 				}
 			}
 
+			/// <summary>
+			/// Removes a task from the system and Destroys all synch point jobs if the task set becomes empty
+			/// </summary>
+			/// <param name="task"></param>
 			inline void RemoveTask(Task* task) {
 				std::unique_lock<std::mutex> lock(m_taskLock);
-				size_t taskCount = m_taskSet->AddTask(task);
-				if (taskCount != 0u) return;
-
-				RemoveSchedulingJob();
-				RemoveSynchJobs();
-				if (m_taskCollectionJob != nullptr) {
-					m_context->Graphics()->SynchPointJobs().Remove(m_taskCollectionJob);
-					m_taskCollectionJob = nullptr;
-				}
-
-				m_context->EraseDataObject(this);
+				size_t taskCount = m_taskSet->RemoveTask(task);
+				if (taskCount == 0u)
+					RemoveAllJobs();
 			}
 
 		private:
@@ -406,8 +551,21 @@ namespace Jimara {
 				m_context->Graphics()->SynchPointJobs().Remove(m_schedulingJob);
 				m_schedulingJob = nullptr;
 			}
+
+			inline void RemoveAllJobs() {
+				RemoveSchedulingJob();
+				RemoveSynchJobs();
+				if (m_taskCollectionJob != nullptr) {
+					m_context->Graphics()->SynchPointJobs().Remove(m_taskCollectionJob);
+					m_taskCollectionJob = nullptr;
+				}
+				m_context->EraseDataObject(this);
+			}
 		};
 
+		/// <summary>
+		/// Cache of Simulation instances per SceneContext
+		/// </summary>
 		class Cache : public virtual ObjectCache<Reference<SceneContext>> {
 		public:
 			inline static Reference<Simulation> GetSimulation(SceneContext* context) {
