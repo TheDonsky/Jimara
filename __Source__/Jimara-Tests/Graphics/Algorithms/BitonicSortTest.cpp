d83a491d025d8385bd9f733efec2b756b45124ff
TheDonsky <nikadon.net@gmail.com>
2022-11-08 12:01:11 +0400

    Change kernel group size and create placeholders for bitonic sort tests

---
+++ __Source__/Jimara-Tests/Graphics/Algorithms/BitonicSortTest.cpp
@@ -0,0 +1,71 @@
+#include "../../GtestHeaders.h"
+#include "OS/Logging/StreamLogger.h"
+#include "Core/Stopwatch.h"
+#include "Environment/Rendering/Algorithms/BitonicSort/BitonicSortKernel.h"
+
+
+namespace Jimara {
+	namespace {
+		static const constexpr uint32_t BLOCK_SIZE = 512u;
+	}
+
+	TEST(BitonicSortTest, AlreadySorted_SingleStep_PowerOf2) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, AlreadySorted_SingleStep_RandomSize) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, RandomFloats_SingleStep_PowerOf2) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, RandomFloats_GroupsharedStep_RandomSize) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, AlreadySorted_WithGroupsharedStep_PowerOf2) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, AlreadySorted_WithGroupsharedStep_RandomSize) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, RandomFloats_WithGroupsharedStep_PowerOf2) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, RandomFloats_WithGroupsharedStep_RandomSize) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, AlreadySorted_GroupsharedOnly_PowerOf2) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, AlreadySorted_GroupsharedOnly_RandomSize) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, RandomFloats_GroupsharedOnly_PowerOf2) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+
+	TEST(BitonicSortTest, RandomFloats_GroupsharedOnly_RandomSize) {
+		// __TODO__: Implement this crap!
+		ASSERT_FALSE(true);
+	}
+}
