d3b00bc112dd68851e3624dd71fc70a30fad6c62
TheDonsky <nikadon.net@gmail.com>
2022-11-14 20:56:41 +0400

    Ok... Style..

--- __Source__/Jimara/Components/GraphicsObjects/MeshRenderer.cpp
+++ __Source__/Jimara/Components/GraphicsObjects/MeshRenderer.cpp
@@ -325,8 +325,7 @@ namespace Jimara {
 			}
 			m_pipelineDescriptor = nullptr;
 		}
-		if (ActiveInHeirarchy() && Mesh() != nullptr && MaterialInstance() != nullptr) {
-			if (GetTransfrom() == nullptr) return;
+		if (ActiveInHeirarchy() && Mesh() != nullptr && MaterialInstance() != nullptr && GetTransfrom() != nullptr) {
 			const TriMeshRenderer::Configuration desc(this);
 			Reference<MeshRenderPipelineDescriptor> descriptor;
 			if (IsInstanced()) descriptor = MeshRenderPipelineDescriptor::Instancer::GetDescriptor(desc);
