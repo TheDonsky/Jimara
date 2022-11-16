e13bd90bacede27af419053e36ebbba622c52791
TheDonsky <nikadon.net@gmail.com>
2022-11-16 14:41:24 +0400

    Expand Random library

--- __Source__/Jimara/Math/Random.cpp
+++ __Source__/Jimara/Math/Random.cpp
@@ -20,5 +20,67 @@ namespace Jimara {
 			thread_local std::mt19937 generator(GlobalRNG());
 			return generator;
 		}
+
+		unsigned int Uint() {
+			return ThreadRNG()(); 
+		}
+
+		unsigned int Uint(unsigned int minimum, unsigned int maximum) {
+			return Uint() % (maximum - minimum) + minimum; 
+		}
+
+		size_t Size() {
+			size_t result;
+			static_assert(sizeof(size_t) >= sizeof(unsigned int));
+			static_assert((sizeof(size_t) % sizeof(unsigned int)) == 0u);
+			const constexpr size_t count = (sizeof(size_t) / sizeof(unsigned int));
+			unsigned int* uints = reinterpret_cast<unsigned int*>(&result);
+			for (size_t i = 0; i < count; i++)
+				uints[i] = Uint();
+			return result;
+		}
+
+		size_t Size(size_t minimum, size_t maximum) {
+			return Size() % (maximum - minimum) + minimum;
+		}
+
+		int Int() {
+			return static_cast<float>(ThreadRNG()());
+		}
+
+		int Int(int minimum, int maximum) {
+			return Int() % (maximum - minimum) + minimum;
+		}
+
+		float Float() {
+			return float(Uint()) / float(~uint32_t(0));
+		}
+
+		float Float(float minimum, float maximum) {
+			return Float() * (maximum - minimum) + minimum;
+		}
+
+		bool Bool() {
+			return (Uint() & 1u) == 1u;
+		}
+
+		bool Bool(float chance) {
+			return Float() <= chance;
+		}
+
+		Vector2 PointOnCircle() {
+			float theta = (2.0f * Math::Pi() * Float());
+			return Vector2(std::cos(theta), std::sin(theta));
+		}
+
+		Vector3 PointOnSphere() {
+			float theta = (2.0f * Math::Pi() * Float());
+			float phi = (1.0f - (2.0f * Float()));
+			float sinPhi = std::sin(phi);
+			return Vector3(
+				(sinPhi * std::cos(theta)),
+				(sinPhi * std::sin(theta)),
+				std::cos(phi));
+		}
 	}
 }
--- __Source__/Jimara/Math/Random.h
+++ __Source__/Jimara/Math/Random.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../Core/JimaraApi.h"
+#include "Math.h"
 #include <random>
 
 
@@ -7,5 +8,65 @@ namespace Jimara {
 	namespace Random {
 		/// <summary> Thread-local random number generator </summary>
 		JIMARA_API std::mt19937& ThreadRNG();
+
+		/// <summary> Random unsigned integer </summary>
+		JIMARA_API unsigned int Uint();
+
+		/// <summary>
+		/// Generates a random unsigned integer in given range
+		/// </summary>
+		/// <param name="minimum"> Min value (inclusive) </param>
+		/// <param name="maximum"> Max value (exclusive) </param>
+		/// <returns> Random unsigned integer between minimum and maximum </returns>
+		JIMARA_API unsigned int Uint(unsigned int minimum, unsigned int maximum);
+
+		/// <summary> Random size_t </summary>
+		JIMARA_API size_t Size();
+
+		/// <summary>
+		/// Generates a random size_t in given range
+		/// </summary>
+		/// <param name="minimum"> Min value (inclusive) </param>
+		/// <param name="maximum"> Max value (exclusive) </param>
+		/// <returns> Random unsigned integer between minimum and maximum </returns>
+		JIMARA_API size_t Size(size_t minimum, size_t maximum);
+
+		/// <summary> Random signed integer </summary>
+		JIMARA_API int Int();
+
+		/// <summary>
+		/// Generates a random signed integer in given range
+		/// </summary>
+		/// <param name="minimum"> Min value (inclusive) </param>
+		/// <param name="maximum"> Max value (exclusive) </param>
+		/// <returns> Random unsigned integer between minimum and maximum </returns>
+		JIMARA_API int Int(int minimum, int maximum);
+
+		/// <summary> Generates a random floating point in (0-1) range </summary>
+		JIMARA_API float Float();
+
+		/// <summary> 
+		/// Generates a random floating point in given range 
+		/// </summary>
+		/// <param name="minimum"> Min value </param>
+		/// <param name="maximum"> Max value </param>
+		/// <returns> Random floating point between minimum and maximum </returns>
+		JIMARA_API float Float(float minimum, float maximum);
+
+		/// <summary> Random boolean value (with 50:50 chance) </summary>
+		JIMARA_API bool Bool();
+
+		/// <summary> 
+		/// Weighted random boolean value 
+		/// </summary>
+		/// <param name="chance"> Probability of true result </param>
+		/// <returns> Random boolean value </returns>
+		JIMARA_API bool Bool(float chance);
+
+		/// <summary> Random 2d direction (a point on unit circle) </summary>
+		JIMARA_API Vector2 PointOnCircle();
+
+		/// <summary> Random 3d direction (a point on unit sphere) </summary>
+		JIMARA_API Vector3 PointOnSphere();
 	}
 }
