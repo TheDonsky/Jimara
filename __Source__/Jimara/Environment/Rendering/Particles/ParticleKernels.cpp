a7271c89c36e61502dd5d44dad1f6e00c8c0bf5e
TheDonsky <nikadon.net@gmail.com>
2022-12-01 22:17:17 +0400

    Clean the interfaces up a bit

--- __Source__/Jimara/Environment/Rendering/Particles/ParticleKernels.cpp
+++ __Source__/Jimara/Environment/Rendering/Particles/ParticleKernels.cpp
@@ -4,72 +4,60 @@
 
 namespace Jimara {
 	namespace {
-		template<typename KernelType>
-		struct ParticleKernel_InstanceAttribute : public virtual Object {
-			const Reference<const KernelType> kernelInstance;
-			const TypeId typeId;
+		template<typename TaskType>
+		struct ParticleKernel_FactorySetData : public virtual Object {
+			typedef typename TaskType::Factory Factory;
+			typedef typename TaskType::Factory::Set FactorySet;
 
-			inline ParticleKernel_InstanceAttribute(const KernelType* kernel, TypeId type)
-				: kernelInstance(kernel), typeId(type) {}
+			std::vector<Reference<const Factory>> factories;
+			std::unordered_map<std::type_index, size_t> indexByType;
 
-			struct Set : public virtual Object {
-				std::vector<Reference<const ParticleKernel_InstanceAttribute>> attributes;
-				std::unordered_map<const KernelType*, size_t> indexByKernel;
-				std::unordered_map<TypeId, size_t> indexByType;
-			};
-
-			template<typename SetType, typename SetCreateFn>
-			inline static Reference<SetType> All(const SetCreateFn& createSet) {
+			template<typename SetCreateFn>
+			inline static Reference<FactorySet> All(const SetCreateFn& createSet) {
 				static std::mutex instanceLock;
-				static Reference<SetType> instance;
+				static Reference<FactorySet> instance;
 				static bool subscribedToTypeRegistry = false;
 
-				static auto onRegisteredTypeSetChanged = []() {
-					std::unique_lock<std::mutex> lock(instanceLock);
-					instance = nullptr;
-				};
-
-				std::unique_lock<std::mutex> lock(instanceLock);
-
 				// If old instance is still valid, we return it:
-				Reference<SetType> all = instance;
+				Reference<FactorySet> all = instance;
 				if (all != nullptr) return all;
 
 				// Create new set:
-				const Reference<Set> set = Object::Instantiate<Set>();
+				const Reference<ParticleKernel_FactorySetData> set = Object::Instantiate<ParticleKernel_FactorySetData>();
 				all = instance = createSet(set);
-				
-				// Find all attributes:
+
+				// Find all factories:
 				{
 					const Reference<RegisteredTypeSet> currentTypes = RegisteredTypeSet::Current();
 					std::unordered_set<TypeId> registeredTypes;
 					for (size_t i = 0; i < currentTypes->Size(); i++) {
 						auto checkAttribute = [&](const Object* attribute) {
-							const ParticleKernel_InstanceAttribute* instanceAttribute = dynamic_cast<const ParticleKernel_InstanceAttribute*>(attribute);
-							if (instanceAttribute == nullptr || instanceAttribute->kernelInstance == nullptr) return;
-							if (registeredTypes.find(instanceAttribute->typeId) != registeredTypes.end()) return;
-							registeredTypes.insert(instanceAttribute->typeId);
-							set->attributes.push_back(instanceAttribute);
+							const Factory* instanceAttribute = dynamic_cast<const Factory*>(attribute);
+							if (instanceAttribute == nullptr) return;
+							if (registeredTypes.find(instanceAttribute->TaskType()) != registeredTypes.end()) return;
+							registeredTypes.insert(instanceAttribute->TaskType());
+							set->factories.push_back(instanceAttribute);
 						};
 						currentTypes->At(i).GetAttributes(Callback<const Object*>::FromCall(&checkAttribute));
 					}
 				}
-				
+
 				// Sort attributes by name:
-				std::sort(set->attributes.begin(), set->attributes.end(),
-					[](const Reference<const ParticleKernel_InstanceAttribute>& a, const Reference<const ParticleKernel_InstanceAttribute>& b) {
-						return a->typeId.Name() < b->typeId.Name();
+				std::sort(set->factories.begin(), set->factories.end(),
+					[](const Reference<const Factory>& a, const Reference<const Factory>& b) {
+						return a->TaskType().Name() < b->TaskType().Name();
 					});
 
-				// Fill in indexes:
-				for (size_t i = 0; i < set->attributes.size(); i++) {
-					const ParticleKernel_InstanceAttribute* attribute = set->attributes[i];
-					set->indexByKernel[attribute->kernelInstance] = i;
-					set->indexByType[attribute->typeId] = i;
-				}
+				// Fill in index:
+				for (size_t i = 0; i < set->factories.size(); i++)
+					set->indexByType[set->factories[i]->TaskType().TypeIndex()] = i;
 
 				// Make sure we are subscribed to the OnRegisteredTypeSetChanged event:
 				if (!subscribedToTypeRegistry) {
+					static auto onRegisteredTypeSetChanged = []() {
+						std::unique_lock<std::mutex> lock(instanceLock);
+						instance = nullptr;
+					};
 					TypeId::OnRegisteredTypeSetChanged() += Callback<>::FromCall(&onRegisteredTypeSetChanged);
 					subscribedToTypeRegistry = true;
 				}
@@ -80,7 +68,7 @@ namespace Jimara {
 		};
 	}
 
-	void ParticleInitializationKernel::Task::SetBuffers(ParticleBuffers* buffers) {
+	void ParticleInitializationTask::SetBuffers(ParticleBuffers* buffers) {
 		std::unique_lock<SpinLock> lock(m_dependencyLock);
 		m_dependencies.Clear();
 		auto findBuffer = [&](const ParticleBuffers::BufferId* bufferId) -> Graphics::BindlessSet<Graphics::ArrayBuffer>::Binding* {
@@ -96,7 +84,7 @@ namespace Jimara {
 			BufferSearchFn::FromCall(&findBuffer));
 	}
 
-	void ParticleInitializationKernel::Task::GetDependencies(const Callback<GraphicsSimulation::Task*>& recordDependency)const {
+	void ParticleInitializationTask::GetDependencies(const Callback<GraphicsSimulation::Task*>& recordDependency)const {
 		std::unique_lock<SpinLock> lock(m_dependencyLock);
 		const Reference<GraphicsSimulation::Task>* taskPtr = m_dependencies.Data();
 		const Reference<GraphicsSimulation::Task>* const end = taskPtr + m_dependencies.Size();
@@ -106,92 +94,57 @@ namespace Jimara {
 		}
 	}
 
-	size_t ParticleInitializationKernel::Set::Size()const {
-		return dynamic_cast<const ParticleKernel_InstanceAttribute<ParticleInitializationKernel>::Set*>(m_data.operator->())->attributes.size();
-	}
-
-	std::pair<const ParticleInitializationKernel*, TypeId> ParticleInitializationKernel::Set::operator[](size_t index)const {
-		const ParticleKernel_InstanceAttribute<ParticleInitializationKernel>* entry =
-			dynamic_cast<const ParticleKernel_InstanceAttribute<ParticleInitializationKernel>::Set*>(m_data.operator->())->attributes[index];
-		return std::make_pair(entry->kernelInstance.operator->(), entry->typeId);
-	}
-
-	size_t ParticleInitializationKernel::Set::operator[](const ParticleInitializationKernel* kernel)const {
-		const auto data = dynamic_cast<const ParticleKernel_InstanceAttribute<ParticleInitializationKernel>::Set*>(m_data.operator->());
-		const auto it = data->indexByKernel.find(kernel);
-		return (it == data->indexByKernel.end()) ? InvalidIndex() : it->second;
-	}
-
-	size_t ParticleInitializationKernel::Set::operator[](const TypeId& type)const {
-		const auto data = dynamic_cast<const ParticleKernel_InstanceAttribute<ParticleInitializationKernel>::Set*>(m_data.operator->());
-		const auto it = data->indexByType.find(type);
-		return (it == data->indexByType.end()) ? InvalidIndex() : it->second;
-	}
-
-	Reference<ParticleInitializationKernel::Set> ParticleInitializationKernel::All() {
+	Reference<ParticleInitializationTask::Factory::Set> ParticleInitializationTask::Factory::Set::Current() {
 		auto createInstance = [](const Object* data) -> Reference<Set> {
-			const Reference<ParticleInitializationKernel::Set> instance = new ParticleInitializationKernel::Set(data);
+			const Reference<ParticleInitializationTask::Factory::Set> instance = new ParticleInitializationTask::Factory::Set(data);
 			instance->ReleaseRef();
 			return instance;
 		};
-		return ParticleKernel_InstanceAttribute<ParticleInitializationKernel>::All<Set>(createInstance);
+		return ParticleKernel_FactorySetData<ParticleInitializationTask>::All(createInstance);
 	}
 
-	Reference<Object> ParticleInitializationKernel::CreateInstanceAttribute(const ParticleInitializationKernel* instance, TypeId type) {
-		return Object::Instantiate<ParticleKernel_InstanceAttribute<ParticleInitializationKernel>>(instance, type);
+	size_t ParticleInitializationTask::Factory::Set::Size()const {
+		return dynamic_cast<const ParticleKernel_FactorySetData<ParticleInitializationTask>*>(m_data.operator->())->factories.size();
 	}
 
-	Reference<ParticleTimestepKernel::Task> ParticleTimestepKernel::CreateTask(GraphicsSimulation::Task* spawningStep) {
-		if (spawningStep == nullptr) return nullptr;
-		Reference<ParticleTimestepKernel::Task> task = CreateTask(spawningStep->Context());
-		if (task == nullptr) {
-			spawningStep->Context()->Log()->Error("ParticleTimestepKernel::CreateTask - CreateTask(Context) failed! [File: ", __FILE__, "; Line: ", __LINE__, "]");
-			return nullptr;
-		}
-		if (task->m_spawningStep != nullptr) {
-			spawningStep->Context()->Log()->Error("ParticleTimestepKernel::CreateTask - CreateTask(Context) returned an existing instance! [File: ", __FILE__, "; Line: ", __LINE__, "]");
-			return nullptr;
-		}
-		task->m_spawningStep = spawningStep;
-		return task;
+	const ParticleInitializationTask::Factory* ParticleInitializationTask::Factory::Set::operator[](size_t index)const {
+		return dynamic_cast<const ParticleKernel_FactorySetData<ParticleInitializationTask>*>(m_data.operator->())->factories[index];
 	}
 
-	void ParticleTimestepKernel::Task::GetDependencies(const Callback<GraphicsSimulation::Task*>& recordDependency)const {
+	const ParticleInitializationTask::Factory* ParticleInitializationTask::Factory::Set::operator[](const std::type_index& taskType)const {
+		const auto data = dynamic_cast<const ParticleKernel_FactorySetData<ParticleInitializationTask>*>(m_data.operator->());
+		const auto it = data->indexByType.find(taskType);
+		return (it == data->indexByType.end()) ? nullptr : data->factories[it->second];
+	}
+
+
+
+
+
+	void ParticleTimestepTask::GetDependencies(const Callback<GraphicsSimulation::Task*>& recordDependency)const {
 		recordDependency(m_spawningStep);
 	}
 
-	size_t ParticleTimestepKernel::Set::Size()const {
-		return dynamic_cast<const ParticleKernel_InstanceAttribute<ParticleTimestepKernel>::Set*>(m_data.operator->())->attributes.size();
-	}
-
-	std::pair<const ParticleTimestepKernel*, TypeId> ParticleTimestepKernel::Set::operator[](size_t index)const {
-		const ParticleKernel_InstanceAttribute<ParticleTimestepKernel>* entry =
-			dynamic_cast<const ParticleKernel_InstanceAttribute<ParticleTimestepKernel>::Set*>(m_data.operator->())->attributes[index];
-		return std::make_pair(entry->kernelInstance.operator->(), entry->typeId);
-	}
-
-	size_t ParticleTimestepKernel::Set::operator[](const ParticleTimestepKernel* kernel)const {
-		const auto data = dynamic_cast<const ParticleKernel_InstanceAttribute<ParticleTimestepKernel>::Set*>(m_data.operator->());
-		const auto it = data->indexByKernel.find(kernel);
-		return (it == data->indexByKernel.end()) ? InvalidIndex() : it->second;
-	}
-
-	size_t ParticleTimestepKernel::Set::operator[](const TypeId& type)const {
-		const auto data = dynamic_cast<const ParticleKernel_InstanceAttribute<ParticleTimestepKernel>::Set*>(m_data.operator->());
-		const auto it = data->indexByType.find(type);
-		return (it == data->indexByType.end()) ? InvalidIndex() : it->second;
-	}
-
-	Reference<ParticleTimestepKernel::Set> ParticleTimestepKernel::All() {
+	Reference<ParticleTimestepTask::Factory::Set> ParticleTimestepTask::Factory::Set::Current() {
 		auto createInstance = [](const Object* data) -> Reference<Set> {
-			const Reference<ParticleTimestepKernel::Set> instance = new ParticleTimestepKernel::Set(data);
+			const Reference<ParticleTimestepTask::Factory::Set> instance = new ParticleTimestepTask::Factory::Set(data);
 			instance->ReleaseRef();
 			return instance;
 		};
-		return ParticleKernel_InstanceAttribute<ParticleTimestepKernel>::All<Set>(createInstance);
+		return ParticleKernel_FactorySetData<ParticleTimestepTask>::All(createInstance);
 	}
 
-	Reference<Object> ParticleTimestepKernel::CreateInstanceAttribute(const ParticleTimestepKernel* instance, TypeId type) {
-		return Object::Instantiate<ParticleKernel_InstanceAttribute<ParticleTimestepKernel>>(instance, type);
+	size_t ParticleTimestepTask::Factory::Set::Size()const {
+		return dynamic_cast<const ParticleKernel_FactorySetData<ParticleTimestepTask>*>(m_data.operator->())->factories.size();
+	}
+
+	const ParticleTimestepTask::Factory* ParticleTimestepTask::Factory::Set::operator[](size_t index)const {
+		return dynamic_cast<const ParticleKernel_FactorySetData<ParticleTimestepTask>*>(m_data.operator->())->factories[index];
+	}
+
+	const ParticleTimestepTask::Factory* ParticleTimestepTask::Factory::Set::operator[](const std::type_index& taskType)const {
+		const auto data = dynamic_cast<const ParticleKernel_FactorySetData<ParticleTimestepTask>*>(m_data.operator->());
+		const auto it = data->indexByType.find(taskType);
+		return (it == data->indexByType.end()) ? nullptr : data->factories[it->second];
 	}
 }
--- __Source__/Jimara/Environment/Rendering/Particles/ParticleKernels.h
+++ __Source__/Jimara/Environment/Rendering/Particles/ParticleKernels.h
@@ -3,109 +3,138 @@
 
 
 namespace Jimara {
-	class JIMARA_API ParticleInitializationKernel : public virtual GraphicsSimulation::Kernel {
+	class JIMARA_API ParticleInitializationTask : public virtual GraphicsSimulation::Task {
 	public:
-		class JIMARA_API Task : public virtual GraphicsSimulation::Task {
-		public:
-			typedef Function<Graphics::BindlessSet<Graphics::ArrayBuffer>::Binding*, const ParticleBuffers::BufferId*> BufferSearchFn;
+		class Factory;
 
-			void SetBuffers(ParticleBuffers* buffers);
+		typedef Function<Graphics::BindlessSet<Graphics::ArrayBuffer>::Binding*, const ParticleBuffers::BufferId*> BufferSearchFn;
 
-			virtual void GetDependencies(const Callback<GraphicsSimulation::Task*>& recordDependency)const override;
+		void SetBuffers(ParticleBuffers* buffers);
 
-		protected:
-			virtual void SetBuffers(
-				Graphics::BindlessSet<Graphics::ArrayBuffer>::Binding* indirectionBuffer,
-				Graphics::BindlessSet<Graphics::ArrayBuffer>::Binding* liveParticleCountBuffer,
-				const BufferSearchFn& findBuffer) = 0;
-
-		private:
-			mutable SpinLock m_dependencyLock;
-			Stacktor<Reference<GraphicsSimulation::Task>, 4u> m_dependencies;
-		};
-
-		virtual Reference<Task> CreateTask(SceneContext* context)const = 0;
-
-		template<typename KernelType>
-		Reference<Object> CreateInstanceAttribute(const KernelType* instance) {
-			return CreateInstanceAttribute(instance, TypeId::Of<KernelType>());
-		}
-
-		class JIMARA_API Set : public virtual Object {
-		public:
-			inline virtual ~Set() {}
-
-			size_t Size()const;
-
-			std::pair<const ParticleInitializationKernel*, TypeId> operator[](size_t index)const;
-
-			const constexpr size_t InvalidIndex()const { return ~size_t(0u); }
-
-			size_t operator[](const ParticleInitializationKernel* kernel)const;
-
-			size_t operator[](const TypeId& type)const;
-
-		private:
-			const Reference<const Object> m_data;
-
-			inline Set(const Object* data) : m_data(data) {}
-			friend class ParticleInitializationKernel;
-		};
-
-		static Reference<Set> All();
-
-	private:
-		Reference<Object> CreateInstanceAttribute(const ParticleInitializationKernel* instance, TypeId type);
-	};
-
-	class JIMARA_API ParticleTimestepKernel : public virtual GraphicsSimulation::Kernel {
-	public:
-		class JIMARA_API Task : public virtual GraphicsSimulation::Task {
-		public:
-			virtual void SetBuffers(ParticleBuffers* buffers) = 0;
-
-			virtual void GetDependencies(const Callback<GraphicsSimulation::Task*>& recordDependency)const override;
-
-		private:
-			Reference<GraphicsSimulation::Task> m_spawningStep;
-			friend class ParticleTimestepKernel;
-		};
-
-		template<typename KernelType>
-		Reference<Object> CreateInstanceAttribute(const KernelType* instance) {
-			return CreateInstanceAttribute(instance, TypeId::Of<KernelType>());
-		}
-
-		Reference<Task> CreateTask(GraphicsSimulation::Task* spawningStep);
+		virtual void GetDependencies(const Callback<GraphicsSimulation::Task*>& recordDependency)const override;
 
 	protected:
-		virtual Reference<Task> CreateTask(SceneContext* context)const = 0;
-
-	public:
-		class JIMARA_API Set : public virtual Object {
-		public:
-			inline virtual ~Set() {}
-
-			size_t Size()const;
-
-			std::pair<const ParticleTimestepKernel*, TypeId> operator[](size_t index)const;
-
-			const constexpr size_t InvalidIndex()const { return ~size_t(0u); }
-
-			size_t operator[](const ParticleTimestepKernel* kernel)const;
-
-			size_t operator[](const TypeId& type)const;
-
-		private:
-			const Reference<const Object> m_data;
-
-			inline Set(const Object* data) : m_data(data) {}
-			friend class ParticleTimestepKernel;
-		};
-
-		static Reference<Set> All();
+		virtual void SetBuffers(
+			Graphics::BindlessSet<Graphics::ArrayBuffer>::Binding* indirectionBuffer,
+			Graphics::BindlessSet<Graphics::ArrayBuffer>::Binding* liveParticleCountBuffer,
+			const BufferSearchFn& findBuffer) = 0;
 
 	private:
-		Reference<Object> CreateInstanceAttribute(const ParticleTimestepKernel* instance, TypeId type);
+		mutable SpinLock m_dependencyLock;
+		Stacktor<Reference<GraphicsSimulation::Task>, 4u> m_dependencies;
+	};
+
+	class JIMARA_API ParticleInitializationTask::Factory : public virtual Object {
+	public:
+		class Set;
+
+		template<typename TaskType> 
+		inline static Reference<Factory> Of() {
+			static const CreateFn createFn = [](SceneContext* context) -> Reference<ParticleInitializationTask> { 
+				if (context == nullptr) return nullptr;
+				const Reference<TaskType> task = Object::Instantiate<TaskType>(context);
+				return Reference<ParticleInitializationTask>(task.operator->());
+			};
+			const Reference<Factory> instance = new Factory(createFn, TypeId::Of<TaskType>());
+			instance->ReleaseRef();
+			return instance;
+		}
+
+		inline virtual ~Factory() {}
+
+		inline Reference<ParticleInitializationTask> CreateTask(SceneContext* context) const { return m_createFn(context); }
+
+		inline TypeId TaskType()const { return m_taskType; }
+
+	private:
+		typedef Reference<ParticleInitializationTask>(*CreateFn)(SceneContext*);
+		const CreateFn m_createFn;
+		const TypeId m_taskType;
+
+		inline Factory(const CreateFn& createFn, const TypeId& taskType) : m_createFn(createFn), m_taskType(taskType) {}
+	};
+
+	class JIMARA_API ParticleInitializationTask::Factory::Set : public virtual Object {
+	public:
+		static Reference<Set> Current();
+
+		inline virtual ~Set() {}
+
+		size_t Size()const;
+
+		const Factory* operator[](size_t index)const;
+
+		const Factory* operator[](const std::type_index& taskType)const;
+
+	private:
+		const Reference<const Object> m_data;
+
+		inline Set(const Object* data) : m_data(data) {}
+	};
+
+
+
+
+
+	class JIMARA_API ParticleTimestepTask : public virtual GraphicsSimulation::Task {
+	public:
+		class Factory;
+
+		virtual void SetBuffers(ParticleBuffers* buffers) = 0;
+
+		virtual void GetDependencies(const Callback<GraphicsSimulation::Task*>& recordDependency)const override;
+
+	private:
+		Reference<GraphicsSimulation::Task> m_spawningStep;
+		friend class ParticleTimestepKernel;
+	};
+
+	class JIMARA_API ParticleTimestepTask::Factory : public virtual Object {
+	public:
+		class Set;
+
+		template<typename TaskType>
+		inline static Reference<Factory> Of() {
+			static const CreateFn createFn = [](GraphicsSimulation::Task* spawningStep) -> Reference<ParticleTimestepTask> {
+				if (spawningStep == nullptr) return nullptr;
+				const Reference<TaskType> task = Object::Instantiate<TaskType>(spawningStep->Context());
+				task->m_spawningStep = spawningStep;
+				return Reference<ParticleTimestepTask>(task.operator->());
+			};
+			const Reference<Factory> instance = new Factory(createFn, TypeId::Of<TaskType>());
+			instance->ReleaseRef();
+			return instance;
+		}
+
+		inline virtual ~Factory() {}
+
+		inline Reference<ParticleTimestepTask> CreateTask(GraphicsSimulation::Task* spawningStep) const { return m_createFn(spawningStep); }
+
+		inline TypeId TaskType()const { return m_taskType; }
+
+	private:
+		typedef Reference<ParticleTimestepTask>(*CreateFn)(GraphicsSimulation::Task*);
+		const CreateFn m_createFn;
+		const TypeId m_taskType;
+
+		inline Factory(const CreateFn& createFn, const TypeId& taskType) : m_createFn(createFn), m_taskType(taskType) {}
+	};
+
+	class JIMARA_API ParticleTimestepTask::Factory::Set : public virtual Object {
+	public:
+		static Reference<Set> Current();
+
+		inline virtual ~Set() {}
+
+		size_t Size()const;
+
+		const Factory* operator[](size_t index)const;
+
+		const Factory* operator[](const std::type_index& taskType)const;
+
+	private:
+		const Reference<const Object> m_data;
+
+		inline Set(const Object* data) : m_data(data) {}
 	};
 }
