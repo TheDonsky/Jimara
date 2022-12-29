777c5e7e218002692a1a89851b740b27809696fd
TheDonsky <nikadon.net@gmail.com>
2022-12-29 23:50:53 +0400

    Add handle controls to timeline curve

--- __Source__/Jimara-Editor/GUI/Utils/CustomSerializedObjectDrawers/TimelineCurveDrawer.cpp
+++ __Source__/Jimara-Editor/GUI/Utils/CustomSerializedObjectDrawers/TimelineCurveDrawer.cpp
@@ -47,7 +47,9 @@ namespace Jimara {
 
 					// Move point:
 					ImPlotPoint nodePosition = { info.time, info.node.Value() };
-					if (ImPlot::DragPoint(static_cast<int>(nodes.size()) * nodeIdSign, &nodePosition.x, &nodePosition.y, ImVec4(0.0f, 1.0f, 0.0f, 1.0f), DRAG_POINT_SIZE)) {
+					if (ImPlot::DragPoint(
+						static_cast<int>(nodes.size()) * nodeIdSign, 
+						&nodePosition.x, &nodePosition.y, ImVec4(0.0f, 1.0f, 0.0f, 1.0f), DRAG_POINT_SIZE)) {
 						info.time = static_cast<float>(nodePosition.x);
 						info.node.Value() = static_cast<float>(nodePosition.y);
 						stuffChanged = true;
@@ -64,14 +66,71 @@ namespace Jimara {
 						}
 					}
 
-					// __TODO__: Move 'left handle'
-					
-					// __TODO__: Move 'right handle'
-
 					nodes.push_back(info);
 				}
 
-				bool nodeMoved = false;
+				// Draw handles:
+
+				const constexpr float HANDLE_SIZE = 64.0f;
+				const Vector2 HANDLE_LENGTH = [&]() -> Vector2 {
+					ImPlotPoint a = ImPlot::PixelsToPlot({ 0.0f, 0.0f });
+					ImPlotPoint b = ImPlot::PixelsToPlot({ HANDLE_SIZE, HANDLE_SIZE });
+					return {
+						static_cast<float>(std::abs(b.x - a.x)),
+						static_cast<float>(std::abs(b.y - a.y))
+					};
+				}();
+				const constexpr float HANDLE_DRAG_SIZE = 3.0f;
+
+				for (size_t i = 0u; i < nodes.size(); i++) {
+					PointInfo& node = nodes[i];
+					
+					ImPlotPoint line[2];
+					line[0] = { node.time, node.node.Value() };
+					
+					auto drawHandle = [&](Property<float> handle, size_t index, size_t indexMultiplier) {
+						Vector2 direction;
+
+						auto setDirection = [&](float dx, float dy) {
+							direction =
+								(std::isfinite(dx) && std::isfinite(dy)) ? Math::Normalize(Vector2(dx, dy)) :
+								std::isfinite(dx) ? Vector2(0.0f, dy > 0.0f ? 1.0f : -1.0f) :
+								Vector2(dx >= 0.0f ? 1.0f : -1.0f, 0.0f);
+						};
+
+						const Vector2 delta(
+							(index == (size_t(0u) - 1u)) ? -1.0f :
+							(index >= nodes.size()) ? 1.0f :
+							((nodes[index].time - node.time) / 3.0f), handle);
+
+						setDirection(delta.x, delta.y);
+
+						ImPlotPoint& handleControl = line[1];
+						handleControl = {
+							static_cast<double>(direction.x) * HANDLE_LENGTH.x + node.time,
+							static_cast<double>(direction.y) * HANDLE_LENGTH.y + node.node.Value()
+						};
+						
+						
+						if (ImPlot::DragPoint(
+							static_cast<int>(curve->size() * indexMultiplier + i) * nodeIdSign,
+							&handleControl.x, &handleControl.y,
+							ImVec4(0.5f, 0.5f, 0.5f, 1.0f), HANDLE_DRAG_SIZE)) {
+							setDirection(
+								(static_cast<float>(handleControl.x) - node.time) / Math::Max(HANDLE_LENGTH.x, std::numeric_limits<float>::epsilon()),
+								(static_cast<float>(handleControl.y) - node.node.Value()) / Math::Max(HANDLE_LENGTH.y, std::numeric_limits<float>::epsilon()));
+							float multiplier = (std::abs(direction.x) > std::numeric_limits<float>::epsilon()) ? (1.0f / std::abs(direction.x)) : 1.0f;
+							handle = std::abs(delta.x) * multiplier * direction.y;
+							stuffChanged = true;
+						}
+
+						ImPlot::SetNextLineStyle(ImVec4(0.25f, 0.25f, 0.25f, 1.0f), 1);
+						ImPlot::PlotLine("##shape", &line[0].x, &line[0].y, 2, 0, 0, sizeof(ImPlotPoint));
+					};
+
+					drawHandle(node.node.PrevHandle(), i - 1u, 1u);
+					drawHandle(node.node.NextHandle(), i + 1u, 2u);
+				}
 
 				if (stuffChanged) {
 					curve->clear();
@@ -87,6 +146,7 @@ namespace Jimara {
 					ImPlotPoint point = ImPlot::GetPlotMousePos();
 					curve->operator[](static_cast<float>(point.x)) = BezierNode<float>(static_cast<float>(point.y));
 					nodeIdSign *= -1;
+					stuffChanged = true;
 				}
 
 
@@ -121,7 +181,21 @@ namespace Jimara {
 				ImPlot::EndPlot();
 			}
 
-			return stuffChanged;
+			static thread_local const Serialization::ItemSerializer* lastSerializer = nullptr;
+			static thread_local const void* lastTargetAddr = nullptr;
+
+			const bool isSameObject = (object.Serializer() == lastSerializer && object.TargetAddr() == lastTargetAddr);
+			if (stuffChanged) {
+				lastSerializer = object.Serializer();
+				lastTargetAddr = object.TargetAddr();
+			}
+
+			if (!ImGui::IsAnyItemActive()) {
+				lastSerializer = nullptr;
+				lastTargetAddr = nullptr;
+			}
+
+			return (isSameObject || stuffChanged) && (lastSerializer == nullptr);
 		}
 	}
 
