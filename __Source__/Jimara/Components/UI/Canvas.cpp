0842c26363174f08b9e221089fd3550b0fb244bb
TheDonsky <nikadon.net@gmail.com>
2023-02-02 12:53:19 +0400

    Define public-facing interface of Canvas

--- __Source__/Jimara/Components/UI/Canvas.cpp
+++ __Source__/Jimara/Components/UI/Canvas.cpp
@@ -1,8 +1,60 @@
 #include "Canvas.h"
+#include "../../Data/Serialization/Helpers/SerializerMacros.h"
+#include "../../Data/Serialization/Attributes/SliderAttribute.h"
 
 
 namespace Jimara {
 	namespace UI {
-		// __TODO__: Implement this crap!
+		struct Canvas::Helpers {
+			static void OnCanvasDestroyed(Canvas* self, Component*) {
+				self->m_renderStack = nullptr;
+			}
+		};
+
+		Canvas::Canvas(Component* parent, const std::string_view& name) 
+			: Component(parent, name) {
+			m_renderStack = RenderStack::Main(Context());
+			OnDestroyed() += Callback<Component*>(Helpers::OnCanvasDestroyed, this);
+		}
+
+		Canvas::~Canvas() {
+			OnDestroyed() -= Callback<Component*>(Helpers::OnCanvasDestroyed, this);
+			m_renderStack = nullptr;
+		}
+
+		Vector2 Canvas::Size()const {
+			const auto aspect = [](const Vector2& size) { return size.x / Math::Max(size.y, std::numeric_limits<float>::epsilon()); };
+			const Vector2 textureSize = m_renderStack == nullptr ? Vector2(0.0f) : (Vector2)m_renderStack->Resolution();
+			const float targetAspect = aspect(textureSize);
+			const float referenceAspect = aspect(m_referenceResolution);
+			const float rescaledX = m_referenceResolution.x * targetAspect / Math::Max(referenceAspect, std::numeric_limits<float>::epsilon());
+			const float rescaledY = m_referenceResolution.y * referenceAspect / Math::Max(targetAspect, std::numeric_limits<float>::epsilon());
+			return Vector2(
+				Math::Lerp(rescaledX, m_referenceResolution.x, m_widthBias),
+				Math::Lerp(m_referenceResolution.y, rescaledY, m_widthBias));
+		}
+
+		void Canvas::GetFields(Callback<Serialization::SerializedObject> recordElement) {
+			Component::GetFields(recordElement);
+			JIMARA_SERIALIZE_FIELDS(this, recordElement) {
+				JIMARA_SERIALIZE_FIELD_GET_SET(ReferenceSize, SetReferenceSize, "Reference Size", 
+					"Reference 'resolution'/virtual size of the canvas");
+				
+				JIMARA_SERIALIZE_FIELD_GET_SET(WidthBias, SetWidthBias, "Width Bias",
+					"Value between 0 and 1, indicating whether the virtual size of the canvas scales horizontally(0.0f) or vertically(1.0f)",
+					Object::Instantiate<Serialization::SliderAttribute<float>>(0.0f, 1.0f));
+				
+				JIMARA_SERIALIZE_FIELD_GET_SET(RendererCategory, SetRendererCategory, "Render Category", 
+					"Higher category will render later; refer to Scene::GraphicsContext::Renderer for further details.");
+				
+				JIMARA_SERIALIZE_FIELD_GET_SET(RendererPriority, SetRendererPriority, "Render Priority", 
+					"Higher priority will render earlier within the same category; refer to Scene::GraphicsContext::Renderer for further details.");
+			};
+		}
+	}
+
+	template<> void TypeIdDetails::GetTypeAttributesOf<UI::Canvas>(const Callback<const Object*>& report) {
+		static const ComponentSerializer::Of<UI::Canvas> serializer("Jimara/UI/Canvas", "Canvas");
+		report(&serializer);
 	}
 }
--- __Source__/Jimara/Components/UI/Canvas.h
+++ __Source__/Jimara/Components/UI/Canvas.h
@@ -1,9 +1,107 @@
 #pragma once
 #include "../Component.h"
+#include "../../Environment/Rendering/RenderStack.h"
 
 
 namespace Jimara {
+	/// <summary> Let the system know of the type </summary>
+	JIMARA_REGISTER_TYPE(Jimara::UI::Canvas);
+
 	namespace UI {
-		// __TODO__: Implement this crap!
+		/// <summary>
+		/// UI Canvas for in-game HUD
+		/// </summary>
+		class JIMARA_API Canvas : public virtual Component {
+		public:
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="parent"> Parent component [Can not be nullptr] </param>
+			/// <param name="name"> Canvas name </param>
+			Canvas(Component* parent, const std::string_view& name = "Canvas");
+
+			/// <summary> Virtual destructor </summary>
+			virtual ~Canvas();
+
+			/// <summary> Reference 'resolution'/virtual size of the canvas </summary>
+			inline Vector2 ReferenceSize()const { return m_referenceResolution; }
+
+			/// <summary>
+			/// Sets reference resolution of the canvas
+			/// </summary>
+			/// <param name="size"> Reference size to use </param>
+			inline void SetReferenceSize(const Vector2& size) { m_referenceResolution = Vector2(Math::Max(size.x, 0.0f), Math::Max(size.y, 0.0f)); }
+
+			/// <summary> Value between 0 and 1, indicating whether the virtual size of the canvas scales horizontally(0.0f) or vertically(1.0f) </summary>
+			inline float WidthBias()const { return m_widthBias; }
+
+			/// <summary>
+			/// Sets width bias
+			/// </summary>
+			/// <param name="bias"> 0.0f for the canvas size to scale horizontally, 1.0f for pure vertical scaling and values in the middle for interpolation </param>
+			inline void SetWidthBias(float bias) { m_widthBias = Math::Max(0.0f, Math::Min(bias, 1.0f)); }
+
+			/// <summary>
+			/// Virtual size of the canvas
+			/// <para/> Note that this is calculated based on the target render stack resolution, width bias and reference size.
+			/// </summary>
+			Vector2 Size()const;
+
+			/// <summary> 
+			/// Renderer category for render stack 
+			/// <para/> Note: Higher category will render later; refer to Scene::GraphicsContext::Renderer for further details.
+			/// </summary>
+			inline uint32_t RendererCategory()const { return m_renderCategory; }
+
+			/// <summary>
+			/// Sets renderer category for render stack
+			/// Note: Higher category will render later; refer to Scene::GraphicsContext::Renderer for further details.
+			/// </summary>
+			/// <param name="category"> Category to set </param>
+			inline void SetRendererCategory(uint32_t category) { m_renderCategory = category; }
+
+			/// <summary> 
+			/// Renderer priority for render stack 
+			/// <para/> Note: Higher priority will render earlier within the same category; refer to Scene::GraphicsContext::Renderer for further details.
+			/// </summary>
+			inline uint32_t RendererPriority()const { return m_renderPriority; }
+
+			/// <summary>
+			/// Sets renderer priority for render stack
+			/// <para/> Note: Higher priority will render earlier within the same category; refer to Scene::GraphicsContext::Renderer for further details.
+			/// </summary>
+			/// <param name="priority"> Priority to set </param>
+			inline void SetRendererPriority(uint32_t priority) { m_renderPriority = priority; }
+
+			/// <summary>
+			/// Exposes fields to serialization utilities
+			/// </summary>
+			/// <param name="recordElement"> Reports elements with this </param>
+			virtual void GetFields(Callback<Serialization::SerializedObject> recordElement)override;
+
+
+		private:
+			// Reference 'resolution'/virtual size of the canvas
+			Vector2 m_referenceResolution = Vector2(1920.0f, 1080.0f);
+
+			// Value between 0 and 1, indicating whether the virtual size of the canvas scales horizontally(0.0f) or vertically(1.0f)
+			float m_widthBias = 0.0f;
+
+			// Render stack, this canvas is tied to
+			Reference<RenderStack> m_renderStack;
+
+			// Renderer category for render stack (higher category will render later)
+			uint32_t m_renderCategory = 2048;
+
+			// Renderer priority for render stack (higher priority will render earlier within the same category)
+			uint32_t m_renderPriority = 0;
+
+			// Some private functionality resides here...
+			struct Helpers;
+		};
 	}
+
+	// Type detail callbacks
+	template<> inline void TypeIdDetails::GetParentTypesOf<UI::Canvas>(const Callback<TypeId>& report) { report(TypeId::Of<Component>()); }
+	template<> JIMARA_API void TypeIdDetails::GetTypeAttributesOf<UI::Canvas>(const Callback<const Object*>& report);
 }
