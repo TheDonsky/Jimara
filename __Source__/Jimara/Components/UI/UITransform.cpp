3ffaf0d5de303719004cfa3a2782ec081028ddcc
TheDonsky <nikadon.net@gmail.com>
2023-02-02 22:04:49 +0400

    Define basic modifiable parameters of UI Transform

--- __Source__/Jimara/Components/UI/UITransform.cpp
+++ __Source__/Jimara/Components/UI/UITransform.cpp
@@ -3,6 +3,83 @@
 
 namespace Jimara {
 	namespace UI {
-		// __TODO__: Implement this crap!
+		struct UITransform::Helpers {
+			inline static void SetCanvasReference(UITransform* self, UI::Canvas* canvas) {
+				if (self->m_canvas == canvas) return;
+
+				const Callback<Component*> onCanvasDestroyed(Helpers::OnCanvasDestroyed, self);
+				
+				if (self->m_canvas != nullptr) 
+					self->m_canvas->OnDestroyed() -= onCanvasDestroyed;
+				
+				self->m_canvas = canvas;
+				
+				if (self->m_canvas != nullptr) 
+					self->m_canvas->OnDestroyed() += onCanvasDestroyed;
+			}
+
+			inline static void FindCanvasReference(UITransform* self) { SetCanvasReference(self, self->GetComponentInParents<UI::Canvas>()); }
+			inline static void ClearCanvasReference(UITransform* self) { SetCanvasReference(self, nullptr); }
+
+			inline static void OnTransformParentChanged(UITransform* self, ParentChangeInfo) { 
+				FindCanvasReference(self); 
+			}
+			inline static void OnCanvasDestroyed(UITransform* self, Component*) { 
+				FindCanvasReference(self); 
+			}
+			inline static void OnTransformDestroyed(UITransform* self, Component*) { 
+				ClearCanvasReference(self); 
+			}
+		};
+
+		UITransform::UITransform(Component* parent, const std::string_view& name)
+			: Component(parent, name) {
+			OnParentChanged() += Callback<ParentChangeInfo>(Helpers::OnTransformParentChanged, this);
+			OnDestroyed() += Callback<Component*>(Helpers::OnTransformDestroyed, this);
+			Helpers::FindCanvasReference(this);
+		}
+
+		UITransform::~UITransform() {
+			OnParentChanged() -= Callback<ParentChangeInfo>(Helpers::OnTransformParentChanged, this);
+			OnDestroyed() -= Callback<Component*>(Helpers::OnTransformDestroyed, this);
+			Helpers::ClearCanvasReference(this);
+		}
+
+		UI::Canvas* UITransform::Canvas() { return m_canvas; }
+
+		void UITransform::SetPivot(const Vector2& pivot) {
+			m_pivot = pivot;
+			// __TODO__: Implement this crap...
+		}
+
+		void UITransform::SetAnchorRect(const Rect& anchors) {
+			m_anchorRect = anchors;
+			// __TODO__: Implement this crap...
+		}
+
+		void UITransform::SetBorderSize(const Vector2& size) {
+			m_borderSize = size;
+			// __TODO__: Implement this crap...
+		}
+
+		void UITransform::SetBorderOffset(const Vector2& offset) {
+			m_borderOffset = offset;
+			// __TODO__: Implement this crap...
+		}
+
+		void UITransform::SetRotation(float rotation) {
+			m_rotation = rotation;
+			// __TODO__: Implement this crap...
+		}
+
+		void UITransform::SetLocalScale(const Vector2& scale) {
+			m_scale = scale;
+			// __TODO__: Implement this crap...
+		}
+
+		void UITransform::GetFields(Callback<Serialization::SerializedObject> recordElement) {
+			Component::GetFields(recordElement);
+			// __TODO__: Implement this crap...
+		}
 	}
 }
--- __Source__/Jimara/Components/UI/UITransform.h
+++ __Source__/Jimara/Components/UI/UITransform.h
@@ -4,6 +4,76 @@
 
 namespace Jimara {
 	namespace UI {
-		// __TODO__: Implement this crap!
+		/// <summary>
+		/// HUD Transform
+		/// </summary>
+		class JIMARA_API UITransform : public virtual Component {
+		public:
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="parent"> Parent component [Can not be nullptr] </param>
+			/// <param name="name"> Transform name </param>
+			UITransform(Component* parent, const std::string_view& name = "Transform");
+
+			virtual ~UITransform();
+
+			UI::Canvas* Canvas();
+
+			inline Vector2 Pivot()const { return m_pivot; }
+
+			void SetPivot(const Vector2& pivot);
+
+			inline Rect AnchorRect()const { return m_anchorRect; }
+
+			void SetAnchorRect(const Rect& anchors);
+
+			inline Vector2 BorderSize()const { return m_borderSize; }
+
+			void SetBorderSize(const Vector2& size);
+
+			inline Vector2 BorderOffset()const { return m_borderOffset; }
+
+			void SetBorderOffset(const Vector2& offset);
+
+			inline float Rotation()const { return m_rotation; }
+
+			void SetRotation(float rotation);
+
+			inline Vector2 LocalScale()const { return m_scale; }
+
+			void SetLocalScale(const Vector2& scale);
+
+			struct Transformation {
+				Vector2 position;
+				Vector2 right;
+				Vector2 size;
+				inline Vector2 Up()const { return Vector2(-right.y, right.x); }
+			};
+
+			/// <summary>
+			/// Exposes fields to serialization utilities
+			/// </summary>
+			/// <param name="recordElement"> Reports elements with this </param>
+			virtual void GetFields(Callback<Serialization::SerializedObject> recordElement)override;
+
+		private:
+			Vector2 m_pivot = Vector2(0.0f);
+
+			Rect m_anchorRect = Rect();
+
+			Vector2 m_borderSize = Vector2(128.0f, 128.0f);
+
+			Vector2 m_borderOffset = Vector2(0.0f);
+
+			float m_rotation = 0.0f;
+
+			Vector2 m_scale = Vector2(1.0f);
+
+			Reference<UI::Canvas> m_canvas;
+
+			// Some private functionality resides here...
+			struct Helpers;
+		};
 	}
 }
