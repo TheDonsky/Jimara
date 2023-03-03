a9ed4a0e3890fdb076d647be8bdb6e7f91b5746c
TheDonsky <nikadon.net@gmail.com>
2023-03-03 09:14:43 +0400

    Define public inteface of UI Image

--- __Source__/Jimara/Components/UI/UIImage.cpp
+++ __Source__/Jimara/Components/UI/UIImage.cpp
@@ -1,8 +1,62 @@
 #include "UIImage.h"
+#include "../../Data/Serialization/Helpers/SerializerMacros.h"
 
 
 namespace Jimara {
 	namespace UI {
-		// __TODO__: Implement this crap!
+		struct UIImage::Helpers {
+			// __TODO__: Implement this crap!
+		};
+
+
+		UIImage::UIImage(Component* parent, const std::string_view& name) 
+			: Component(parent, name) {
+			// __TODO__: Implement this crap!
+		}
+
+		UIImage::~UIImage() {
+			// __TODO__: Implement this crap!
+		}
+
+		Graphics::TextureSampler* UIImage::Texture()const {
+			// __TODO__: Implement this crap!
+			return nullptr;
+		}
+
+		void UIImage::SetTexture(Graphics::TextureSampler* SetTexture) {
+			// __TODO__: Implement this crap!
+		}
+
+		bool UIImage::KeepAspectRatio()const {
+			// __TODO__: Implement this crap!
+			return false;
+		}
+
+		void UIImage::KeepAspectRatio(bool preserve) {
+			// __TODO__: Implement this crap!
+		}
+
+		void UIImage::GetFields(Callback<Serialization::SerializedObject> recordElement) {
+			Component::GetFields(recordElement);
+			JIMARA_SERIALIZE_FIELDS(this, recordElement) {
+				static const std::string textureHint = "Sampler to the main texture (overrides material filed named '" + std::string(TextureShaderBindingName()) + "')";
+				JIMARA_SERIALIZE_FIELD_GET_SET(Texture, SetTexture, "Texture", textureHint);
+				JIMARA_SERIALIZE_FIELD_GET_SET(KeepAspectRatio, KeepAspectRatio, "Keep Aspect", "If true, the UIImage will keep the aspect ratio of the Texture");
+			};
+		}
+
+		void UIImage::OnComponentEnabled() {
+			// __TODO__: Implement this crap!
+		}
+
+		void UIImage::OnComponentDisabled() {
+			// __TODO__: Implement this crap!
+		}
+	}
+
+
+	template<> JIMARA_API void TypeIdDetails::GetTypeAttributesOf<UI::UIImage>(const Callback<const Object*>& report) {
+		static const ComponentSerializer::Of<UI::UIImage> serializer("Jimara/UI/Image", "Image");
+		report(&serializer);
 	}
 }
--- __Source__/Jimara/Components/UI/UIImage.h
+++ __Source__/Jimara/Components/UI/UIImage.h
@@ -4,6 +4,68 @@
 
 namespace Jimara {
 	namespace UI {
-		// __TODO__: Implement this crap!
+		/// <summary> Let the system know of the type </summary>
+		JIMARA_REGISTER_TYPE(Jimara::UI::UIImage);
+
+		/// <summary>
+		/// Image that can appear on UI Canvas
+		/// </summary>
+		class JIMARA_API UIImage : public virtual Component {
+		public:
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="parent"> Parent component </param>
+			/// <param name="name"> Image name </param>
+			UIImage(Component* parent, const std::string_view& name = "UIImage");
+
+			/// <summary> Virtual destructor </summary>
+			virtual ~UIImage();
+
+			/// <summary> Image's Texture field will override a shader input of this name </summary>
+			inline static const constexpr std::string_view TextureShaderBindingName() { return "MainTexture"; };
+
+			/// <summary> Sampler to the main texture (overrides material filed with the name: TextureShaderBindingName()) </summary>
+			Graphics::TextureSampler* Texture()const;
+
+			/// <summary>
+			/// Sets main texture sampler
+			/// </summary>
+			/// <param name="SetTexture"> Sampler to the main texture </param>
+			void SetTexture(Graphics::TextureSampler* SetTexture);
+
+			/// <summary> If true, the UIImage will keep the aspect ratio of the Texture </summary>
+			bool KeepAspectRatio()const;
+
+			/// <summary>
+			/// Configures, whether or not to keep the main texture aspect ratio
+			/// </summary>
+			/// <param name="preserve"> If true, Texture aspect ratio will be preserved </param>
+			void KeepAspectRatio(bool preserve);
+
+			/// <summary>
+			/// Exposes fields to serialization utilities
+			/// </summary>
+			/// <param name="recordElement"> Reports elements with this </param>
+			virtual void GetFields(Callback<Serialization::SerializedObject> recordElement)override;
+
+
+		protected:
+			/// <summary> Invoked, whenever the component becomes active in herarchy </summary>
+			virtual void OnComponentEnabled()override;
+
+			/// <summary> Invoked, whenever the component stops being active in herarchy </summary>
+			virtual void OnComponentDisabled()override;
+
+
+		private:
+
+			// Private stuff resides in here..
+			struct Helpers;
+		};
 	}
+
+	// Type detail callbacks
+	template<> inline void TypeIdDetails::GetParentTypesOf<UI::UIImage>(const Callback<TypeId>& report) { report(TypeId::Of<Component>()); }
+	template<> JIMARA_API void TypeIdDetails::GetTypeAttributesOf<UI::UIImage>(const Callback<const Object*>& report);
 }
