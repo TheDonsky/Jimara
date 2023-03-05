fee7cccc35c969d957f59909a30b90d928e09bc9
TheDonsky <nikadon.net@gmail.com>
2023-03-05 21:17:22 +0400

    Document some stuff

--- __Source__/Jimara/Environment/Rendering/LightingModels/UnlitRendering/CanvasRenderer.h
+++ __Source__/Jimara/Environment/Rendering/LightingModels/UnlitRendering/CanvasRenderer.h
@@ -5,13 +5,23 @@
 
 namespace Jimara {
 	namespace UI {
+		/// <summary>
+		/// Renderer for UI Canvas Components
+		/// </summary>
 		class CanvasRenderer {
 		public:
+			/// <summary>
+			/// Creates UI renderer (used internally by Canvas)
+			/// </summary>
+			/// <param name="canvas"> Target Canvas </param>
+			/// <returns> New instance of an UI renderer </returns>
 			static Reference<RenderStack::Renderer> CreateFor(const Canvas* canvas);
 
 		private:
+			// Constructor is private:
 			inline CanvasRenderer() {}
 
+			// Actual implementation is hidden behind Helpers struct
 			struct Helpers;
 		};
 	}
