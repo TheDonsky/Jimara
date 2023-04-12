e01c6a87bcf603f8ee1ecc94ec1e289731378095
TheDonsky <nikadon.net@gmail.com>
2023-04-12 21:50:44 +0400

    Switch to new pipelines and binding sets

--- __Source__/Jimara/Environment/GraphicsSimulation/CombinedGraphicsSimulationKernel.h
+++ __Source__/Jimara/Environment/GraphicsSimulation/CombinedGraphicsSimulationKernel.h
@@ -88,55 +88,17 @@ namespace Jimara {
 			alignas(16) Size3 taskBoundaries = {};
 		};
 
-		// Compute pipeline descriptor
-		class PipelineDescriptor : public virtual Graphics::ComputePipeline::Descriptor {
-		private:
-			// Binding set descriptors, created automagically based on the shader binary and the binding descriptors
-			const std::vector<Reference<Graphics::PipelineDescriptor::BindingSetDescriptor>> m_bindingSets;
-
-			// Compute shader from the shader class
-			const Reference<Graphics::Shader> m_shader;
-
-			// Total number of threads
-			uint32_t m_threadCount = 0u;
-
-		public:
-			// Constructor
-			inline PipelineDescriptor(std::vector<Reference<Graphics::PipelineDescriptor::BindingSetDescriptor>>* bindingSets, Graphics::Shader* shader)
-				: m_bindingSets(std::move(*bindingSets)), m_shader(shader) {
-			}
-
-			// virtual desctructor
-			inline virtual ~PipelineDescriptor() {}
-
-			// Graphics::PipelineDescriptor:
-			inline virtual size_t BindingSetCount()const { return 2u; }
-			inline virtual const BindingSetDescriptor* BindingSet(size_t index)const { return m_bindingSets[index]; }
-
-			// Graphics::ComputePipeline::Descriptor:
-			inline virtual Reference<Graphics::Shader> ComputeShader()const override { return m_shader; }
-			inline virtual Size3 NumBlocks()const override {
-				const constexpr size_t BLOCK_SIZE = 256u;
-				return Size3((m_threadCount + BLOCK_SIZE - 1u) / BLOCK_SIZE, 1u, 1u);
-			}
-
-			// Thread count controls for Execute() method:
-			inline uint32_t ThreadCount()const { return m_threadCount; }
-			inline void SetThreadCount(uint32_t threadCount) { m_threadCount = threadCount; }
-		};
-
-	private:
 		// Scene context
 		const Reference<SceneContext> m_context;
 
 		// Binding of the jimara_CombinedGraphicsSimulationKernelTasks buffer
 		const Reference<Graphics::ShaderResourceBindings::StructuredBufferBinding> m_taskDescriptorBinding;
 
-		// Compute pipeline descriptor
-		const Reference<PipelineDescriptor> m_pipelineDescriptor;
+		// Compute pipeline
+		const Reference<Graphics::Experimental::ComputePipeline> m_computePipeline;
 
-		// Compute pipeline instance
-		const Reference<Graphics::ComputePipeline> m_pipeline;
+		// Binding sets
+		const Stacktor<Reference<Graphics::BindingSet>, 4u> m_bindingSets;
 
 		// Last content of the m_taskDescriptorBinding buffer
 		std::vector<TaskDescriptor> m_lastTaskDescriptors;
@@ -145,8 +107,10 @@ namespace Jimara {
 		inline CombinedGraphicsSimulationKernel(
 			SceneContext* context,
 			Graphics::ShaderResourceBindings::StructuredBufferBinding* taskDescriptorBinding,
-			PipelineDescriptor* pipelineDescriptor, Graphics::ComputePipeline* pipeline)
-			: m_context(context), m_taskDescriptorBinding(taskDescriptorBinding), m_pipelineDescriptor(pipelineDescriptor), m_pipeline(pipeline) {}
+			Graphics::Experimental::ComputePipeline* computePipeline,
+			Stacktor<Reference<Graphics::BindingSet>, 4u>&& bindingSets)
+			: m_context(context), m_taskDescriptorBinding(taskDescriptorBinding)
+			, m_computePipeline(computePipeline), m_bindingSets(std::move(bindingSets)) {}
 	};
 
 
@@ -166,81 +130,80 @@ namespace Jimara {
 		if (shaderSet == nullptr) return fail("Failed to get shader set! [File: ", __FILE__, "; Line: ", __LINE__, "]");
 		const Reference<Graphics::SPIRV_Binary> binary = shaderSet->GetShaderModule(shaderClass, Graphics::PipelineStage::COMPUTE);
 		if (binary == nullptr) return fail("Failed to get shader binary for '", shaderClass->ShaderPath(), "'! [File: ", __FILE__, "; Line: ", __LINE__, "]");
-		const Reference<Graphics::ShaderCache> shaderCache = Graphics::ShaderCache::ForDevice(context->Graphics()->Device());
-		if (shaderCache == nullptr) return fail("Failed to get shader cache! [File: ", __FILE__, "; Line: ", __LINE__, "]");
-		const Reference<Graphics::Shader> shader = shaderCache->GetShader(binary);
-		if (shader == nullptr) return fail("Failed to create shader module for '", shaderClass->ShaderPath(), "'! [File: ", __FILE__, "; Line: ", __LINE__, "]");
 
 		// Create combined binding set:
 		static const std::string CombinedGraphicsSimulationKernelTasksBindingName = "jimara_CombinedGraphicsSimulationKernelTasks";
 		if (bindings.FindStructuredBufferBinding(CombinedGraphicsSimulationKernelTasksBindingName) != nullptr)
-			return fail("Binding for ", CombinedGraphicsSimulationKernelTasksBindingName, " is supposed to be provided by CombinedGraphicsSimulationKernel! [File: ", __FILE__, "; Line: ", __LINE__, "]");
-		struct BindingSet : public virtual Graphics::ShaderResourceBindings::ShaderResourceBindingSet {
-			const Reference<Graphics::ShaderResourceBindings::StructuredBufferBinding> taskDescriptorBinding =
-				Object::Instantiate<Graphics::ShaderResourceBindings::StructuredBufferBinding>();
-			const Graphics::ShaderResourceBindings::ShaderResourceBindingSet* baseBindings = nullptr;
-			SceneContext* context = nullptr;
-			mutable Reference<const Graphics::ShaderResourceBindings::BindlessStructuredBufferSetBinding> bindlessBuffers;
-			mutable Reference<const Graphics::ShaderResourceBindings::BindlessTextureSamplerSetBinding> bindlessSamplers;
+			return fail("Binding for ", CombinedGraphicsSimulationKernelTasksBindingName, 
+				" is supposed to be provided by CombinedGraphicsSimulationKernel! [File: ", __FILE__, "; Line: ", __LINE__, "]");
 
-			inline virtual Reference<const Graphics::ShaderResourceBindings::ConstantBufferBinding> FindConstantBufferBinding(const std::string& name)const override { return baseBindings->FindConstantBufferBinding(name); }
-			inline virtual Reference<const Graphics::ShaderResourceBindings::StructuredBufferBinding> FindStructuredBufferBinding(const std::string& name)const override {
-				if (name == CombinedGraphicsSimulationKernelTasksBindingName) return taskDescriptorBinding;
-				return baseBindings->FindStructuredBufferBinding(name);
-			}
-			inline virtual Reference<const Graphics::ShaderResourceBindings::TextureSamplerBinding> FindTextureSamplerBinding(const std::string& name)const override { return baseBindings->FindTextureSamplerBinding(name); }
-			inline virtual Reference<const Graphics::ShaderResourceBindings::TextureViewBinding> FindTextureViewBinding(const std::string& name)const override { return baseBindings->FindTextureViewBinding(name); }
-			inline virtual Reference<const Graphics::ShaderResourceBindings::BindlessStructuredBufferSetBinding> FindBindlessStructuredBufferSetBinding(const std::string& name)const override { 
-				Reference<const Graphics::ShaderResourceBindings::BindlessStructuredBufferSetBinding> binding = baseBindings->FindBindlessStructuredBufferSetBinding(name);
-				if (binding != nullptr) return binding;
-				if (bindlessBuffers == nullptr)
-					bindlessBuffers = Object::Instantiate<Graphics::ShaderResourceBindings::BindlessStructuredBufferSetBinding>(context->Graphics()->Bindless().BufferBinding());
-				return bindlessBuffers;
-			}
-			inline virtual Reference<const Graphics::ShaderResourceBindings::BindlessTextureSamplerSetBinding> FindBindlessTextureSamplerSetBinding(const std::string& name)const override { 
-				Reference<const Graphics::ShaderResourceBindings::BindlessTextureSamplerSetBinding> binding = baseBindings->FindBindlessTextureSamplerSetBinding(name);
-				if (binding != nullptr) return binding;
-				if (bindlessSamplers == nullptr)
-					bindlessSamplers = Object::Instantiate<Graphics::ShaderResourceBindings::BindlessTextureSamplerSetBinding>(context->Graphics()->Bindless().SamplerBinding());
-				return bindlessSamplers;
-			}
-			inline virtual Reference<const Graphics::ShaderResourceBindings::BindlessTextureViewSetBinding> FindBindlessTextureViewSetBinding(const std::string& name)const override { return baseBindings->FindBindlessTextureViewSetBinding(name); }
-		} bindingSet;
-		bindingSet.baseBindings = &bindings;
-		bindingSet.context = context;
+		// Create task descriptor binding:
+		const Reference<Graphics::ShaderResourceBindings::StructuredBufferBinding> taskDescriptorBinding =
+			Object::Instantiate<Graphics::ShaderResourceBindings::StructuredBufferBinding>();
 
-		// Create graphics binding set descriptors:
-		std::vector<Reference<Graphics::PipelineDescriptor::BindingSetDescriptor>> bindingSetDescriptors(binary->BindingSetCount());
+		// Get compute pipeline:
+		const Reference<Graphics::Experimental::ComputePipeline> computePipeline = context->Graphics()->Device()->GetComputePipeline(binary);
+		if (computePipeline == nullptr) return fail("Failed to get/create compute pipeline! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+
+		// Create binding pool:
+		const Reference<Graphics::BindingPool> bindingPool = context->Graphics()->Device()->CreateBindingPool(
+			context->Graphics()->Configuration().MaxInFlightCommandBufferCount());
+		if (bindingPool == nullptr) return fail("Failed to create binding pool! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+
+		// Define binding search functions:
+		auto findConstantBuffer = [&](const auto& setDesc) { return bindings.FindConstantBufferBinding(setDesc.bindingName); };
+		auto findStructuredBuffer = [&](const auto& setDesc) -> Reference<const Graphics::ShaderResourceBindings::StructuredBufferBinding> {
+			if (setDesc.bindingName == CombinedGraphicsSimulationKernelTasksBindingName) return taskDescriptorBinding;
+			return bindings.FindStructuredBufferBinding(setDesc.bindingName);
+		};
+		auto findTextureSampler = [&](const auto& setDesc) { return bindings.FindTextureSamplerBinding(setDesc.bindingName); };
+		auto findTextureView = [&](const auto& setDesc) { return bindings.FindTextureViewBinding(setDesc.bindingName); };
+
+		using BindlessArrays = Graphics::ResourceBinding<Graphics::BindlessSet<Graphics::ArrayBuffer>::Instance>;
+		Reference<const BindlessArrays> bindlessBuffers;
+		auto findBindlessStructuredBuffers = [&](const auto& setDesc) {
+			const Reference<const BindlessArrays> binding = bindings.FindBindlessStructuredBufferSetBinding(setDesc.bindingName);
+			if (binding != nullptr) return binding;
+			if (bindlessBuffers == nullptr)
+				bindlessBuffers = Object::Instantiate<BindlessArrays>(context->Graphics()->Bindless().BufferBinding());
+			return bindlessBuffers;
+		};
+
+		using BindlessSamplers = Graphics::ResourceBinding<Graphics::BindlessSet<Graphics::TextureSampler>::Instance>;
+		Reference<const BindlessSamplers> bindlessSamplers;
+		auto findBindlessTextureSamplers = [&](const auto& setDesc) {
+			Reference<const BindlessSamplers> binding = bindings.FindBindlessTextureSamplerSetBinding(setDesc.bindingName);
+			if (binding != nullptr) return binding;
+			if (bindlessSamplers == nullptr)
+				bindlessSamplers = Object::Instantiate<BindlessSamplers>(context->Graphics()->Bindless().SamplerBinding());
+			return bindlessSamplers;
+		};
+
+		// Create binding sets:
+		Graphics::BindingSet::Descriptor bindingSetDescriptor = {};
 		{
-			assert(bindingSetDescriptors.size() == binary->BindingSetCount());
-			const Graphics::SPIRV_Binary* binaryPtr = binary;
-			size_t maxIndex = 0u;
-			if (!Graphics::ShaderResourceBindings::GenerateShaderBindings(&binaryPtr, 1u, bindingSet,
-				[&](const Graphics::ShaderResourceBindings::BindingSetInfo& info) {
-					maxIndex = Math::Max(info.setIndex, maxIndex);
-					if (info.setIndex < bindingSetDescriptors.size()) {
-						if (bindingSetDescriptors[info.setIndex] == nullptr)
-							bindingSetDescriptors[info.setIndex] = info.set;
-						else context->Log()->Warning("CombinedGraphicsSimulationKernel<", TypeId::Of<SimulationTaskSettings>().Name(), ">::Create - ",
-							"Duplicate binding set descriptor generated for set ", info.setIndex, "! [File:", __FILE__, "; Line: ", __LINE__, "]");
-					};
-				}, context->Log()))
-				return fail("Failed to generate shader bindings! [File:", __FILE__, "; Line: ", __LINE__, "]");
-			if (maxIndex >= bindingSetDescriptors.size())
-				return fail("Generated shader binding set index out of range! [File:", __FILE__, "; Line: ", __LINE__, "]");
-			for (size_t i = 0; i < bindingSetDescriptors.size(); i++)
-				if (bindingSetDescriptors[i] == nullptr)
-					return fail("Binding set descriptor missing for set ", i, "! [File:", __FILE__, "; Line: ", __LINE__, "]");
+			bindingSetDescriptor.pipeline = computePipeline;
+			bindingSetDescriptor.findConstantBuffer = Graphics::BindingSet::Descriptor::BindingSearchFn<Graphics::Buffer>::FromCall(&findConstantBuffer);
+			bindingSetDescriptor.findStructuredBuffer = Graphics::BindingSet::Descriptor::BindingSearchFn<Graphics::ArrayBuffer>::FromCall(&findStructuredBuffer);
+			bindingSetDescriptor.findTextureSampler = Graphics::BindingSet::Descriptor::BindingSearchFn<Graphics::TextureSampler>::FromCall(&findTextureSampler);
+			bindingSetDescriptor.findTextureView = Graphics::BindingSet::Descriptor::BindingSearchFn<Graphics::TextureView>::FromCall(&findTextureView);
+			bindingSetDescriptor.findBindlessStructuredBuffers = Graphics::BindingSet::Descriptor::BindingSearchFn<
+				Graphics::BindlessSet<Graphics::ArrayBuffer>::Instance>::FromCall(&findBindlessStructuredBuffers);
+			bindingSetDescriptor.findBindlessTextureSamplers = Graphics::BindingSet::Descriptor::BindingSearchFn<
+				Graphics::BindlessSet<Graphics::TextureSampler>::Instance>::FromCall(&findBindlessTextureSamplers);
+		}
+		Stacktor<Reference<Graphics::BindingSet>, 4u> bindingSets;
+		for (size_t i = 0u; i < computePipeline->BindingSetCount(); i++) {
+			bindingSetDescriptor.bindingSetId = i;
+			const Reference<Graphics::BindingSet> bindingSet = bindingPool->AllocateBindingSet(bindingSetDescriptor);
+			if (bindingSet == nullptr)
+				return fail("Failed to allocate binding set ", i, "! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+			bindingSets.Push(bindingSet);
 		}
 
-		// Create pipeline:
-		const Reference<PipelineDescriptor> pipelineDescriptor = Object::Instantiate<PipelineDescriptor>(&bindingSetDescriptors, shader);
-		const Reference<Graphics::ComputePipeline> pipeline = context->Graphics()->Device()->CreateComputePipeline(
-			pipelineDescriptor, context->Graphics()->Configuration().MaxInFlightCommandBufferCount());
-		if (pipeline == nullptr) return fail("Compute Pipeline could not be created! [File: ", __FILE__, "; Line: ", __LINE__, "]");
-
 		// Create CombinedGraphicsSimulationKernel Object:
-		const Reference<CombinedGraphicsSimulationKernel> instance = new CombinedGraphicsSimulationKernel(context, bindingSet.taskDescriptorBinding, pipelineDescriptor, pipeline);
+		const Reference<CombinedGraphicsSimulationKernel> instance = new CombinedGraphicsSimulationKernel(
+			context, taskDescriptorBinding, computePipeline, std::move(bindingSets));
 		instance->ReleaseRef();
 		return instance;
 	}
@@ -257,7 +220,7 @@ namespace Jimara {
 		}
 
 		// Store memory on CPU side and update kernel size:
-		{
+		const uint32_t threadCount = [&]() {
 			uint32_t numberOfThreads = 0u;
 			TaskDescriptor* descPtr = m_lastTaskDescriptors.data();
 			const GraphicsSimulation::Task* const* taskPtr = tasks;
@@ -282,11 +245,8 @@ namespace Jimara {
 				else taskCount--;
 				taskPtr++;
 			}
-			if (m_pipelineDescriptor->ThreadCount() != numberOfThreads) {
-				m_pipelineDescriptor->SetThreadCount(numberOfThreads);
-				taskDescriptorBufferDirty = true;
-			}
-		}
+			return numberOfThreads;
+		}();
 
 		// (Re)Allocate buffer if needed:
 		if (m_taskDescriptorBinding->BoundObject() == nullptr || m_taskDescriptorBinding->BoundObject()->ObjectCount() != taskCount) {
@@ -307,6 +267,15 @@ namespace Jimara {
 		}
 
 		// Execute pipeline:
-		m_pipeline->Execute(commandBufferInfo);
+		for (size_t i = 0u; i < m_bindingSets.Size(); i++) {
+			Graphics::BindingSet* const set = m_bindingSets[i];
+			set->Update(commandBufferInfo);
+			set->Bind(commandBufferInfo);
+		}
+		{
+			const constexpr size_t BLOCK_SIZE = 256u;
+			const Size3 blockCount = Size3((threadCount + BLOCK_SIZE - 1u) / BLOCK_SIZE, 1u, 1u);
+			m_computePipeline->Dispatch(commandBufferInfo, blockCount);
+		}
 	}
 }
