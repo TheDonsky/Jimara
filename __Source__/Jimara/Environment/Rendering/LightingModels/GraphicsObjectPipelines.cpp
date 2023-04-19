a5beeb7459d34b35dfa3bc9494db3e57577c68e8
TheDonsky <nikadon.net@gmail.com>
2023-04-19 15:14:49 +0400

    Define basic job graph

--- __Source__/Jimara/Environment/Rendering/LightingModels/GraphicsObjectPipelines.cpp
+++ __Source__/Jimara/Environment/Rendering/LightingModels/GraphicsObjectPipelines.cpp
@@ -3,15 +3,47 @@
 
 namespace Jimara {
 	struct GraphicsObjectPipelines::Helpers {
+		class BaseJob;
+
 		class DescriptorPools;
 		class PipelineCreationJob;
 		class DescriptorSetUpdateJob;
+		class FrameCounterJob;
 
 		class GraphicsObjectDescriptorManager;
 		class GraphicsObjectDescriptorManagerCache;
+		class GraphicsObjectDescriptorManagerCleanupJob;
 
-		class InstanceManager;
-		class InstanceManagerCache;
+		class PerContextData;
+		class PerContextDataCache;
+	};
+
+
+
+
+
+	class GraphicsObjectPipelines::Helpers::BaseJob : public virtual JobSystem::Job {
+	private:
+		const std::shared_ptr<const std::atomic<size_t>> m_frameCounter;
+		std::atomic<size_t> m_lastFrameId;
+
+	protected:
+		inline BaseJob(const std::shared_ptr<const std::atomic<size_t>>& frameCounter)
+			: m_frameCounter(frameCounter) {
+			assert(m_frameCounter != nullptr);
+			m_lastFrameId = m_frameCounter->load() - 1u;
+		}
+
+		inline virtual ~BaseJob() {}
+
+		virtual void Run() = 0;
+
+		inline virtual void Execute() final override {
+			const size_t frameId = m_frameCounter->load() - 1u;
+			if (m_lastFrameId == frameId) return;
+			m_lastFrameId = frameId;
+			Run();
+		}
 	};
 
 
@@ -25,7 +57,7 @@ namespace Jimara {
 
 		inline DescriptorPools(std::vector<Reference<Graphics::BindingPool>>&& pools) : m_pools(std::move(pools)) {}
 	public:
-		inline Reference<DescriptorPools> Create(SceneContext* context, size_t poolCount = 0u) {
+		inline static Reference<DescriptorPools> Create(SceneContext* context, size_t poolCount = 0u) {
 			if (poolCount <= 0u)
 				poolCount = Math::Max(size_t(1u), size_t(std::thread::hardware_concurrency() >> 1u));
 			std::vector<Reference<Graphics::BindingPool>> pools;
@@ -60,58 +92,65 @@ namespace Jimara {
 
 
 
-	class GraphicsObjectPipelines::Helpers::PipelineCreationJob : public virtual JobSystem::Job {
-	private:
-		const Reference<DescriptorPools> m_descriptorPools;
-
-	public:
-		inline PipelineCreationJob(DescriptorPools* pools) 
-			: m_descriptorPools(pools) { 
-			assert(m_descriptorPools != nullptr);
-		}
-
-		inline virtual ~PipelineCreationJob() {}
-
-	protected:
-		virtual void Execute() override {
-			// __TODO__: Implement this crap!
-		}
-		virtual void CollectDependencies(Callback<Job*> addDependency) override { }
-	};
-
-
-
-
-
-	class GraphicsObjectPipelines::Helpers::DescriptorSetUpdateJob : public virtual JobSystem::Job {
+	class GraphicsObjectPipelines::Helpers::DescriptorSetUpdateJob : public virtual BaseJob {
 	private:
 		const Reference<Scene::GraphicsContext> m_context;
 		const Reference<Graphics::BindingPool> m_pool;
-		const std::shared_ptr<const std::atomic<size_t>> m_frameCounter;
-		std::atomic<size_t> m_lastFrameId;
+		const Reference<JobSystem::Job> m_objectListCleanupJob;
+		
 
 	public:
 		inline DescriptorSetUpdateJob(
 			Scene::GraphicsContext* context, 
 			Graphics::BindingPool* pool, 
-			std::shared_ptr<const std::atomic<size_t>> frameCounter)
-			: m_context(context), m_pool(pool), m_frameCounter(frameCounter) {
+			JobSystem::Job* objectListCleanupJob,
+			const std::shared_ptr<const std::atomic<size_t>>& frameCounter)
+			: BaseJob(frameCounter)
+			, m_context(context), m_pool(pool), m_objectListCleanupJob(objectListCleanupJob) {
 			assert(m_context != nullptr);
 			assert(m_pool != nullptr); 
-			assert(m_frameCounter != nullptr);
-			m_lastFrameId = m_frameCounter->load() - 1u;
+			assert(m_objectListCleanupJob != nullptr);
 		}
 		inline virtual ~DescriptorSetUpdateJob() {}
 
 	protected:
-		virtual void Execute() override { 
-			const size_t frameId = m_frameCounter->load() - 1u;
-			if (m_lastFrameId == frameId) return;
-			m_lastFrameId = frameId;
+		virtual void Run() final override { 
 			m_pool->UpdateAllBindingSets(m_context->InFlightCommandBufferIndex());
 		}
-		virtual void CollectDependencies(Callback<Job*> addDependency) override {
-			// __TODO__: These should run after pipeline creation tasks...
+		virtual void CollectDependencies(Callback<JobSystem::Job*> addDependency) override {
+			addDependency(m_objectListCleanupJob);
+		}
+	};
+
+
+
+
+	class GraphicsObjectPipelines::Helpers::FrameCounterJob : public virtual JobSystem::Job {
+	private:
+		const std::shared_ptr<std::atomic<size_t>> m_frameCounter;
+		const std::vector<Reference<JobSystem::Job>> m_descriptorUpdateJobs;
+		std::atomic<size_t> m_lastFrameId;
+
+	public:
+		inline FrameCounterJob(
+			const std::shared_ptr<std::atomic<size_t>>& frameCounter,
+			const std::vector<Reference<JobSystem::Job>>& descriptorUpdateJobs)
+			: m_frameCounter(frameCounter)
+			, m_descriptorUpdateJobs(descriptorUpdateJobs) {
+			assert(m_frameCounter != nullptr);
+			m_lastFrameId = m_frameCounter->load() - 1u;
+		}
+		inline virtual ~FrameCounterJob() {}
+		inline virtual void Execute() final override { 
+			(*m_frameCounter.operator->())++; 
+		}
+		virtual void CollectDependencies(Callback<Job*> addDependency) {
+			const Reference<JobSystem::Job>* ptr = m_descriptorUpdateJobs.data();
+			const Reference<JobSystem::Job>* const end = ptr + m_descriptorUpdateJobs.size();
+			while (ptr < end) {
+				addDependency(*ptr);
+				ptr++;
+			}
 		}
 	};
 
@@ -119,12 +158,41 @@ namespace Jimara {
 
 
 
+	class GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManagerCleanupJob : public virtual BaseJob {
+	private:
+		const std::vector<Reference<JobSystem::Job>> m_pipelineCretionJobs;
+		EventInstance<> m_onCleanup;
+
+	public:
+		inline GraphicsObjectDescriptorManagerCleanupJob(
+			const std::vector<Reference<JobSystem::Job>>& creationJobs,
+			const std::shared_ptr<const std::atomic<size_t>>& frameCounter)
+			: BaseJob(frameCounter), m_pipelineCretionJobs(creationJobs) {}
+
+		inline virtual ~GraphicsObjectDescriptorManagerCleanupJob() {}
+
+		inline Event<>& OnCleanup() { return m_onCleanup; }
+
+	protected:
+		virtual void Run() final override { m_onCleanup(); }
+		virtual void CollectDependencies(Callback<JobSystem::Job*> addDependency) override {
+			const Reference<JobSystem::Job>* ptr = m_pipelineCretionJobs.data();
+			const Reference<JobSystem::Job>* end = ptr + m_pipelineCretionJobs.size();
+			while (ptr < end) {
+				addDependency(*ptr);
+				ptr++;
+			}
+		}
+	};
 	class GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager 
 		: public virtual ObjectCache<Reference<const Jimara::Object>>::StoredObject {
 	private:
 		const Reference<const GraphicsObjectDescriptor::Set> m_set;
+		const Reference<GraphicsObjectDescriptorManagerCleanupJob> m_cleanupJob;
 		std::vector<Reference<Jimara::GraphicsObjectDescriptor>> m_added;
 		std::vector<Reference<Jimara::GraphicsObjectDescriptor>> m_removed;
+		std::mutex m_allLock;
+		std::vector<Reference<Jimara::GraphicsObjectDescriptor>> m_all;
 
 		void OnAdded(Jimara::GraphicsObjectDescriptor* const* elems, size_t count) {
 			m_added.clear();
@@ -142,35 +210,66 @@ namespace Jimara {
 				elems++;
 			}
 		}
+		void OnFlushed() {
+			std::unique_lock<std::mutex> m_allLock;
+			m_all.clear();
+		}
+		void StoreAllElements() {
+			std::unique_lock<std::mutex> m_allLock;
+			if (!m_all.empty()) return;
+			m_set->GetAll([&](Jimara::GraphicsObjectDescriptor* elem) { m_all.push_back(elem); });
+		}
 
 	public:
-		inline GraphicsObjectDescriptorManager(const GraphicsObjectDescriptor::Set* set) : m_set(set) {
+		inline GraphicsObjectDescriptorManager(
+			const GraphicsObjectDescriptor::Set* set,
+			GraphicsObjectDescriptorManagerCleanupJob* cleanupJob) 
+			: m_set(set), m_cleanupJob(cleanupJob) {
 			assert(m_set != nullptr);
 			m_set->OnAdded() += Callback(&GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager::OnAdded, this);
-			m_set->OnRemoved() += Callback(&GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager::OnAdded, this);
+			m_set->OnRemoved() += Callback(&GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager::OnRemoved, this);
+			m_set->OnFlushed() += Callback(&GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager::OnFlushed, this);
+			m_cleanupJob->OnCleanup() += Callback(&GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager::Clear, this);
 		}
 
 		inline virtual ~GraphicsObjectDescriptorManager() {
 			m_set->OnAdded() -= Callback(&GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager::OnAdded, this);
-			m_set->OnRemoved() -= Callback(&GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager::OnAdded, this);
+			m_set->OnRemoved() -= Callback(&GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager::OnRemoved, this);
+			m_set->OnFlushed() -= Callback(&GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager::OnFlushed, this);
+			m_cleanupJob->OnCleanup() -= Callback(&GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManager::Clear, this);
 			Clear();
 		}
 
 		inline void Clear() {
-			m_added.clear();
-			m_removed.clear();
+			OnAdded(nullptr, 0u);
+			OnRemoved(nullptr, 0u);
+			OnFlushed();
+		}
+
+		inline size_t AddedElementCount()const { return m_added.size(); }
+		inline const Reference<Jimara::GraphicsObjectDescriptor>* AddedElements()const { return m_added.data(); }
+
+		inline size_t RemovedElementCount()const { return m_removed.size(); }
+		inline const Reference<Jimara::GraphicsObjectDescriptor>* RemovedElements()const { return m_removed.data(); }
+
+		inline size_t LiveCount() {
+			StoreAllElements();
+			return m_all.size();
+		}
+		inline const Reference<Jimara::GraphicsObjectDescriptor>* LiveElements() {
+			StoreAllElements();
+			return m_all.data();
 		}
 	};
 	class GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManagerCache 
 		: public virtual ObjectCache<Reference<const Jimara::Object>> {
 	public:
 		inline virtual ~GraphicsObjectDescriptorManagerCache() {}
-		Reference<GraphicsObjectDescriptorManager> Get(GraphicsObjectDescriptor::Set* set) {
+		Reference<GraphicsObjectDescriptorManager> Get(
+			GraphicsObjectDescriptor::Set* set,
+			GraphicsObjectDescriptorManagerCleanupJob* cleanupJob) {
 			return GetCachedOrCreate(set, false, [&]() -> Reference<GraphicsObjectDescriptorManager> {
-				// __TODO__: Implement this crap!
-				set->Context()->Log()->Error(
-					"GraphicsObjectPipelines::Helpers::GraphicsObjectDescriptorManagerCache::Get - ",
-					"Not yet implemented! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+				Object::Instantiate<GraphicsObjectDescriptorManager>(set, cleanupJob);
 				});
 		}
 	};
@@ -179,21 +278,81 @@ namespace Jimara {
 
 
 
-	class GraphicsObjectPipelines::Helpers::InstanceManager 
+	class GraphicsObjectPipelines::Helpers::PipelineCreationJob : public virtual BaseJob {
+	private:
+		const Reference<DescriptorPools> m_descriptorPools;
+
+	public:
+		inline PipelineCreationJob(
+			DescriptorPools* pools,
+			const std::shared_ptr<const std::atomic<size_t>>& frameCounter)
+			: BaseJob(frameCounter),
+			m_descriptorPools(pools) {
+			assert(m_descriptorPools != nullptr);
+		}
+
+		inline virtual ~PipelineCreationJob() {}
+
+	protected:
+		virtual void Run() final override {
+			// __TODO__: Implement this crap!
+		}
+		virtual void CollectDependencies(Callback<Job*> addDependency) override { }
+	};
+
+
+
+
+
+	class GraphicsObjectPipelines::Helpers::PerContextData
 		: public virtual ObjectCache<Reference<const Jimara::Object>>::StoredObject {
 	private:
+		const Reference<SceneContext> m_context;
+		const Reference<DescriptorPools> m_descriptorPools;
+		const Reference<JobSystem::Job> m_frameCounterJob;
 
 	public:
+		inline PerContextData(
+			SceneContext* context,
+			DescriptorPools* descriptorPools,
+			JobSystem::Job* frameCounterJob)
+			: m_context(context)
+			, m_descriptorPools(descriptorPools)
+			, m_frameCounterJob(frameCounterJob) {
+			assert(m_context != nullptr);
+			assert(m_descriptorPools != nullptr);
+			assert(m_frameCounterJob != nullptr);
+			m_context->Graphics()->RenderJobs().Add(m_frameCounterJob);
+		}
 
+		virtual inline ~PerContextData() {
+			m_context->Graphics()->RenderJobs().Remove(m_frameCounterJob);
+		}
 	};
-	class GraphicsObjectPipelines::Helpers::InstanceManagerCache 
+	class GraphicsObjectPipelines::Helpers::PerContextDataCache
 		: public virtual ObjectCache<Reference<const Jimara::Object>> {
 	public:
-		static Reference<InstanceManager> Get(SceneContext* context) {
-			static InstanceManagerCache cache;
-			return cache.GetCachedOrCreate(context, false, [&]() -> Reference<InstanceManager> {
+		static Reference<PerContextData> Get(SceneContext* context) {
+			static PerContextDataCache cache;
+			return cache.GetCachedOrCreate(context, false, [&]() -> Reference<PerContextData> {
+				const Reference<DescriptorPools> pools = DescriptorPools::Create(context);
+				if (pools == nullptr) return nullptr;
+
+				const std::shared_ptr<std::atomic<size_t>> frameCounter = std::make_shared<std::atomic<size_t>>();
+
+				std::vector<Reference<JobSystem::Job>> pipelineCreationJobs;
+				for (size_t i = 0u; i < pools->PoolCount(); i++)
+					pipelineCreationJobs.push_back(Object::Instantiate<PipelineCreationJob>(pools, frameCounter));
+				const Reference<GraphicsObjectDescriptorManagerCleanupJob> cleanupJob =
+					Object::Instantiate<GraphicsObjectDescriptorManagerCleanupJob>(pipelineCreationJobs, frameCounter);
+
+				std::vector<Reference<JobSystem::Job>> bindingSetUpdateJobs;
+				for (size_t i = 0u; i < pools->PoolCount(); i++)
+					bindingSetUpdateJobs.push_back(Object::Instantiate<DescriptorSetUpdateJob>(context->Graphics(), pools->Pool(i), cleanupJob, frameCounter));
+				const Reference<FrameCounterJob> frameCounterJob = Object::Instantiate<FrameCounterJob>(frameCounter, bindingSetUpdateJobs);
+
 				// __TODO__: Implement this crap!
-				context->Log()->Error("GraphicsObjectPipelines::Helpers::ManagerCache::Get - Not yet implemented! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+				context->Log()->Error("GraphicsObjectPipelines::Helpers::PerContextDataCache::Get - Not yet implemented! [File: ", __FILE__, "; Line: ", __LINE__, "]");
 				return nullptr;
 				});
 		}
@@ -206,7 +365,7 @@ namespace Jimara {
 	Reference<GraphicsObjectPipelines> GraphicsObjectPipelines::Get(const Descriptor& desc) {
 		if (desc.descriptorSet == nullptr) return nullptr;
 
-		const Reference<Helpers::InstanceManager> manager = Helpers::InstanceManagerCache::Get(desc.descriptorSet->Context());
+		const Reference<Helpers::PerContextData> manager = Helpers::PerContextDataCache::Get(desc.descriptorSet->Context());
 		if (manager == nullptr) return nullptr;
 
 		desc.descriptorSet->Context()->Log()->Error("GraphicsObjectPipelines::Get - Not yet implemented! [File: ", __FILE__, "; Line: ", __LINE__, "]");
