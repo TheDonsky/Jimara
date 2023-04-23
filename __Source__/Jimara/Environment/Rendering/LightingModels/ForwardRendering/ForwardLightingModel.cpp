f13bb51a30a2c8f4e5bcb23cb50333ac383bed62
TheDonsky <nikadon.net@gmail.com>
2023-04-23 16:04:22 +0400

    Use new GraphicsObjectPipelines in ForwardLightingModel instead of the legacy ones

--- __Source__/Jimara/Environment/Rendering/LightingModels/ForwardRendering/ForwardLightingModel.cpp
+++ __Source__/Jimara/Environment/Rendering/LightingModels/ForwardRendering/ForwardLightingModel.cpp
@@ -1,5 +1,5 @@
 #include "ForwardLightingModel.h"
-#include "../LightingModelPipelines.h"
+#include "../GraphicsObjectPipelines.h"
 #include "../../SceneObjects/Lights/LightDataBuffer.h"
 #include "../../SceneObjects/Lights/LightTypeIdBuffer.h"
 #include "../../../../Graphics/Data/GraphicsPipelineSet.h"
@@ -7,112 +7,61 @@
 
 namespace Jimara {
 	namespace {
-		/** ENVIRONMENT SHAPE DESCRIPTOR: */
-		class EnvironmentShapeDescriptor : public virtual Graphics::ShaderResourceBindings::ShaderResourceBindingSet {
-		protected:
-			const Reference<Graphics::ShaderResourceBindings::NamedBindlessTextureSamplerSetBinding> jimara_BindlessTextures =
-				Object::Instantiate<Graphics::ShaderResourceBindings::NamedBindlessTextureSamplerSetBinding>("jimara_BindlessTextures");
-			const Reference<Graphics::ShaderResourceBindings::NamedBindlessStructuredBufferSetBinding> jimara_BindlessBuffers =
-				Object::Instantiate<Graphics::ShaderResourceBindings::NamedBindlessStructuredBufferSetBinding>("jimara_BindlessBuffers");
-			const Reference<Graphics::ShaderResourceBindings::NamedStructuredBufferBinding> jimara_LightDataBinding =
-				Object::Instantiate<Graphics::ShaderResourceBindings::NamedStructuredBufferBinding>("jimara_LightDataBinding");
-			const Reference<Graphics::ShaderResourceBindings::NamedStructuredBufferBinding> jimara_ForwardRenderer_LightTypeIds =
-				Object::Instantiate<Graphics::ShaderResourceBindings::NamedStructuredBufferBinding>("jimara_ForwardRenderer_LightTypeIds");
-			const Reference<Graphics::ShaderResourceBindings::NamedConstantBufferBinding> jimara_ForwardRenderer_ViewportBuffer =
-				Object::Instantiate<Graphics::ShaderResourceBindings::NamedConstantBufferBinding>("jimara_ForwardRenderer_ViewportBuffer");
-
-		public:
-			inline virtual Reference<const Graphics::ShaderResourceBindings::ConstantBufferBinding> FindConstantBufferBinding(const std::string_view& name)const override {
-				if (name == jimara_ForwardRenderer_ViewportBuffer->BindingName()) return jimara_ForwardRenderer_ViewportBuffer;
-				else return nullptr;
-			}
-
-			inline virtual Reference<const Graphics::ShaderResourceBindings::StructuredBufferBinding> FindStructuredBufferBinding(const std::string_view& name)const override {
-				if (name == jimara_LightDataBinding->BindingName()) return jimara_LightDataBinding;
-				else if (name == jimara_ForwardRenderer_LightTypeIds->BindingName()) return jimara_ForwardRenderer_LightTypeIds;
-				else return nullptr;
-			}
-
-			inline virtual Reference<const Graphics::ShaderResourceBindings::TextureSamplerBinding> FindTextureSamplerBinding(const std::string_view&)const override {
-				return nullptr;
-			}
-			inline virtual Reference<const Graphics::ShaderResourceBindings::TextureViewBinding> FindTextureViewBinding(const std::string_view&)const override {
-				return nullptr;
-			}
-			inline virtual Reference<const Graphics::ShaderResourceBindings::BindlessStructuredBufferSetBinding> FindBindlessStructuredBufferSetBinding(const std::string_view& name)const override {
-				if (name == jimara_BindlessBuffers->BindingName()) return jimara_BindlessBuffers;
-				else return nullptr;
-			}
-
-			inline virtual Reference<const Graphics::ShaderResourceBindings::BindlessTextureSamplerSetBinding> FindBindlessTextureSamplerSetBinding(const std::string_view& name)const override {
-				if (name == jimara_BindlessTextures->BindingName()) return jimara_BindlessTextures;
-				else return nullptr;
-			}
-			inline virtual Reference<const Graphics::ShaderResourceBindings::BindlessTextureViewSetBinding> FindBindlessTextureViewSetBinding(const std::string_view&)const override {
-				return nullptr;
-			}
-
-			static const EnvironmentShapeDescriptor& Instance() {
-				static EnvironmentShapeDescriptor instance;
-				return instance;
-			}
-		};
-
-
-		
-		/** CONCRETE ENVIRONMENT BINDINGS: */
-		class EnvironmentDescriptor : public virtual EnvironmentShapeDescriptor {
-		private:
-			struct ViewportBuffer_t {
-				alignas(16) Matrix4 view;
-				alignas(16) Matrix4 projection;
-			};
-
-			const Reference<const ViewportDescriptor> m_viewport;
-			const Reference<LightDataBuffer> m_lightDataBuffer;
-			const Reference<LightTypeIdBuffer> m_lightTypeIdBuffer;
-			const Graphics::BufferReference<ViewportBuffer_t> m_viewportBuffer;
-
-		public:
-			inline EnvironmentDescriptor(const ViewportDescriptor* viewport)
-				: m_viewport(viewport)
-				, m_lightDataBuffer(LightDataBuffer::Instance(viewport))
-				, m_lightTypeIdBuffer(LightTypeIdBuffer::Instance(viewport))
-				, m_viewportBuffer(viewport->Context()->Graphics()->Device()->CreateConstantBuffer<ViewportBuffer_t>()) {
-				if (m_viewportBuffer == nullptr) m_viewport->Context()->Log()->Fatal("ForwardLightingModel - Could not create Viewport Buffer!");
-				jimara_BindlessTextures->BoundObject() = m_viewport->Context()->Graphics()->Bindless().SamplerBinding();
-				jimara_BindlessBuffers->BoundObject() = m_viewport->Context()->Graphics()->Bindless().BufferBinding();
-				jimara_ForwardRenderer_ViewportBuffer->BoundObject() = m_viewportBuffer;
-			}
-
-			inline void Update() {
-				jimara_LightDataBinding->BoundObject() = m_lightDataBuffer->Buffer();
-				jimara_ForwardRenderer_LightTypeIds->BoundObject() = m_lightTypeIdBuffer->Buffer();
-				ViewportBuffer_t& buffer = m_viewportBuffer.Map();
-				buffer.view = m_viewport->ViewMatrix();
-				buffer.projection = m_viewport->ProjectionMatrix();
-				m_viewportBuffer->Unmap(true);
-			}
-
-			inline void GetDependencies(Callback<JobSystem::Job*> report) {
-				report(m_lightDataBuffer);
-				report(m_lightTypeIdBuffer);
-			}
-		};
-
-
-
-
-
 		/** FORWARD RENDERER: */
-
 		class ForwardRenderer : public virtual RenderStack::Renderer {
 		private:
 			const Reference<const ViewportDescriptor> m_viewport;
 			const LayerMask m_layerMask;
 			const Graphics::RenderPass::Flags m_clearAndResolveFlags;
-			const Reference<LightingModelPipelines> m_pipelineObjects;
-			EnvironmentDescriptor m_environmentDescriptor;
+
+			struct Bindings {
+				struct ViewportBuffer_t {
+					alignas(16) Matrix4 view;
+					alignas(16) Matrix4 projection;
+				};
+				const Reference<Graphics::ResourceBinding<Graphics::BindlessSet<Graphics::TextureSampler>::Instance>> jimara_BindlessTextures =
+					Object::Instantiate<Graphics::ResourceBinding<Graphics::BindlessSet<Graphics::TextureSampler>::Instance>>();
+				const Reference<Graphics::ResourceBinding<Graphics::BindlessSet<Graphics::ArrayBuffer>::Instance>> jimara_BindlessBuffers =
+					Object::Instantiate<Graphics::ResourceBinding<Graphics::BindlessSet<Graphics::ArrayBuffer>::Instance>>();
+				const Reference<Graphics::ResourceBinding<Graphics::ArrayBuffer>> jimara_LightDataBinding =
+					Object::Instantiate<Graphics::ResourceBinding<Graphics::ArrayBuffer>>();
+				const Reference<Graphics::ResourceBinding<Graphics::ArrayBuffer>> jimara_ForwardRenderer_LightTypeIds =
+					Object::Instantiate<Graphics::ResourceBinding<Graphics::ArrayBuffer>>();
+				const Reference<Graphics::ResourceBinding<Graphics::Buffer>> jimara_ForwardRenderer_ViewportBuffer =
+					Object::Instantiate<Graphics::ResourceBinding<Graphics::Buffer>>();
+
+				const Reference<LightDataBuffer> m_lightDataBuffer;
+				const Reference<LightTypeIdBuffer> m_lightTypeIdBuffer;
+				const Graphics::BufferReference<ViewportBuffer_t> m_viewportBuffer;
+
+				inline Bindings(const ViewportDescriptor* viewport)
+					: m_lightDataBuffer(LightDataBuffer::Instance(viewport))
+					, m_lightTypeIdBuffer(LightTypeIdBuffer::Instance(viewport))
+					, m_viewportBuffer(viewport->Context()->Graphics()->Device()->CreateConstantBuffer<ViewportBuffer_t>()) {
+					if (m_viewportBuffer == nullptr) viewport->Context()->Log()->Fatal("ForwardLightingModel - Could not create Viewport Buffer!");
+					jimara_BindlessTextures->BoundObject() = viewport->Context()->Graphics()->Bindless().SamplerBinding();
+					jimara_BindlessBuffers->BoundObject() = viewport->Context()->Graphics()->Bindless().BufferBinding();
+					jimara_ForwardRenderer_ViewportBuffer->BoundObject() = m_viewportBuffer;
+				}
+
+				inline void Update(const ViewportDescriptor* viewport) {
+					jimara_LightDataBinding->BoundObject() = m_lightDataBuffer->Buffer();
+					jimara_ForwardRenderer_LightTypeIds->BoundObject() = m_lightTypeIdBuffer->Buffer();
+					ViewportBuffer_t& buffer = m_viewportBuffer.Map();
+					buffer.view = viewport->ViewMatrix();
+					buffer.projection = viewport->ProjectionMatrix();
+					m_viewportBuffer->Unmap(true);
+				}
+
+				inline void GetDependencies(Callback<JobSystem::Job*> report) {
+					report(m_lightDataBuffer);
+					report(m_lightTypeIdBuffer);
+				}
+			} m_bindings;
+
+			Reference<Graphics::BindingPool> m_bindingPool;
+			Reference<GraphicsObjectPipelines> m_graphicsObjectPipelines;
+			Stacktor<Reference<Graphics::BindingSet>, 4u> m_environmentBindingSets;
 
 			
 			struct {
@@ -121,10 +70,6 @@ namespace Jimara {
 				Graphics::Texture::Multisampling sampleCount = Graphics::Texture::Multisampling::MAX_AVAILABLE;
 			} m_renderPass;
 
-			struct {
-				Reference<Graphics::Pipeline> environmentPipeline;
-				Reference<LightingModelPipelines::Instance> objectPipelines;
-			} m_pipelines;
 
 			struct {
 				Reference<RenderImages> renderImages;
@@ -136,28 +81,82 @@ namespace Jimara {
 				Graphics::Texture::PixelFormat depthFormat,
 				Graphics::Texture::Multisampling sampleCount) {
 				
-				if (m_pipelines.objectPipelines != nullptr && 
+				if (m_graphicsObjectPipelines != nullptr &&				
 					m_renderPass.pixelFormat == pixelFormat && 
 					m_renderPass.sampleCount == sampleCount) return true;
-				
+
+				auto fail = [&](const auto&... message) {
+					m_graphicsObjectPipelines = nullptr;
+					m_environmentBindingSets.Clear();
+					m_viewport->Context()->Log()->Error("ForwardRenderer::RefreshRenderPass - ", message...);
+					return false;
+				};
+
+				// Make sure we have a binding pool:
+				if (m_bindingPool == nullptr) {
+					m_bindingPool = m_viewport->Context()->Graphics()->Device()->CreateBindingPool(
+						m_viewport->Context()->Graphics()->Configuration().MaxInFlightCommandBufferCount());
+					if (m_bindingPool == nullptr)
+						return fail("Failed to create a binding pool! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+				}
+
+				// Get GraphicsObjectPipelines: 
+				{
+					const Reference<Graphics::RenderPass> renderPass = m_viewport->Context()->Graphics()->Device()->GetRenderPass(
+						sampleCount, 1u, &pixelFormat, depthFormat, m_clearAndResolveFlags);
+					if (renderPass == nullptr)
+						return fail("Failed to create/get render pass! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+
+					GraphicsObjectPipelines::Descriptor desc = {};
+					{
+						desc.descriptorSet = GraphicsObjectDescriptor::Set::GetInstance(m_viewport->Context());
+						desc.viewportDescriptor = m_viewport;
+						desc.renderPass = renderPass;
+						desc.layers = m_layerMask;
+						desc.lightingModel = OS::Path("Jimara/Environment/Rendering/LightingModels/ForwardRendering/Jimara_ForwardRenderer.jlm");
+					}
+					m_graphicsObjectPipelines = GraphicsObjectPipelines::Get(desc);
+					if (m_graphicsObjectPipelines == nullptr)
+						return fail("Failed to create/get GraphicsObjectPipelines! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+				}
+
+				// Create binding sets:
+				{
+					m_environmentBindingSets.Clear();
+					Graphics::BindingSet::Descriptor desc = {};
+					
+					desc.pipeline = m_graphicsObjectPipelines->EnvironmentPipeline();
+
+					auto findConstantBuffer = [&](const auto& info) {
+						return (info.name == "jimara_ForwardRenderer_ViewportBuffer") ? m_bindings.jimara_ForwardRenderer_ViewportBuffer : nullptr;
+					};
+					desc.find.constantBuffer = &findConstantBuffer;
+
+					auto findStructuredBuffer = [&](const auto& info) {
+						return
+							(info.name == "jimara_LightDataBinding") ? m_bindings.jimara_LightDataBinding :
+							(info.name == "jimara_ForwardRenderer_LightTypeIds") ? m_bindings.jimara_ForwardRenderer_LightTypeIds : nullptr;
+					};
+					desc.find.structuredBuffer = &findStructuredBuffer;
+
+					auto findBindlessArrays = [&](const auto&) { return m_bindings.jimara_BindlessBuffers; };
+					desc.find.bindlessStructuredBuffers = &findBindlessArrays;
+
+					auto findBindlessTextures = [&](const auto&) { return m_bindings.jimara_BindlessTextures; };
+					desc.find.bindlessTextureSamplers = &findBindlessTextures;
+
+					for (size_t i = 0u; i < desc.pipeline->BindingSetCount(); i++) {
+						desc.bindingSetId = i;
+						const Reference<Graphics::BindingSet> bindingSet = m_bindingPool->AllocateBindingSet(desc);
+						if (bindingSet == nullptr)
+							return fail("Failed to allocate binding set ", i, "! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+						m_environmentBindingSets.Push(bindingSet);
+					}
+				}
+
 				m_renderPass.pixelFormat = pixelFormat;
 				m_renderPass.depthFormat = depthFormat;
 				m_renderPass.sampleCount = sampleCount;
-
-				LightingModelPipelines::RenderPassDescriptor renderPassDesc = {};
-				{
-					renderPassDesc.sampleCount = sampleCount;
-					renderPassDesc.colorAttachmentFormats.Push(pixelFormat);
-					renderPassDesc.depthFormat = depthFormat;
-					renderPassDesc.renderPassFlags = m_clearAndResolveFlags;
-				}
-				
-				m_pipelines.objectPipelines = m_pipelineObjects->GetInstance(renderPassDesc);
-				if (m_pipelines.objectPipelines == nullptr) {
-					m_viewport->Context()->Log()->Error(
-						"ForwardRenderer::RefreshRenderPass - Failed to create LightingModelPipelines::Instance! [File: ", __FILE__, "; Line: ", __LINE__, "]");
-					return false;
-				}
 
 				return true;
 			}
@@ -179,8 +178,8 @@ namespace Jimara {
 					depthAttachment->TargetTexture()->ImageFormat(),
 					images->SampleCount())) return nullptr;
 
-				m_lastFrameBuffer.frameBuffer = m_pipelines.objectPipelines->RenderPass()->CreateFrameBuffer(
-					&colorAttachment, depthAttachment, &resolveAttachment, depthResolve);
+				m_lastFrameBuffer.frameBuffer = m_graphicsObjectPipelines->RenderPass()
+					->CreateFrameBuffer(&colorAttachment, depthAttachment, &resolveAttachment, depthResolve);
 				if (m_lastFrameBuffer.frameBuffer == nullptr)
 					m_viewport->Context()->Log()->Error("ForwardRenderer::RefreshRenderPass - Failed to create the frame buffer!");
 				else m_lastFrameBuffer.renderImages = images;
@@ -193,29 +192,18 @@ namespace Jimara {
 				: m_viewport(viewport)
 				, m_layerMask(layers)
 				, m_clearAndResolveFlags(flags)
-				, m_pipelineObjects([&]()->Reference<LightingModelPipelines> {
-				LightingModelPipelines::Descriptor desc = {};
-				{
-					desc.viewport = viewport;
-					desc.layers = layers;
-					desc.lightingModel = OS::Path("Jimara/Environment/Rendering/LightingModels/ForwardRendering/Jimara_ForwardRenderer.jlm");
-				}
-				return LightingModelPipelines::Get(desc);
-					}())
-				, m_environmentDescriptor(viewport) {
-				m_pipelines.environmentPipeline = m_pipelineObjects->CreateEnvironmentPipeline(m_environmentDescriptor);
-				if (m_pipelines.environmentPipeline == nullptr) 
-					m_viewport->Context()->Log()->Error("ForwardRenderer::ForwardRenderer - Failed to create environment pipeline!");
-			}
+				, m_bindings(viewport) {}
 
 			inline virtual ~ForwardRenderer() {}
 
 			inline virtual void Render(Graphics::InFlightBufferInfo commandBufferInfo, RenderImages* images) final override {
-				if (images == nullptr || m_pipelines.environmentPipeline == nullptr) return;
+				if (images == nullptr) return;
 
 				// Get frame buffer:
+				Reference<Graphics::FrameBuffer> oldFrameBufer = m_lastFrameBuffer.frameBuffer;
 				Reference<Graphics::FrameBuffer> frameBuffer = RefreshFrameBuffer(images);
-				if (frameBuffer == nullptr || m_pipelines.objectPipelines == nullptr) return;
+				if (frameBuffer == nullptr || m_graphicsObjectPipelines == nullptr || oldFrameBufer != frameBuffer) 
+					return;
 
 				// Verify resolution:
 				{
@@ -227,29 +215,33 @@ namespace Jimara {
 				{
 					const Vector4 clearColor = m_viewport->ClearColor();
 					const Vector4* CLEAR_VALUE = &clearColor;
-					m_pipelines.objectPipelines->RenderPass()->BeginPass(commandBufferInfo.commandBuffer, frameBuffer, CLEAR_VALUE, false);
+					m_graphicsObjectPipelines->RenderPass()->BeginPass(commandBufferInfo.commandBuffer, frameBuffer, CLEAR_VALUE, false);
 				}
 
 				// Set environment:
 				{
-					m_environmentDescriptor.Update();
-					m_pipelines.environmentPipeline->Execute(commandBufferInfo);
+					m_bindings.Update(m_viewport);
+					m_bindingPool->UpdateAllBindingSets(commandBufferInfo);
+					for (size_t i = 0u; i < m_environmentBindingSets.Size(); i++)
+						m_environmentBindingSets[i]->Bind(commandBufferInfo);
 				}
 
 				// Draw objects:
 				{
 					// __TODO__: we need some sorting here, as well as separate opaque and transparent treatment...
-					const LightingModelPipelines::Reader reader(m_pipelines.objectPipelines);
-					for (size_t i = 0; i < reader.PipelineCount(); i++)
-						reader.Pipeline(i)->Execute(commandBufferInfo);
+					const GraphicsObjectPipelines::Reader reader(*m_graphicsObjectPipelines);
+					for (size_t i = 0; i < reader.ObjectCount(); i++)
+						reader.Object(i).ExecutePipeline(commandBufferInfo);
 				}
 
 				// End pass:
-				m_pipelines.objectPipelines->RenderPass()->EndPass(commandBufferInfo.commandBuffer);
+				m_graphicsObjectPipelines->RenderPass()->EndPass(commandBufferInfo.commandBuffer);
 			}
 
 			inline virtual void GetDependencies(Callback<JobSystem::Job*> report) final override {
-				m_environmentDescriptor.GetDependencies(report);
+				if (m_graphicsObjectPipelines != nullptr)
+					m_graphicsObjectPipelines->GetUpdateTasks(report);
+				m_bindings.GetDependencies(report);
 			}
 		};
 	}
