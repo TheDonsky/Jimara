33a702288ec1637d71be9763bbcfac8bfb40c1f4
TheDonsky <nikadon.net@gmail.com>
2023-06-11 20:34:03 +0400

    Add documentation

--- __Source__/Jimara/Environment/Rendering/PostFX/LinearToSRGB/LinearToSRGB.h
+++ __Source__/Jimara/Environment/Rendering/PostFX/LinearToSRGB/LinearToSRGB.h
@@ -3,17 +3,36 @@
 
 
 namespace Jimara {
+	/// <summary>
+	/// Simple kernel that can be used to translate images from Linear to SRGB color space
+	/// <para/> Kernel also implements RenderStack::Renderer in case one wants to use this one inside the render loop.
+	/// </summary>
 	class JIMARA_API LinearToSrgbKernel : public virtual RenderStack::Renderer {
 	public:
+		/// <summary>
+		/// Creates LinearToSrgbKernel instance
+		/// </summary>
+		/// <param name="device"> Graphics device </param>
+		/// <param name="shaderLoader"> Shader loader </param>
+		/// <param name="maxInFlightCommandBuffers"> In-flight command buffer count </param>
+		/// <returns></returns>
 		static Reference<LinearToSrgbKernel> Create(
 			Graphics::GraphicsDevice* device, Graphics::ShaderLoader* shaderLoader, size_t maxInFlightCommandBuffers);
 
+		/// <summary>
+		/// Translate image from Linear to SRGB color space
+		/// </summary>
+		/// <param name="source"> Source(linear) image </param>
+		/// <param name="result"> Result(SRGB) image (can be the same as source) </param>
+		/// <param name="commandBuffer"> In-flight command buffer </param>
 		void Execute(
 			Graphics::TextureView* source, Graphics::TextureView* result, const Graphics::InFlightBufferInfo& commandBuffer);
 
 	private:
+		// Constructor is private
 		inline LinearToSrgbKernel() {}
 
+		// Implementation is hidden behind this..
 		struct Helpers;
 	};
 }
