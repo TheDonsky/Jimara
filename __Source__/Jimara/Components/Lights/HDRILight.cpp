f6131f0a93468439a10712163b0f7b192f4bdbc3
TheDonsky <nikadon.net@gmail.com>
2023-06-16 18:59:44 +0400

    Start implementing HDRI lighting

---
+++ __Source__/Jimara/Components/Lights/HDRILight.cpp
@@ -0,0 +1,123 @@
+#include "HDRILight.h"
+#include "../../Data/Serialization/Helpers/SerializerMacros.h"
+#include "../../Data/Serialization/Attributes/ColorAttribute.h"
+
+
+namespace Jimara {
+	struct HDRILight::Helpers {
+		struct Data {
+			alignas(16) Vector3 color = Vector3(1.0f);
+			alignas(4) uint32_t textureID = 0u;
+		};
+		static_assert(sizeof(Data) == 16u);
+
+		class HDRILightDescriptor
+			: public virtual LightDescriptor
+			, public virtual LightDescriptor::ViewportData
+			, public virtual JobSystem::Job {
+		public:
+			HDRILight* m_owner;
+
+		private:
+			const Reference<const Graphics::ShaderClass::TextureSamplerBinding> m_whiteTexture;
+			Reference<Graphics::BindlessSet<Graphics::TextureSampler>::Binding> m_textureIndex;
+
+			Data m_data;
+			LightInfo m_info;
+
+			inline void UpdateData() {
+				if (m_owner == nullptr) return;
+				m_data.color = m_owner->m_color * m_owner->m_intensity;
+				Graphics::TextureSampler* const sampler = (m_owner->Texture() == nullptr)
+					? m_whiteTexture->BoundObject() : m_owner->Texture();
+				if (m_textureIndex == nullptr || m_textureIndex->BoundObject() != sampler)
+					m_textureIndex = m_owner->Context()->Graphics()->Bindless().Samplers()->GetBinding(sampler);
+				m_data.textureID = m_textureIndex->Index();
+			}
+
+		public:
+			inline HDRILightDescriptor(HDRILight* owner, uint32_t typeId)
+				: m_owner(owner)
+				, m_whiteTexture(Graphics::ShaderClass::SharedTextureSamplerBinding(Vector4(1.0f), owner->Context()->Graphics()->Device()))
+				, m_info{} {
+				UpdateData();
+				m_info.typeId = typeId;
+				m_info.data = &m_data;
+				m_info.dataSize = sizeof(Data);
+			}
+
+			inline virtual ~HDRILightDescriptor() {}
+
+			inline virtual Reference<const ViewportData> GetViewportData(const ViewportDescriptor*) final override { return this; }
+			inline virtual LightInfo GetLightInfo()const override { return m_info; }
+			inline virtual AABB GetLightBounds()const override {
+				static const AABB BOUNDS = [] {
+					static const float inf = std::numeric_limits<float>::infinity();
+					AABB bounds = {};
+					bounds.start = Vector3(-inf, -inf, -inf);
+					bounds.end = Vector3(inf, inf, inf);
+					return bounds;
+				}();
+				return BOUNDS;
+			}
+
+		protected:
+			// JobSystem::Job:
+			virtual void Execute()override {
+				if (m_owner == nullptr) return;
+				UpdateData();
+			}
+			virtual void CollectDependencies(Callback<Job*>)override {}
+		};
+	};
+	
+
+	HDRILight::HDRILight(Component* parent, const std::string_view& name)
+		: Component(parent, name)
+		, m_allLights(LightDescriptor::Set::GetInstance(parent->Context())) {}
+
+	HDRILight::~HDRILight() {
+		OnComponentDisabled();
+	}
+
+	void HDRILight::GetFields(Callback<Serialization::SerializedObject> recordElement) {
+		Component::GetFields(recordElement);
+		JIMARA_SERIALIZE_FIELDS(this, recordElement) {
+			JIMARA_SERIALIZE_FIELD_GET_SET(Color, SetColor, "Color", "Base color of the emission", Object::Instantiate<Serialization::ColorAttribute>());
+			JIMARA_SERIALIZE_FIELD_GET_SET(Intensity, SetIntensity, "Intensity", "Color multiplier");
+			JIMARA_SERIALIZE_FIELD_GET_SET(Texture, SetTexture, "Texture", "Environment HDRI texture");
+		};
+	}
+
+	void HDRILight::OnComponentEnabled() {
+		if (!ActiveInHeirarchy())
+			OnComponentDisabled();
+		else if (m_lightDescriptor == nullptr) {
+			uint32_t typeId;
+			if (Context()->Graphics()->Configuration().ShaderLoader()->GetLightTypeId("Jimara_HDRI_Light", typeId)) {
+				Reference<Helpers::HDRILightDescriptor> descriptor = Object::Instantiate<Helpers::HDRILightDescriptor>(this, typeId);
+				m_lightDescriptor = Object::Instantiate<LightDescriptor::Set::ItemOwner>(descriptor);
+				m_allLights->Add(m_lightDescriptor);
+				Context()->Graphics()->SynchPointJobs().Add(descriptor);
+			}
+		}
+	}
+
+	void HDRILight::OnComponentDisabled() {
+		if (ActiveInHeirarchy())
+			OnComponentEnabled();
+		else {
+			if (m_lightDescriptor != nullptr) {
+				m_allLights->Remove(m_lightDescriptor);
+				Context()->Graphics()->SynchPointJobs().Remove(dynamic_cast<JobSystem::Job*>(m_lightDescriptor->Item()));
+				dynamic_cast<Helpers::HDRILightDescriptor*>(m_lightDescriptor->Item())->m_owner = nullptr;
+				m_lightDescriptor = nullptr;
+			}
+		}
+	}
+
+	template<> void TypeIdDetails::GetTypeAttributesOf<HDRILight>(const Callback<const Object*>& report) {
+		static const ComponentSerializer::Of<HDRILight> serializer("Jimara/Lights/HDRILight", "HDR Texture component");
+		report(&serializer);
+	}
+}
---
+++ __Source__/Jimara/Components/Lights/HDRILight.h
@@ -0,0 +1,91 @@
+#pragma once
+#include "../Component.h"
+#include "../../Environment/Rendering/SceneObjects/Lights/LightDescriptor.h"
+#include "../Camera.h"
+
+
+namespace Jimara {
+	/// <summary> This will make sure, Component is registered with BuiltInTypeRegistrator </summary>
+	JIMARA_REGISTER_TYPE(Jimara::HDRILight);
+
+	/// <summary>
+	/// Light source from an HDRI environment map
+	/// </summary>
+	class JIMARA_API HDRILight : public virtual Component {
+	public:
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="parent"> Parent component </param>
+		/// <param name="name"> Component name </param>
+		HDRILight(Component* parent, const std::string_view& name = "HDRILight");
+
+		/// <summary> Virtual destructor </summary>
+		virtual ~HDRILight();
+
+		/// <summary> Base color </summary>
+		inline Vector3 Color()const { return m_color; }
+
+		/// <summary>
+		/// Sets base color
+		/// </summary>
+		/// <param name="color"> Color multiplier </param>
+		inline void SetColor(Vector3 color) { m_color = color; }
+
+		/// <summary> Color multiplier </summary>
+		inline float Intensity()const { return m_intensity; }
+
+		/// <summary>
+		/// Sets intensity
+		/// </summary>
+		/// <param name="intensity"> Color multiplier </param>
+		inline void SetIntensity(float intensity) { m_intensity = Math::Max(intensity, 0.0f); }
+
+		/// <summary> Environment HDRI texture </summary>
+		inline Graphics::TextureSampler* Texture()const { return m_hdriTexture; }
+
+		/// <summary>
+		/// Sets environment texture
+		/// </summary>
+		/// <param name="texture"> Texture sampler </param>
+		inline void SetTexture(Graphics::TextureSampler* texture) { m_hdriTexture = texture; }
+
+		/// <summary>
+		/// Exposes fields to serialization utilities
+		/// </summary>
+		/// <param name="recordElement"> Reports elements with this </param>
+		virtual void GetFields(Callback<Serialization::SerializedObject> recordElement)override;
+
+
+	protected:
+		/// <summary> Invoked, whenever the component becomes active in herarchy </summary>
+		virtual void OnComponentEnabled()override;
+
+		/// <summary> Invoked, whenever the component stops being active in herarchy </summary>
+		virtual void OnComponentDisabled()override;
+
+
+	private:
+		// Set of all lights from the scene
+		const Reference<LightDescriptor::Set> m_allLights;
+
+		// Color multiplier
+		Vector3 m_color = Vector3(1.0f);
+
+		// Color intensity
+		float m_intensity = 1.0f;
+
+		// Environment HDRI texture
+		Reference<Graphics::TextureSampler> m_hdriTexture;
+
+		// Underlying light descriptor
+		Reference<LightDescriptor::Set::ItemOwner> m_lightDescriptor;
+
+		// Private stuff resides in here
+		struct Helpers;
+	};
+
+	// Type detail callbacks
+	template<> inline void TypeIdDetails::GetParentTypesOf<HDRILight>(const Callback<TypeId>& report) { report(TypeId::Of<Component>()); }
+	template<> JIMARA_API void TypeIdDetails::GetTypeAttributesOf<HDRILight>(const Callback<const Object*>& report);
+}
