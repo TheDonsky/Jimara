40973ece04450e5e3139ec98a6bdeb599bbd81ab
TheDonsky <nikadon.net@gmail.com>
2023-07-13 18:56:49 +0400

    Add a generic input provider interface

---
+++ __Source__/Jimara/Core/Systems/InputProvider.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "../Object.h"
+#include <optional>
+
+
+namespace Jimara {
+	/// <summary>
+	/// Interface for a generic "input" source
+	/// </summary>
+	/// <typeparam name="ValueType"> Provided input/impulse type </typeparam>
+	template<typename ValueType>
+	class InputProvider : public virtual Object {
+	public:
+		/// <summary> Virtual destructor </summary>
+		inline virtual ~InputProvider() {}
+
+		/// <summary>
+		/// Provides "input" value;
+		/// <para/> Return type is optional and, therefore, the input is allowed to be empty.
+		/// </summary>
+		virtual std::optional<ValueType> GetInput() = 0;
+	};
+}
