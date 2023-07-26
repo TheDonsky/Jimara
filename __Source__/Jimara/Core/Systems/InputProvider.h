7557652a29253b330ff146313ec92f9bf64aa54d
TheDonsky <nikadon.net@gmail.com>
2023-07-26 19:29:50 +0400

    Update InputProvider interface with some convenience methods

--- __Source__/Jimara/Core/Systems/InputProvider.h
+++ __Source__/Jimara/Core/Systems/InputProvider.h
@@ -8,7 +8,8 @@ namespace Jimara {
 	/// Interface for a generic "input" source
 	/// </summary>
 	/// <typeparam name="ValueType"> Provided input/impulse type </typeparam>
-	template<typename ValueType>
+	/// <typeparam name="...Arguments"> Any arguments passed to GetInput() method </typeparam>
+	template<typename ValueType, typename... Arguments>
 	class InputProvider : public virtual WeaklyReferenceable {
 	public:
 		/// <summary> Virtual destructor </summary>
@@ -18,6 +19,34 @@ namespace Jimara {
 		/// Provides "input" value;
 		/// <para/> Return type is optional and, therefore, the input is allowed to be empty.
 		/// </summary>
-		virtual std::optional<ValueType> GetInput() = 0;
+		/// <param name="...args"> Some contextual arguments </param>
+		/// <returns> Input value if present </returns>
+		virtual std::optional<ValueType> GetInput(Arguments... args) = 0;
+
+		/// <summary>
+		/// Safetly provides input with a default value
+		/// </summary>
+		/// <param name="inputProvider"> Input provider address (if nullptr, default value will be returned automatically) </param>
+		/// <param name="...args"> Some contextual arguments for the GetInput method </param>
+		/// <param name="defaultValue"> Default value to return when InputProvider is null or does not have a value </param>
+		/// <returns> Input value or default value </returns>
+		inline static ValueType GetInput(InputProvider* inputProvider, Arguments... args, const ValueType& defaultValue) {
+			if (inputProvider == nullptr)
+				return defaultValue;
+			const std::optional<ValueType> rv = inputProvider->GetInput(args...);
+			return rv.has_value() ? rv.value() : defaultValue;
+		}
+
+		/// <summary>
+		/// Safetly provides input value with an internal null-check
+		/// </summary>
+		/// <param name="inputProvider"> Input provider address (if nullptr, no value will be returned) </param>
+		/// <param name="...args"> Some contextual arguments for the GetInput method </param>
+		/// <returns> Input value if inputProvider is valid and it's input is present </returns>
+		inline static std::optional<ValueType> GetInput(InputProvider* inputProvider, Arguments... args) {
+			if (inputProvider == nullptr)
+				return std::optional<ValueType>();
+			else return inputProvider->GetInput(args...);
+		}
 	};
 }
