35de6b18622f4c6f0e74fc034fec8db4205c9b84
TheDonsky <nikadon.net@gmail.com>
2023-08-09 17:02:21 +0400

    Add a base component type

--- __Source__/Jimara-GenericInputs/VectorInput.h
+++ __Source__/Jimara-GenericInputs/VectorInput.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Types.h"
 #include <Jimara/Math/Math.h>
+#include <Jimara/Environment/Scene/Scene.h>
 #include <Jimara/Core/Systems/InputProvider.h>
 
 
@@ -108,6 +109,47 @@ namespace Jimara {
 		};
 
 
+		/// <summary>
+		/// Vector input provider that is also a Component
+		/// </summary>
+		/// <typeparam name="Type"> Input value type </typeparam>
+		/// <typeparam name="...Args"> Arguments for evaluation </typeparam>
+		template<typename Type, typename... Args>
+		class ComponentFrom
+			: public virtual Component
+			, public virtual From<Type, Args...> {
+		public:
+			/// <summary> Virtual destructor </summary>
+			inline virtual ~ComponentFrom() = 0;
+
+			/// <summary>
+			/// [Only intended to be used by WeakReference<>; not safe for general use] Fills WeakReferenceHolder with a StrongReferenceProvider, 
+			/// that will return this WeaklyReferenceable back upon request (as long as it still exists)
+			/// <para/> Note that this is not thread-safe!
+			/// </summary>
+			/// <param name="holder"> Reference to a reference of a StrongReferenceProvider </param>
+			inline virtual void FillWeakReferenceHolder(WeaklyReferenceable::WeakReferenceHolder& holder)override {
+				Component::FillWeakReferenceHolder(holder);
+			}
+
+			/// <summary>
+			/// [Only intended to be used by WeakReference<>; not safe for general use] Should clear the link to the StrongReferenceProvider;
+			/// <para/> Address of the holder has to be the same as the one, previously passed to FillWeakReferenceHolder() method
+			/// <para/> Note that this is not thread-safe!
+			/// </summary>
+			/// <param name="holder"> Reference to a reference of a StrongReferenceProvider </param>
+			inline virtual void ClearWeakReferenceHolder(WeaklyReferenceable::WeakReferenceHolder& holder)override {
+				Component::ClearWeakReferenceHolder(holder);
+			}
+		};
+
+		/// <summary> Virtual destructor </summary>
+		template<typename Type, typename... Args>
+		inline ComponentFrom<Type, Args...>::~ComponentFrom() {}
+
+
+
+
 
 		// Type-cast operations
 		template<typename Type, typename InputType, typename... Args>
@@ -176,6 +218,21 @@ namespace Jimara {
 		inline static void GetParentTypes(const Callback<TypeId>& reportParentType) {
 			reportParentType(TypeId::Of<VectorInput::VectorInput_Base<Type, Args...>>());
 			reportParentType(TypeId::Of<InputProvider<InputType>>());
+		}
+		inline static void GetTypeAttributes(const Callback<const Object*>&) {}
+	};
+
+	/// <summary>
+	/// Type details for VectorInput::ComponentFrom
+	/// </summary>
+	/// <typeparam name="Type"> Base value type </typeparam>
+	/// <typeparam name="InputType"> Input provider type </typeparam>
+	/// <typeparam name="...Args"> Evaluation Arguments </typeparam>
+	template<typename Type, typename InputType, typename... Args>
+	struct TypeIdDetails::TypeDetails<VectorInput::ComponentFrom<Type, InputType, Args...>> {
+		inline static void GetParentTypes(const Callback<TypeId>& reportParentType) {
+			reportParentType(TypeId::Of<Component>());
+			reportParentType(TypeId::Of<VectorInput::From<Type, Args...>>());
 		}
 		inline static void GetTypeAttributes(const Callback<const Object*>&) {}
 	};
