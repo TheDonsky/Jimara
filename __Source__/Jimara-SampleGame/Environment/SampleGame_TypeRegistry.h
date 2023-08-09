33979618605870fd57ba15f96b78737732778e7b
TheDonsky <nikadon.net@gmail.com>
2023-08-09 12:56:13 +0400

    Add API macro

--- __Source__/Jimara-SampleGame/Environment/SampleGame_TypeRegistry.h
+++ __Source__/Jimara-SampleGame/Environment/SampleGame_TypeRegistry.h
@@ -4,9 +4,8 @@
 namespace Jimara {
 	namespace SampleGame {
 		JIMARA_REGISTER_TYPE(Jimara::SampleGame::SampleGame_TypeRegistry);
-#define JimaraEditorTypeRegistry_TMP_DLL_EXPORT_MACRO
+#define JIMARA_GENERIC_INPUTS_API
 		/// <summary> Type registry for Jimara-SampleGame </summary>
-		JIMARA_DEFINE_TYPE_REGISTRATION_CLASS(SampleGame_TypeRegistry, JimaraEditorTypeRegistry_TMP_DLL_EXPORT_MACRO);
-#undef JimaraEditorTypeRegistry_TMP_DLL_EXPORT_MACRO
+		JIMARA_DEFINE_TYPE_REGISTRATION_CLASS(SampleGame_TypeRegistry, JIMARA_GENERIC_INPUTS_API);
 	}
 }
