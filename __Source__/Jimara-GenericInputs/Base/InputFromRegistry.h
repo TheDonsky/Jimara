e103dcdbae4befdc794a81b210b2729e7100620d
TheDonsky <nikadon.net@gmail.com>
2023-08-10 15:21:06 +0400

    Fix paths

--- __Source__/Jimara-GenericInputs/Base/InputFromRegistry.h
+++ __Source__/Jimara-GenericInputs/Base/InputFromRegistry.h
@@ -263,42 +263,42 @@ namespace Jimara {
 		report(TypeId::Of<InputFromRegistry<bool>>());
 	}
 	template<> inline void TypeIdDetails::GetTypeAttributesOf<BooleanInputFromRegistry>(const Callback<const Object*>& report) {
-		static const ComponentSerializer::Of<BooleanInputFromRegistry> serializer("Jimara/Input/Value/Boolean", "Boolean Value Input");
+		static const ComponentSerializer::Of<BooleanInputFromRegistry> serializer("Jimara/Input/RegistryReference/Boolean", "Boolean Input From Registry");
 		report(&serializer);
 	}
 	template<> inline void TypeIdDetails::GetParentTypesOf<FloatInputFromRegistry>(const Callback<TypeId>& report) {
 		report(TypeId::Of<InputFromRegistry<float>>());
 	}
 	template<> inline void TypeIdDetails::GetTypeAttributesOf<FloatInputFromRegistry>(const Callback<const Object*>& report) {
-		static const ComponentSerializer::Of<FloatInputFromRegistry> serializer("Jimara/Input/Value/Float", "Floating point Value Input");
+		static const ComponentSerializer::Of<FloatInputFromRegistry> serializer("Jimara/Input/RegistryReference/Float", "Floating point Input From Registry");
 		report(&serializer);
 	}
 	template<> inline void TypeIdDetails::GetParentTypesOf<IntInputFromRegistry>(const Callback<TypeId>& report) {
 		report(TypeId::Of<InputFromRegistry<int>>());
 	}
 	template<> inline void TypeIdDetails::GetTypeAttributesOf<IntInputFromRegistry>(const Callback<const Object*>& report) {
-		static const ComponentSerializer::Of<IntInputFromRegistry> serializer("Jimara/Input/Value/Integer", "Integer Value Input");
+		static const ComponentSerializer::Of<IntInputFromRegistry> serializer("Jimara/Input/RegistryReference/Integer", "Integer Input From Registry");
 		report(&serializer);
 	}
 	template<> inline void TypeIdDetails::GetParentTypesOf<Vector2InputFromRegistry>(const Callback<TypeId>& report) {
 		report(TypeId::Of<InputFromRegistry<Vector2>>());
 	}
 	template<> inline void TypeIdDetails::GetTypeAttributesOf<Vector2InputFromRegistry>(const Callback<const Object*>& report) {
-		static const ComponentSerializer::Of<Vector2InputFromRegistry> serializer("Jimara/Input/Value/Vector2", "Vector2 Value Input");
+		static const ComponentSerializer::Of<Vector2InputFromRegistry> serializer("Jimara/Input/RegistryReference/Vector2", "Vector2 Input From Registry");
 		report(&serializer);
 	}
 	template<> inline void TypeIdDetails::GetParentTypesOf<Vector3InputFromRegistry>(const Callback<TypeId>& report) {
 		report(TypeId::Of<InputFromRegistry<Vector3>>());
 	}
 	template<> inline void TypeIdDetails::GetTypeAttributesOf<Vector3InputFromRegistry>(const Callback<const Object*>& report) {
-		static const ComponentSerializer::Of<Vector3InputFromRegistry> serializer("Jimara/Input/Value/Vector3", "Vector3 Value Input");
+		static const ComponentSerializer::Of<Vector3InputFromRegistry> serializer("Jimara/Input/RegistryReference/Vector3", "Vector3 Input From Registry");
 		report(&serializer);
 	}
 	template<> inline void TypeIdDetails::GetParentTypesOf<Vector4InputFromRegistry>(const Callback<TypeId>& report) {
 		report(TypeId::Of<InputFromRegistry<Vector4>>());
 	}
 	template<> inline void TypeIdDetails::GetTypeAttributesOf<Vector4InputFromRegistry>(const Callback<const Object*>& report) {
-		static const ComponentSerializer::Of<Vector4InputFromRegistry> serializer("Jimara/Input/Value/Vector4", "Vector4 Value Input");
+		static const ComponentSerializer::Of<Vector4InputFromRegistry> serializer("Jimara/Input/RegistryReference/Vector4", "Vector4 Input From Registry");
 		report(&serializer);
 	}
 }
