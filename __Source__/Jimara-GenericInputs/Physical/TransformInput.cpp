37868c314223f170b4ff6cde5e3d899011381211
TheDonsky <nikadon.net@gmail.com>
2023-08-10 11:48:07 +0400

    Add Transform input

---
+++ __Source__/Jimara-GenericInputs/Physical/TransformInput.cpp
@@ -0,0 +1,64 @@
+#include "TransformInput.h"
+#include <Jimara/Data/Serialization/Attributes/EnumAttribute.h>
+#include <Jimara/Data/Serialization/Helpers/SerializerMacros.h>
+
+
+namespace Jimara {
+	TransformInput::TransformInput(Component* parent, const std::string_view& name) 
+		: Component(parent, name) {}
+
+	TransformInput::~TransformInput() {}
+
+	void TransformInput::GetFields(Callback<Jimara::Serialization::SerializedObject> recordElement) {
+		Jimara::Component::GetFields(recordElement);
+		JIMARA_SERIALIZE_FIELDS(this, recordElement) {
+			JIMARA_SERIALIZE_FIELD_GET_SET(Mode, SetMode, "InputMode", "Input InputMode",
+				Object::Instantiate<Jimara::Serialization::EnumAttribute<std::underlying_type_t<InputMode>>>(false,
+					"WORLD_POSITION", InputMode::WORLD_POSITION,
+					"LOCAL_POSITION", InputMode::LOCAL_POSITION,
+					"WORLD_ROTATION", InputMode::WORLD_ROTATION,
+					"LOCAL_ROTATION", InputMode::LOCAL_ROTATION,
+					"WORLD_SCALE", InputMode::WORLD_SCALE,
+					"LOCAL_SCALE", InputMode::LOCAL_SCALE,
+					"FORWARD", InputMode::FORWARD,
+					"LOCAL_FORWARD", InputMode::LOCAL_FORWARD,
+					"RIGHT", InputMode::RIGHT,
+					"LOCAL_RIGHT", InputMode::LOCAL_RIGHT,
+					"UP", InputMode::UP,
+					"LOCAL_UP", InputMode::LOCAL_UP,
+					"NO_INPUT", InputMode::NO_INPUT));
+			JIMARA_SERIALIZE_FIELD_GET_SET(Flags, SetFlags, "No Input If Disabled", "Blocks input if component is disabled",
+				Object::Instantiate<Jimara::Serialization::EnumAttribute<std::underlying_type_t<InputFlags>>>(true,
+					"NO_VALUE_IF_DISABLED", InputFlags::NO_VALUE_IF_DISABLED));
+		};
+	}
+
+	std::optional<Vector3> TransformInput::EvaluateInput() {
+		auto hasFlag = [&](InputFlags flag) {
+			return (static_cast<std::underlying_type_t<InputFlags>>(m_flags) & static_cast<std::underlying_type_t<InputFlags>>(flag)) != 0;
+		};
+		Jimara::Transform* transform = GetTransfrom();
+		if (m_mode >= InputMode::NO_INPUT || (transform == nullptr) || (hasFlag(InputFlags::NO_VALUE_IF_DISABLED) && (!ActiveInHeirarchy())))
+			return std::optional<Vector3>();
+		switch (m_mode) {
+		case InputMode::WORLD_POSITION: return transform->WorldPosition();
+		case InputMode::LOCAL_POSITION: return transform->LocalPosition();
+		case InputMode::WORLD_ROTATION: return transform->WorldEulerAngles();
+		case InputMode::LOCAL_ROTATION: return transform->LocalEulerAngles();
+		case InputMode::WORLD_SCALE: return transform->LossyScale();
+		case InputMode::LOCAL_SCALE: return transform->LocalScale();
+		case InputMode::FORWARD: return transform->Forward();
+		case InputMode::LOCAL_FORWARD: return transform->LocalForward();
+		case InputMode::RIGHT: return transform->Right();
+		case InputMode::LOCAL_RIGHT: return transform->LocalRight();
+		case InputMode::UP: return transform->Up();
+		case InputMode::LOCAL_UP: return transform->LocalUp();
+		}
+		return std::optional<Vector3>();
+	}
+
+	template<> void TypeIdDetails::GetTypeAttributesOf<TransformInput>(const Callback<const Object*>& report) {
+		static const ComponentSerializer::Of<TransformInput> serializer("Jimara/Input/Physical/TransformInput", "TransformInput");
+		report(&serializer);
+	}
+}
---
+++ __Source__/Jimara-GenericInputs/Physical/TransformInput.h
@@ -0,0 +1,118 @@
+#pragma once
+#include "../Base/VectorInput.h"
+#include <Jimara/Components/Transform.h>
+
+
+namespace Jimara {
+	/// <summary> Let the system know about the type </summary>
+	JIMARA_REGISTER_TYPE(Jimara::TransformInput);
+
+	/// <summary>
+	/// 3d input from Transform component state
+	/// </summary>
+	class JIMARA_GENERIC_INPUTS_API TransformInput : public virtual VectorInput::ComponentFrom<Vector3> {
+	public:
+		/// <summary>
+		/// Input value mode
+		/// </summary>
+		enum class InputMode : uint8_t {
+			/// <summary> World-space position </summary>
+			WORLD_POSITION = 0u,
+
+			/// <summary> Parent-space position </summary>
+			LOCAL_POSITION = 1u,
+
+			/// <summary> World-space euler angles </summary>
+			WORLD_ROTATION = 2u,
+
+			/// <summary> Parent-space euler angles </summary>
+			LOCAL_ROTATION = 3u,
+
+			/// <summary> Lossy(global) scale </summary>
+			WORLD_SCALE = 4u,
+
+			/// <summary> Local(parent-space) scale </summary>
+			LOCAL_SCALE = 5u,
+
+			/// <summary> World-space forward direction </summary>
+			FORWARD = 6u,
+
+			/// <summary> Parent-space forward direction </summary>
+			LOCAL_FORWARD = 7u,
+
+			/// <summary> World-space right direction </summary>
+			RIGHT = 8u,
+
+			/// <summary> Parent-space right direction </summary>
+			LOCAL_RIGHT = 9u,
+
+			/// <summary> World-space up direction </summary>
+			UP = 10u,
+
+			/// <summary> Parent-space right direction </summary>
+			LOCAL_UP = 11u,
+
+			/// <summary> Nothing; No input, no value </summary>
+			NO_INPUT = 12u
+		};
+
+		/// <summary> Input flags </summary>
+		enum class InputFlags : uint8_t {
+			/// <summary> No effect </summary>
+			NONE = 0u,
+
+			/// <summary> If true, input will not be produced if the input component is disabled in hierarchy </summary>
+			NO_VALUE_IF_DISABLED = (1u << 0u)
+		};
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="parent"> Parent component </param>
+		/// <param name="name"> Component name </param>
+		TransformInput(Component* parent, const std::string_view& name = "TransformInput");
+
+		/// <summary> Virtual destructor </summary>
+		virtual ~TransformInput();
+
+		/// <summary> Input mode </summary>
+		inline InputMode Mode()const { return m_mode; }
+
+		/// <summary>
+		/// Sets input mode
+		/// </summary>
+		/// <param name="mode"> Input mode </param>
+		inline void SetMode(InputMode mode) { m_mode = Math::Min(mode, InputMode::NO_INPUT); }
+
+		/// <summary> Input flags/settings </summary>
+		inline InputFlags Flags()const { return m_flags; }
+
+		/// <summary>
+		/// Sets input flags
+		/// </summary>
+		/// <param name="flags"> Settings </param>
+		inline void SetFlags(InputFlags flags) { m_flags = flags; }
+
+		/// <summary>
+		/// Exposes fields
+		/// </summary>
+		/// <param name="recordElement"> Fields will be exposed through this callback </param>
+		virtual void GetFields(Callback<Jimara::Serialization::SerializedObject> recordElement)override;
+
+		/// <summary> Evaluates input </summary>
+		virtual std::optional<Vector3> EvaluateInput()override;
+
+	private:
+		// Mode
+		InputMode m_mode = InputMode::WORLD_POSITION;
+
+		// Flags
+		InputFlags m_flags = InputFlags::NO_VALUE_IF_DISABLED;
+	};
+
+	// Expose type details:
+	template<> inline void TypeIdDetails::GetParentTypesOf<TransformInput>(const Callback<TypeId>& report) {
+		report(TypeId::Of<VectorInput::ComponentFrom<Vector3>>());
+	}
+	template<> JIMARA_GENERIC_INPUTS_API void TypeIdDetails::GetTypeAttributesOf<TransformInput>(const Callback<const Object*>& report);
+}
