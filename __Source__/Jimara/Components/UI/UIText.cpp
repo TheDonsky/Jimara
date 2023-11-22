ec9e871264806a16840d5c8c05487d54a9468f23
TheDonsky <nikadon.net@gmail.com>
2023-11-22 14:12:37 +0400

    Create a basic skeleton for UI Text implementation

--- __Source__/Jimara/Components/UI/UIText.cpp
+++ __Source__/Jimara/Components/UI/UIText.cpp
@@ -1,8 +1,118 @@
 #include "UIText.h"
+#include "../../Data/Serialization/Helpers/SerializerMacros.h"
+#include "../../Data/Serialization/Attributes/ColorAttribute.h"
 
 
 namespace Jimara {
 	namespace UI {
-		// __TODO__: Implement this crap!
+		struct UIText::Helpers {
+
+			inline static void RefreshGraphicsObject(UIText* self) {
+				if (self->m_graphicsObject != nullptr) {
+					self->m_canvas->GraphicsObjects()->Remove(self->m_graphicsObject);
+					self->Context()->Graphics()->SynchPointJobs().Remove(dynamic_cast<JobSystem::Job*>(self->m_graphicsObject->Item()));
+					self->m_graphicsObject = nullptr;
+				}
+				self->m_canvas = nullptr;
+
+				if (!self->ActiveInHeirarchy()) return;
+
+				self->m_canvas = self->GetComponentInParents<Canvas>();
+				if (self->m_canvas == nullptr) return;
+
+				// __TODO__: Implement this crap!
+				const Reference<GraphicsObjectDescriptor> graphicsObject = nullptr;// GraphicsObject::Create(self);
+				if (graphicsObject == nullptr) {
+					self->m_canvas = nullptr;
+					return;
+				}
+				self->m_graphicsObject = Object::Instantiate<GraphicsObjectDescriptor::Set::ItemOwner>(graphicsObject);
+
+				self->Context()->Graphics()->SynchPointJobs().Add(dynamic_cast<JobSystem::Job*>(self->m_graphicsObject->Item()));
+				self->m_canvas->GraphicsObjects()->Add(self->m_graphicsObject);
+			}
+
+			inline static void UnsubscribeParentChain(UIText* self) {
+				for (size_t i = 0; i < self->m_parentChain.Size(); i++)
+					self->m_parentChain[i]->OnParentChanged() -= Callback<ParentChangeInfo>(Helpers::OnParentChanged, self);
+				self->m_parentChain.Clear();
+			}
+
+			inline static void SubscribeParentChain(UIText* self) {
+				UnsubscribeParentChain(self);
+				if (self->Destroyed()) return;
+				Component* parent = self;
+				while (parent != nullptr) {
+					parent->OnParentChanged() += Callback<ParentChangeInfo>(Helpers::OnParentChanged, self);
+					self->m_parentChain.Push(parent);
+					if (dynamic_cast<Canvas*>(parent) != nullptr) break;
+					else parent = parent->Parent();
+				}
+			}
+
+			inline static void OnParentChanged(UIText* self, ParentChangeInfo) {
+				RefreshGraphicsObject(self);
+				SubscribeParentChain(self);
+			}
+
+			inline static void OnImageDestroyed(Component* self) {
+				Helpers::UnsubscribeParentChain(dynamic_cast<UIText*>(self));
+				Helpers::RefreshGraphicsObject(dynamic_cast<UIText*>(self));
+				self->OnDestroyed() -= Callback(Helpers::OnImageDestroyed);
+			}
+		};
+
+		UIText::UIText(Component* parent, const std::string_view& name) 
+			: Component(parent, name) {
+			Helpers::SubscribeParentChain(this);
+			OnDestroyed() += Callback(Helpers::OnImageDestroyed);
+		}
+
+		UIText::~UIText() {
+			Helpers::OnImageDestroyed(this);
+		}
+
+		void UIText::SetFont(Jimara::Font* font) {
+			if (m_font == font)
+				return;
+			m_font = font;
+			Helpers::RefreshGraphicsObject(this);
+		}
+
+		void UIText::SetFontSize(float size) {
+			m_fontSize = size;
+		}
+
+		void UIText::SetColor(const Vector4& color) {
+			m_color = color;
+		}
+
+		void UIText::GetFields(Callback<Serialization::SerializedObject> recordElement) {
+			Component::GetFields(recordElement);
+			JIMARA_SERIALIZE_FIELDS(this, recordElement) {
+				JIMARA_SERIALIZE_FIELD(m_text, "Text", "Displayed text");
+
+				static const std::string fontHint = "Sampler to the main texture (overrides material filed named '" + std::string(FontTextureShaderBindingName()) + "')";
+				JIMARA_SERIALIZE_FIELD_GET_SET(Font, SetFont, "Font", fontHint);
+				JIMARA_SERIALIZE_FIELD_GET_SET(FontSize, SetFontSize, "Font Size", "Font size in canvas units");
+
+				static const std::string colorHint = "Image color multiplier (appears as vertex color input with the name: '" + std::string(ColorShaderBindingName()) + "')";
+				JIMARA_SERIALIZE_FIELD_GET_SET(Color, SetColor, "Color", colorHint, Object::Instantiate<Serialization::ColorAttribute>());
+			};
+		}
+
+		void UIText::OnComponentEnabled() {
+			Helpers::RefreshGraphicsObject(this);
+		}
+
+		void UIText::OnComponentDisabled() {
+			Helpers::RefreshGraphicsObject(this);
+		}
+	}
+
+	template<> JIMARA_API void TypeIdDetails::GetTypeAttributesOf<UI::UIText>(const Callback<const Object*>& report) {
+		static const Reference<ComponentFactory> factory = ComponentFactory::Create<UI::UIText>(
+			"UI Image", "Jimara/UI/Text", "Text that can appear on UI Canvas");
+		report(factory);
 	}
 }
--- __Source__/Jimara/Components/UI/UIText.h
+++ __Source__/Jimara/Components/UI/UIText.h
@@ -1,9 +1,108 @@
 #pragma once
 #include "UITransform.h"
+#include "../../Data/Fonts/Font.h"
 
 
 namespace Jimara {
 	namespace UI {
-		// __TODO__: Implement this crap!
+		/// <summary> Let the system know of the type </summary>
+		JIMARA_REGISTER_TYPE(Jimara::UI::UIText);
+
+		/// <summary>
+		/// Text, that can appear on UI Canvas
+		/// </summary>
+		class JIMARA_API UIText : public virtual Component {
+		public:
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="parent"> Parent component </param>
+			/// <param name="name"> Image name </param>
+			UIText(Component* parent, const std::string_view& name = "UIText");
+
+			/// <summary> Virtual destructor </summary>
+			virtual ~UIText();
+
+			/// <summary> Image's Texture field will override a shader input of this name </summary>
+			inline static const constexpr std::string_view FontTextureShaderBindingName() { return "fontAlpha"; };
+
+			/// <summary> Displayed text </summary>
+			inline std::string& Text() { return m_text; }
+
+			/// <summary> Displayed text </summary>
+			inline const std::string& Text()const { return m_text; }
+
+			/// <summary> Font for the text </summary>
+			inline Jimara::Font* Font()const { return m_font; }
+
+			/// <summary>
+			/// Sets font
+			/// </summary>
+			/// <param name="font"> Font to use </param>
+			void SetFont(Jimara::Font* font);
+
+			/// <summary> Font size in canvas units </summary>
+			inline float FontSize()const { return m_fontSize; }
+
+			/// <summary>
+			/// Sets character size
+			/// </summary>
+			/// <param name="size"> Font size in canvas units </param>
+			void SetFontSize(float size);
+
+			/// <summary> Image's Color field will override a shader instance input of this name </summary>
+			inline static const constexpr std::string_view ColorShaderBindingName() { return "VertexColor"; };
+
+			/// <summary> Image color multiplier (appears as vertex color input with the name: ColorShaderBindingName()) </summary>
+			inline Vector4 Color()const { return m_color; }
+
+			/// <summary>
+			/// Sets image color
+			/// </summary>
+			/// <param name="color"> Instance color of the image </param>
+			void SetColor(const Vector4& color);
+
+			/// <summary>
+			/// Exposes fields to serialization utilities
+			/// </summary>
+			/// <param name="recordElement"> Reports elements with this </param>
+			virtual void GetFields(Callback<Serialization::SerializedObject> recordElement)override;
+
+
+		protected:
+			/// <summary> Invoked, whenever the component becomes active in herarchy </summary>
+			virtual void OnComponentEnabled()override;
+
+			/// <summary> Invoked, whenever the component stops being active in herarchy </summary>
+			virtual void OnComponentDisabled()override;
+
+
+		private:
+			// Displayed text
+			std::string m_text;
+
+			// Font
+			Reference<Jimara::Font> m_font;
+
+			// Font size
+			float m_fontSize = 24.0f;
+
+			// Color
+			Vector4 m_color = Vector4(1.0f);
+
+			// Parent object chain (for listening to hierarchical changes)
+			Stacktor<Reference<Component>, 4u> m_parentChain;
+
+			// Underlying graphics object and Canvas
+			Reference<const Canvas> m_canvas;
+			Reference<GraphicsObjectDescriptor::Set::ItemOwner> m_graphicsObject;
+
+			// Private stuff resides in here..
+			struct Helpers;
+		};
 	}
+
+	// Type detail callbacks
+	template<> inline void TypeIdDetails::GetParentTypesOf<UI::UIText>(const Callback<TypeId>& report) { report(TypeId::Of<Component>()); }
+	template<> JIMARA_API void TypeIdDetails::GetTypeAttributesOf<UI::UIText>(const Callback<const Object*>& report);
 }
