4317adf16c0ae17d5de91a29d926e3f2df32590e
TheDonsky <nikadon.net@gmail.com>
2023-12-25 00:55:22 +0400

    Add unbounded transforms into the selection boundary calculations to make sure transform handles can be focused on even if there's no bounded object under them

--- __Source__/Jimara-Editor/Gizmos/SceneNavigation/FocusOnSelectionAction.cpp
+++ __Source__/Jimara-Editor/Gizmos/SceneNavigation/FocusOnSelectionAction.cpp
@@ -34,67 +34,83 @@ namespace Jimara {
 					});
 				return result;
 			}();
-			if (boundedComponents.empty())
-				return;
 			
-			// Calculate total boundaries:
-			auto isBounded = [](const AABB& bnd) {
+
+			// Define a few tools for boundary calculation:
+			static const auto isBounded = [](const AABB& bnd) {
 				return
 					std::isfinite(bnd.start.x) && std::isfinite(bnd.start.y) && std::isfinite(bnd.start.z) &&
 					std::isfinite(bnd.end.x) && std::isfinite(bnd.end.y) && std::isfinite(bnd.end.z);
 			};
-			const auto [bounds, averageSize] = [&]() {
-				std::unordered_set<const BoundedObject*>::const_iterator it = boundedComponents.begin();
-				float averageSize = 0.0f;
-				float index = 0.0f;
-				auto getNextBounds = [&]() {
+			static const auto expand = [](AABB& bounds, const AABB& bnd) {
+				if (!isBounded(bnd))
+					return false;
+				else if (isBounded(bounds))
+					bounds = AABB(
+						Vector3(
+							Math::Min(bounds.start.x, bnd.start.x),
+							Math::Min(bounds.start.y, bnd.start.y),
+							Math::Min(bounds.start.z, bnd.start.z)),
+						Vector3(
+							Math::Max(bounds.end.x, bnd.end.x),
+							Math::Max(bounds.end.y, bnd.end.y),
+							Math::Max(bounds.end.z, bnd.end.z)));
+				else bounds = bnd;
+				return true;
+			};
+			static const auto boundSize = [](const AABB& bnd) {
+				return Math::Max(
+					bnd.end.x - bnd.start.x,
+					bnd.end.y - bnd.start.y,
+					bnd.end.z - bnd.start.z);
+			};
+			AABB bounds = AABB(
+				Vector3(std::numeric_limits<float>::quiet_NaN()), 
+				Vector3(std::numeric_limits<float>::quiet_NaN()));
+			float averageSize = 0.0f;
+
+			// Include transform positions in the total boundary:
+			{
+				GizmoContext()->Selection()->Iterate([&](Component* component) {
+					Transform* transform = component->GetTransfrom();
+					if (transform == nullptr)
+						return;
+					const Vector3 position = transform->WorldPosition();
+					expand(bounds, AABB(position, position));
+					});
+				if (isBounded(bounds))
+					averageSize = boundSize(bounds);
+			}
+
+			// Include bounded objects in the total boundary:
+			{
+				float boundIndex = 0.0f;
+				for (decltype(boundedComponents)::const_iterator it = boundedComponents.begin(); it != boundedComponents.end(); ++it) {
 					AABB bnd = (*it)->GetBoundaries();
-					++it;
-					if (!isBounded(bnd))
-						return bnd;
-					bnd = AABB(
-						Vector3(
-							Math::Min(bnd.start.x, bnd.end.x), 
-							Math::Min(bnd.start.y, bnd.end.y), 
-							Math::Min(bnd.start.z, bnd.end.z)),
-						Vector3(
-							Math::Max(bnd.start.x, bnd.end.x), 
-							Math::Max(bnd.start.y, bnd.end.y), 
-							Math::Max(bnd.start.z, bnd.end.z)));
-					index++;
-					averageSize = Math::Lerp(averageSize, Math::Max(
-						bnd.end.x - bnd.start.x,
-						bnd.end.y - bnd.start.y,
-						bnd.end.z - bnd.start.z), 1.0f / index);
-					return bnd;
-				};
-				AABB result = getNextBounds();
-				while (it != boundedComponents.end()) {
-					const AABB bnd = getNextBounds();
 					if (!isBounded(bnd))
 						continue;
-					else if (!isBounded(result))
-						result = bnd;
-					else result = AABB(
+					bnd = AABB(
 						Vector3(
-							Math::Min(result.start.x, bnd.start.x),
-							Math::Min(result.start.y, bnd.start.y),
-							Math::Min(result.start.z, bnd.start.z)),
+							Math::Min(bnd.start.x, bnd.end.x),
+							Math::Min(bnd.start.y, bnd.end.y),
+							Math::Min(bnd.start.z, bnd.end.z)),
 						Vector3(
-							Math::Max(result.end.x, bnd.end.x),
-							Math::Max(result.end.y, bnd.end.y),
-							Math::Max(result.end.z, bnd.end.z)));
+							Math::Max(bnd.start.x, bnd.end.x),
+							Math::Max(bnd.start.y, bnd.end.y),
+							Math::Max(bnd.start.z, bnd.end.z)));
+					boundIndex++;
+					averageSize = Math::Lerp(averageSize, boundSize(bnd), 1.0f / boundIndex);
+					expand(bounds, bnd);
 				}
-				return std::make_pair(result, averageSize);
-			}();
+			}
+
+			// Do some final checks and size calculations:
 			if (!isBounded(bounds))
 				return;
+			if (averageSize <= std::numeric_limits<float>::epsilon())
+				averageSize = 1.0f;
+			const float boundarySize = boundSize(bounds);
 
-			// Total boundary size:
-			const float boundarySize = Math::Max(
-				bounds.end.x - bounds.start.x,
-				bounds.end.y - bounds.start.y,
-				bounds.end.z - bounds.start.z);
 
 			// Adjust gizmo viewport position and size:
 			GizmoViewport* viewport = GizmoContext()->Viewport();
