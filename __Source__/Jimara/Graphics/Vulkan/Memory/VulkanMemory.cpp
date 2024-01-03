21f8734ea6ed8a5aed61bd5bf534dc62047dfef0
TheDonsky <nikadon.net@gmail.com>
2024-01-04 01:09:27 +0400

    Make it so that max group size kind-of self-regulates itself

--- __Source__/Jimara/Graphics/Vulkan/Memory/VulkanMemory.cpp
+++ __Source__/Jimara/Graphics/Vulkan/Memory/VulkanMemory.cpp
@@ -351,10 +351,15 @@ namespace Jimara {
 				group->freeAllocations.push_back(self);
 				assert(RefCount() <= 0u);
 				if (group->subpool != nullptr) {
-					if (group->subpool->dead || (group->freeAllocations.size() == group->allocations.size() && group->subpool->groups.size() > 1u))
+					if (group->subpool->dead || (group->freeAllocations.size() == group->allocations.size() && group->subpool->groups.size() > 1u)) {
 						group->subpool->groups.erase(group);
-					else if (group->freeAllocations.size() == 1u || group->subpool->groups.empty())
+						if (group->allocations.size() >= group->subpool->maxGroupSize && group->subpool->maxGroupSize > 1u)
+							group->subpool->maxGroupSize >>= 1u;
+					}
+					else if (group->freeAllocations.size() == 1u || group->subpool->groups.empty()) {
 						group->subpool->groups.insert(group);
+						group->subpool->maxGroupSize = Math::Max(static_cast<VkDeviceSize>(group->allocations.size()), group->subpool->maxGroupSize);
+					}
 				}
 			}
 		}
