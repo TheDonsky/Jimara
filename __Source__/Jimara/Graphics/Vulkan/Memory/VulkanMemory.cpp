671b67ede59d6147dac01e11a1bcf4c542313697
TheDonsky <nikadon.net@gmail.com>
2024-01-03 20:20:11 +0400

    Introduce an alternative VulkanMemoryPool implementation that does not overalocate memory as much

--- __Source__/Jimara/Graphics/Vulkan/Memory/VulkanMemory.cpp
+++ __Source__/Jimara/Graphics/Vulkan/Memory/VulkanMemory.cpp
@@ -6,6 +6,7 @@
 namespace Jimara {
 	namespace Graphics {
 		namespace Vulkan {
+			namespace Legacy {
 			namespace {
 				inline static uint32_t FindMemoryType(VulkanDevice* device, uint32_t typeFilter, VkMemoryPropertyFlags& properties) {
 					const VkPhysicalDeviceMemoryProperties& memoryProperties = device->PhysicalDeviceInfo()->MemoryProperties();
@@ -270,6 +271,336 @@ namespace Jimara {
 				blockPool.freeAllocations.push((VulkanMemoryAllocation*)((void*)this));
 				m_memoryPool->GraphicsDevice()->ReleaseRef();
 			}
+		}
+		
+
+
+
+
+		namespace Refactor {
+		struct VulkanMemoryPool::Helpers {
+			struct AllocationGroup : public virtual Object {
+				const VkDeviceMemory vulkanMemory;
+				void* const mappedMemory;
+				const VkDeviceSize sizePerAllocation;
+				const VkMemoryPropertyFlags propertyFlags;
+				const Reference<VkDeviceHandle> device;
+				const Reference<OS::Logger> log;
+
+				SpinLock allocationLock;
+				struct AllocationBlob {
+					uint8_t allocationData[sizeof(VulkanMemoryAllocation)];
+				};
+				static_assert(sizeof(AllocationBlob) == sizeof(VulkanMemoryAllocation));
+				std::vector<AllocationBlob> allocations;
+				std::vector<VulkanMemoryAllocation*> freeAllocations;
+
+				inline VulkanMemoryAllocation* AllocationData() {
+					return reinterpret_cast<VulkanMemoryAllocation*>(allocations.data());
+				}
+
+				inline AllocationGroup(
+					VkDeviceHandle* deviceHandle, OS::Logger* logger,
+					VkDeviceMemory memory, void* mapped, VkMemoryPropertyFlags flags,
+					VkDeviceSize allocationSize, VkDeviceSize numAllocations) 
+					: vulkanMemory(memory)
+					, mappedMemory(mapped)
+					, sizePerAllocation(allocationSize)
+					, propertyFlags(flags)
+					, device(deviceHandle)
+					, log(logger)
+					, allocations(static_cast<size_t>(numAllocations)) {
+					for (size_t i = 0u; i < allocations.size(); i++) {
+						VulkanMemoryAllocation& alloc = AllocationData()[i];
+						new (&alloc) VulkanMemoryAllocation();
+						alloc.m_allocationGroup = this;
+						alloc.ReleaseRef();
+						assert(alloc.m_allocationGroup == nullptr);
+						assert(alloc.RefCount() == 0u);
+						assert(RefCount() == 1u);
+						assert(freeAllocations.size() == (i + 1u));
+					}
+				}
+
+				inline virtual ~AllocationGroup() {
+					if (mappedMemory != nullptr)
+						vkUnmapMemory(*device, vulkanMemory);
+					vkFreeMemory(*device, vulkanMemory, nullptr);
+					for (size_t i = 0u; i < allocations.size(); i++)
+						(AllocationData() + i)->~VulkanMemoryAllocation();
+				}
+
+				inline Reference<VulkanMemoryAllocation> Allocate(VkDeviceSize alignment, VkDeviceSize size) {
+					// Sanity check:
+					if (size > sizePerAllocation) {
+						log->Error(
+							"VulkanMemoryPool::Helpers::AllocationGroup::Allocate - Sub-allocation can not be larger than sizePerAllocation! ",
+							"[File: ", __FILE__, "; Line: ", __LINE__, "]");
+						return nullptr;
+					}
+
+					// Obtain free chunk:
+					std::unique_lock<SpinLock> lock(allocationLock);
+					if (freeAllocations.size() <= 0u)
+						return nullptr;
+					Reference<VulkanMemoryAllocation> result = freeAllocations.back();
+
+					// Calculate and check offset and size:
+					const VkDeviceSize chunkStart = (result.operator->() - AllocationData()) * sizePerAllocation;
+					const VkDeviceSize chunkEnd = chunkStart + sizePerAllocation;
+					alignment = Math::Max(alignment, VkDeviceSize(1u));
+					const VkDeviceSize alignedOffset = ((chunkStart + alignment - 1u) / alignment) * alignment;
+					if (alignedOffset > (chunkEnd - size)) {
+						log->Error(
+							"VulkanMemoryPool::Helpers::AllocationGroup::Allocate - Aligned sub-allocation can not fit in the dedicated memory segment! ",
+							"[File: ", __FILE__, "; Line: ", __LINE__, "]");
+						return nullptr;
+					}
+
+					// Finish allocation:
+					result->m_allocationGroup = this;
+					result->m_offset = alignedOffset;
+					result->m_size = size;
+					freeAllocations.pop_back();
+					assert(result->RefCount() == 1u);
+					return result;
+				}
+
+				inline static Reference<AllocationGroup> Create(
+					VkDeviceHandle* device, OS::Logger* log,
+					VkDeviceSize allocationSize, VkDeviceSize numAllocations, uint32_t memoryTypeIndex) {
+					assert(log != nullptr);
+					assert(device != nullptr);
+					assert(memoryTypeIndex < device->PhysicalDevice()->MemoryProperties().memoryTypeCount);
+					auto fail = [&](const auto&... message) { 
+						log->Error("VulkanMemoryPool::Helpers::AllocationGroup::Create - ", message...); 
+						return nullptr;
+					};
+
+					// Allocate memory:
+					VkDeviceMemory vulkanMemory = VK_NULL_HANDLE;
+					{
+						VkMemoryAllocateInfo allocInfo = {};
+						allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+						allocInfo.allocationSize = allocationSize * numAllocations;
+						allocInfo.memoryTypeIndex = memoryTypeIndex;
+						VkResult result = vkAllocateMemory(*device, &allocInfo, nullptr, &vulkanMemory);
+						if (result != VK_SUCCESS)
+							return fail("Failed to allocate memory (error code: ", result, ")! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+					}
+
+					// Map memory if it is host-visible:
+					void* mappedMemory = nullptr;
+					const VkMemoryPropertyFlags propertyFlags = device->PhysicalDevice()->MemoryProperties().memoryTypes[memoryTypeIndex].propertyFlags;
+					if ((propertyFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) != 0u) {
+						VkResult result = vkMapMemory(*device, vulkanMemory, 0, allocationSize * numAllocations, 0u, &mappedMemory);
+						if (result != VK_SUCCESS) {
+							vkFreeMemory(*device, vulkanMemory, nullptr);
+							return fail("Failed to map memory (error code: ", result, ")! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+						}
+					}
+
+					// Create group:
+					return Object::Instantiate<AllocationGroup>(
+						device, log, vulkanMemory, mappedMemory, propertyFlags, allocationSize, numAllocations);
+				}
+			};
+
+			inline static VkMappedMemoryRange GetNonCoherentMappedMemoryRange(const VulkanMemoryAllocation* allocation) {
+				AllocationGroup* group = dynamic_cast<AllocationGroup*>(allocation->m_allocationGroup.operator->());
+				assert(group != nullptr);
+
+				// Basic boundaries:
+				const VkDeviceSize atomSize = Math::Max(group->device->PhysicalDevice()->DeviceProperties().limits.nonCoherentAtomSize, VkDeviceSize(1u));
+				const VkDeviceSize allocationIndex = static_cast<VkDeviceSize>(allocation - group->AllocationData());
+				const VkDeviceSize allocationBase = allocationIndex * group->sizePerAllocation;
+				const VkDeviceSize allocationEnd = allocationBase + group->sizePerAllocation;
+				const VkDeviceSize memoryStart = allocation->Offset();
+				const VkDeviceSize memoryEnd = memoryStart + allocation->Size();
+				const VkDeviceSize atomAlignedStart = (memoryStart / atomSize) * atomSize;
+				const VkDeviceSize atomAlignedEnd = ((memoryEnd + atomSize - 1u) / atomSize) * atomSize;
+				
+				// Warn if chunks overlap in any way:
+				if (atomAlignedStart < memoryStart)
+					group->log->Warning(
+						"VulkanMemoryPool::Helpers::GetMappedMemoryRange - Can not isolate VkMappedMemoryRange that does not overlap with previous chunk. ",
+						"This may result in unsafe behaviour! [File:", __FILE__, "; Line: ", __LINE__, "]");
+				if (atomAlignedEnd > memoryEnd && group->allocations.size() > 1u)
+					group->log->Warning(
+						"VulkanMemoryPool::Helpers::GetMappedMemoryRange - Can not isolate VkMappedMemoryRange that does not overlap with next chunk. ",
+						"This may result in unsafe behaviour! [File:", __FILE__, "; Line: ", __LINE__, "]");
+
+				// Return range:
+				VkMappedMemoryRange range = {};
+				range.sType = VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE;
+				range.pNext = nullptr;
+				range.memory = group->vulkanMemory;
+				range.offset = atomAlignedStart;
+				range.size = (atomAlignedEnd <= memoryEnd) ? (atomAlignedEnd - atomAlignedStart) : VK_WHOLE_SIZE;
+				return range;
+			}
+		};
+
+		VulkanMemoryPool::VulkanMemoryPool(VulkanDevice* device) 
+			: m_deviceHandle(*device)
+			, m_logger(device->Log()) {
+			assert(m_deviceHandle != nullptr);
+			assert(m_logger != nullptr);
+			const VkPhysicalDeviceMemoryProperties& memoryProps = m_deviceHandle->PhysicalDevice()->MemoryProperties();
+			const VkPhysicalDeviceLimits& deviceLimits = m_deviceHandle->PhysicalDevice()->DeviceProperties().limits;
+			m_subpools.resize(memoryProps.memoryTypeCount);
+			for (size_t i = 0u; i < m_subpools.size(); i++)
+				m_subpools[i] = MemoryTypeSubpools(64u);
+			VkDeviceSize vramCapacity = static_cast<VkDeviceSize>(m_deviceHandle->PhysicalDevice()->VramCapacity());
+			m_individualAllocationThreshold = Math::Max(
+				vramCapacity / Math::Min(static_cast<VkDeviceSize>(deviceLimits.maxMemoryAllocationCount), VkDeviceSize(256u)),
+				VkDeviceSize(256u));
+		}
+
+		VulkanMemoryPool::~VulkanMemoryPool() {}
+
+		Reference<VulkanMemoryAllocation> VulkanMemoryPool::Allocate(const VkMemoryRequirements& requirements, VkMemoryPropertyFlags properties)const {
+			const VkPhysicalDeviceMemoryProperties& memoryProps = m_deviceHandle->PhysicalDevice()->MemoryProperties();
+			const VkDeviceSize alignment = Math::Max(requirements.alignment, VkDeviceSize(1u));
+
+			for (decltype(memoryProps.memoryTypeCount) memoryTypeId = 0u; memoryTypeId < memoryProps.memoryTypeCount; memoryTypeId++) {
+				// If memory type is incompatible, we should ignore it:
+				const VkMemoryType& memoryType = memoryProps.memoryTypes[memoryTypeId];
+				if ((requirements.memoryTypeBits & (1 << memoryTypeId)) == 0u ||
+					(memoryType.propertyFlags & properties) != properties)
+					continue;
+
+				// If size is small enough, we try to sub-allocate:
+				if (requirements.size <= m_individualAllocationThreshold) {
+					const VkDeviceSize minChunkSize = (
+						((memoryType.propertyFlags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT) == 0u) &&
+						((memoryType.propertyFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) != 0u))
+						? m_deviceHandle->PhysicalDevice()->DeviceProperties().limits.nonCoherentAtomSize : VkDeviceSize(32u);
+					const VkDeviceSize groupAllocationThreshold = m_individualAllocationThreshold << 1u;
+
+					const MemoryTypeSubpools& subpools = m_subpools[memoryTypeId];
+					for (size_t subpoolId = 0u; subpoolId < subpools.size(); subpoolId++) {
+						// Check if the subpool chunk size is large enough:
+						const VkDeviceSize chunkSize = minChunkSize << subpoolId;
+						const VkDeviceSize requiredChunkSize = requirements.size +
+							(((chunkSize % alignment) == 0u)
+								? VkDeviceSize(0u) : VkDeviceSize(alignment - 1u));
+						if (chunkSize < requiredChunkSize)
+							continue;
+
+						// Try to allocate within the subpool:
+						const MemoryTypeSubpool& subpool = subpools[subpoolId];
+						{
+							std::unique_lock<std::mutex> lock(subpool.lock);
+
+							// Try to allocate from existing group:
+							Reference<Helpers::AllocationGroup> group = subpool.group;
+							if (group != nullptr) {
+								const Reference<VulkanMemoryAllocation> allocation = group->Allocate(alignment, requirements.size);
+								if (allocation != nullptr)
+									return allocation;
+								else subpool.group = nullptr;
+							}
+
+							// Create new group:
+							VkDeviceSize newGroupSubAllocationCount =
+								(group != nullptr) ? (static_cast<VkDeviceSize>(group->allocations.size()) << 1u) : 1u;
+							if (group != nullptr && (chunkSize * newGroupSubAllocationCount) > groupAllocationThreshold)
+								newGroupSubAllocationCount >>= 1u;
+							group = Helpers::AllocationGroup::Create(
+								m_deviceHandle, m_logger, chunkSize, newGroupSubAllocationCount, memoryTypeId);
+
+							// If group is valid, we should be able to allocate:
+							if (group != nullptr) {
+								const Reference<VulkanMemoryAllocation> allocation = group->Allocate(alignment, requirements.size);
+								if (allocation != nullptr) {
+									subpool.group = group;
+									return allocation;
+								}
+							}
+						}
+					}
+				}
+
+				// If we got here, let us try to make a single allocation:
+				Reference<Helpers::AllocationGroup> allocationGroup = Helpers::AllocationGroup::Create(
+					m_deviceHandle, m_logger, requirements.size, 1u, memoryTypeId);
+				if (allocationGroup != nullptr)
+					return allocationGroup->Allocate(alignment, requirements.size);
+			}
+
+			// If we got here, we failed:
+			m_logger->Error("VulkanMemoryPool - Failed to allocate memory!");
+			return nullptr;
+		}
+
+
+		VkDeviceSize VulkanMemoryAllocation::Size()const { return m_size; }
+
+		VkDeviceMemory VulkanMemoryAllocation::Memory()const {
+			assert(m_allocationGroup != nullptr);
+			return dynamic_cast<VulkanMemoryPool::Helpers::AllocationGroup*>(m_allocationGroup.operator->())->vulkanMemory;
+		}
+
+		VkDeviceSize VulkanMemoryAllocation::Offset()const { return m_offset; }
+
+		VkMemoryPropertyFlags VulkanMemoryAllocation::Flags()const {
+			assert(m_allocationGroup != nullptr);
+			return dynamic_cast<VulkanMemoryPool::Helpers::AllocationGroup*>(m_allocationGroup.operator->())->propertyFlags;
+		}
+
+		void* VulkanMemoryAllocation::Map(bool read)const {
+			VulkanMemoryPool::Helpers::AllocationGroup* group = dynamic_cast<VulkanMemoryPool::Helpers::AllocationGroup*>(m_allocationGroup.operator->());
+			assert(group != nullptr);
+			void* data = group->mappedMemory;
+			if (data == nullptr) {
+				group->log->Error("VulkanMemoryAllocation::Map - Attempting to map memory invisible to host! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+				return nullptr;
+			}
+			if (read && ((group->propertyFlags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT) == 0)) {
+				const VkMappedMemoryRange range = VulkanMemoryPool::Helpers::GetNonCoherentMappedMemoryRange(this);
+				VkResult result = vkInvalidateMappedMemoryRanges(*group->device, 1u, &range);
+				if (result != VK_SUCCESS)
+					group->log->Error(
+						"VulkanMemoryAllocation::Map - Failed to invalidate memory ranges (error code: ", result, ")! "
+						"[File: ", __FILE__, "; Line: ", __LINE__, "]");
+			}
+			return static_cast<void*>(static_cast<uint8_t*>(data) + Offset());
+		}
+
+		void VulkanMemoryAllocation::Unmap(bool write)const {
+			VulkanMemoryPool::Helpers::AllocationGroup* group = dynamic_cast<VulkanMemoryPool::Helpers::AllocationGroup*>(m_allocationGroup.operator->());
+			assert(group != nullptr);
+			void* data = group->mappedMemory;
+			if (data == nullptr) {
+				group->log->Error("VulkanMemoryAllocation::Unmap - Attempting to unmap memory invisible to host! [File: ", __FILE__, "; Line: ", __LINE__, "]");
+				return;
+			}
+			if (write && ((group->propertyFlags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT) == 0)) {
+				const VkMappedMemoryRange range = VulkanMemoryPool::Helpers::GetNonCoherentMappedMemoryRange(this);
+				VkResult result = vkFlushMappedMemoryRanges(*group->device, 1u, &range);
+				if(result != VK_SUCCESS)
+					group->log->Error(
+						"VulkanMemoryAllocation::Unmap - Failed to flush memory ranges (error code: ", result, ")! ",
+						"[File: ", __FILE__, "; Line: ", __LINE__, "]");
+			}
+		}
+
+		void VulkanMemoryAllocation::OnOutOfScope()const {
+			Reference<VulkanMemoryPool::Helpers::AllocationGroup> group = m_allocationGroup;
+			assert(group != nullptr);
+			{
+				std::unique_lock<SpinLock> lock(group->allocationLock);
+				VulkanMemoryAllocation* self = group->AllocationData() + (this - group->AllocationData());
+				assert(self == this);
+				self->m_allocationGroup = nullptr;
+				assert(m_allocationGroup == nullptr);
+				group->freeAllocations.push_back(self);
+				assert(RefCount() <= 0u);
+			}
+		}
+
+		}
 		}
 	}
 }
--- __Source__/Jimara/Graphics/Vulkan/Memory/VulkanMemory.h
+++ __Source__/Jimara/Graphics/Vulkan/Memory/VulkanMemory.h
@@ -2,8 +2,22 @@
 namespace Jimara {
 	namespace Graphics {
 		namespace Vulkan {
+			namespace Legacy {
 			class VulkanMemoryPool;
 			class VulkanMemoryAllocation;
+			}
+			namespace Refactor {
+			class VulkanMemoryPool;
+			class VulkanMemoryAllocation;
+			}
+
+			/*
+			using VulkanMemoryPool = Legacy::VulkanMemoryPool;
+			using VulkanMemoryAllocation = Legacy::VulkanMemoryAllocation;
+			/*/
+			using VulkanMemoryPool = Refactor::VulkanMemoryPool;
+			using VulkanMemoryAllocation = Refactor::VulkanMemoryAllocation;
+			//*/
 		}
 	}
 }
@@ -13,6 +27,7 @@ namespace Jimara {
 namespace Jimara {
 	namespace Graphics {
 		namespace Vulkan {
+			namespace Legacy {
 			/// <summary>
 			/// Vulkan memory pool, responsible for memory allocations
 			/// </summary>
@@ -140,6 +155,125 @@ namespace Jimara {
 				VulkanMemoryAllocation(const VulkanMemoryAllocation&) = delete;
 				VulkanMemoryAllocation& operator=(const VulkanMemoryAllocation&) = delete;
 			};
+			}
+
+
+
+
+
+			namespace Refactor {
+			/// <summary>
+			/// Vulkan memory pool, responsible for memory allocations
+			/// </summary>
+			class JIMARA_API VulkanMemoryPool {
+			public:
+				/// <summary>
+				/// Allocates vulkan memory
+				/// </summary>
+				/// <param name="requirements"> Memory requirements </param>
+				/// <param name="properties"> Required memory property flags </param>
+				/// <returns> New vulkan memory allocation </returns>
+				Reference<VulkanMemoryAllocation> Allocate(const VkMemoryRequirements& requirements, VkMemoryPropertyFlags properties)const;
+
+
+			private:
+				// Device handle:
+				const Reference<VkDeviceHandle> m_deviceHandle;
+
+				// Logger:
+				const Reference<OS::Logger> m_logger;
+
+				// Per-memory-type data:
+				struct MemoryTypeSubpool {
+					mutable std::mutex lock;
+					mutable Reference<Object> group;
+				};
+				using MemoryTypeSubpools = std::vector<MemoryTypeSubpool>;
+				std::vector<MemoryTypeSubpools> m_subpools;
+
+				// Any object larger than this size will not be sub-allocated:
+				VkDeviceSize m_individualAllocationThreshold = 0u;
+
+				/// <summary>
+				/// Constructor
+				/// </summary>
+				/// <param name="device"> "Owner" graphics device </param>
+				VulkanMemoryPool(VulkanDevice* device);
+
+				/// <summary> Destructor </summary>
+				~VulkanMemoryPool();
+
+				// VulkanDevice should be allowed to instantiate VulkanMemoryPool and VulkanMemoryAllocation needs access to it's internals
+				friend class VulkanDevice;
+				friend class VulkanMemoryAllocation;
+
+				// We don't need someone playing around, copying class that's not supposed to be copied
+				VulkanMemoryPool(const VulkanMemoryPool&) = delete;
+				VulkanMemoryPool& operator=(const VulkanMemoryPool&) = delete;
+
+				// Private stuff resides in here:
+				struct Helpers;
+			};
+
+
+			/// <summary>
+			/// Vulkan memory allocation
+			/// </summary>
+			class JIMARA_API VulkanMemoryAllocation : public virtual Object {
+			public:
+				/// <summary> Allocation size </summary>
+				VkDeviceSize Size()const;
+
+				/// <summary> Device memory reference </summary>
+				VkDeviceMemory Memory()const;
+
+				/// <summary> Device memory offset </summary>
+				VkDeviceSize Offset()const;
+
+				/// <summary> Memory property flags (may and likely will contain some characteristics beyond ones requested during allocation) </summary>
+				VkMemoryPropertyFlags Flags()const;
+
+				/// <summary>
+				/// Maps memory data to CPU buffer
+				/// (Note: NEEDS corresponding Unmap() call)
+				/// </summary>
+				/// <param name="read"> If true, cpu buffer will not ignore initial content of the memory allocation </param>
+				/// <returns> Mapped memory </returns>
+				void* Map(bool read)const;
+
+				/// <summary>
+				/// Unmaps memory data
+				/// </summary>
+				/// <param name="write"> If true, GPU buffer will be updated with the content from the mapping, previously returned by Map() call </param>
+				void Unmap(bool write)const;
+
+			protected:
+				/// <summary>
+				/// Invoked, when out of scope
+				/// </summary>
+				virtual void OnOutOfScope()const override;
+
+			private:
+				// Memory allocation group
+				Reference<Object> m_allocationGroup;
+
+				// Memory offset
+				VkDeviceSize m_offset = 0u;
+
+				// Memory size
+				VkDeviceSize m_size = 0u;
+
+				// Memory pool is able to access internals
+				friend class VulkanMemoryPool;
+
+				// Constructor
+				inline VulkanMemoryAllocation() {}
+
+				// We should not be able to copy the VulkanMemoryAllocation objects as we please...
+				VulkanMemoryAllocation(const VulkanMemoryAllocation&) = delete;
+				VulkanMemoryAllocation& operator=(const VulkanMemoryAllocation&) = delete;
+			};
+			}
 		}
 	}
 }
