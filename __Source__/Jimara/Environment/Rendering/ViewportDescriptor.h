eb34e1521c8b6dc5072be60b975b08e1ae2dc468
TheDonsky <nikadon.net@gmail.com>
2024-01-18 19:13:43 +0400

    Add some flags ro frustrum descriptors

--- __Source__/Jimara/Environment/Rendering/ViewportDescriptor.h
+++ __Source__/Jimara/Environment/Rendering/ViewportDescriptor.h
@@ -5,10 +5,34 @@
 
 namespace Jimara {
 	/// <summary>
+	/// Some general information about the viewport
+	/// </summary>
+	enum class JIMARA_API RendererFrustrumFlags : uint64_t {
+		/// <summary> Empty flags </summary>
+		NONE = 0,
+
+		/// <summary> If set, this means that the viewport is important enough for all graphics objects/lights to appear on it as soon as possible </summary>
+		PRIMARY = (uint64_t(1u) << 0u),
+
+		/// <summary> If set, this flag tells that the viewport is used by a shadowmapper (can be used to control if the items can cast shadows or not, for example) </summary>
+		SHADOWMAPPER = (uint64_t(1u) << 1u)
+	};
+
+
+	/// <summary>
 	/// Generic renderer frustrum descriptor; useful for culling and distance-checking, mostly
 	/// </summary>
 	class JIMARA_API RendererFrustrumDescriptor : public virtual Object {
 	public:
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="flags"> RendererFrustrumFlags </param>
+		inline RendererFrustrumDescriptor(RendererFrustrumFlags flags = RendererFrustrumFlags::NONE) : m_flags(flags) {}
+
+		/// <summary> Some general information about the viewport </summary>
+		inline RendererFrustrumFlags Flags()const { return m_flags; }
+
 		/// <summary>
 		/// Frustrum transform
 		/// <para/> Note: For normal ViewportDescriptors, this is the same as ProjectionMatrix() * ViewMatrix(), 
@@ -32,6 +56,10 @@ namespace Jimara {
 		/// this should refer to the camera viewport instead, to allow certain renderers to match LOD-s, for example.
 		/// </summary>
 		inline virtual const RendererFrustrumDescriptor* ViewportFrustrumDescriptor()const { return this; }
+
+	private:
+		// Flags
+		const RendererFrustrumFlags m_flags;
 	};
 
 
@@ -81,4 +109,27 @@ namespace Jimara {
 		/// <param name="context"> Context, the viewport is tied to </param>
 		inline ViewportDescriptor(SceneContext* context) : m_context(context) {}
 	};
+
+
+	/// <summary>
+	/// Logical 'or' between RendererFrustrumFlags
+	/// </summary>
+	/// <param name="a"> First bitamsk </param>
+	/// <param name="b"> Second bitmask </param>
+	/// <returns> a | b </returns>
+	inline static RendererFrustrumFlags operator|(RendererFrustrumFlags a, RendererFrustrumFlags b) {
+		return static_cast<RendererFrustrumFlags>(
+			static_cast<std::underlying_type_t<RendererFrustrumFlags>>(a) | static_cast<std::underlying_type_t<RendererFrustrumFlags>>(b));
+	}
+
+	/// <summary>
+	/// Logical 'and' between RendererFrustrumFlags
+	/// </summary>
+	/// <param name="a"> First bitamsk </param>
+	/// <param name="b"> Second bitmask </param>
+	/// <returns> a & b </returns>
+	inline static RendererFrustrumFlags operator&(RendererFrustrumFlags a, RendererFrustrumFlags b) {
+		return static_cast<RendererFrustrumFlags>(
+			static_cast<std::underlying_type_t<RendererFrustrumFlags>>(a) & static_cast<std::underlying_type_t<RendererFrustrumFlags>>(b));
+	}
 }
