d083c66ee37b8332746faa47029842d7608e6d87
TheDonsky <nikadon.net@gmail.com>
2024-03-11 17:18:08 +0400

    Add generic inputs to the sample game for convenience

--- __Source__/Jimara-SampleGame/Environment/SampleGame_TypeRegistry.cpp
+++ __Source__/Jimara-SampleGame/Environment/SampleGame_TypeRegistry.cpp
@@ -1,14 +1,17 @@
 #include "../__Generated__/JIMARA_SAMPLE_GAME_TYPE_REGISTRY.impl.h"
+#include <Jimara-GenericInputs/Types.h>
 #include <Jimara-StateMachines/Types.h>
 #include <Jimara-StateMachines-Editor/Types.h>
 
 namespace Jimara {
 	namespace SampleGame {
+		static Reference<GenericInputs_TypeRegistry> genericInputsRegistryInstance = nullptr;
 		static Reference<StateMachines_TypeRegistry> stateMachinesRegistryInstance = nullptr;
 		static Reference<StateMachinesEditor_TypeRegistry> stateMachinesEditorRegistryInstance = nullptr;
 		static Reference<SampleGame_TypeRegistry> registryInstance = nullptr;
 
 		inline static void Jimara_SampleGame_OnLibraryLoad() {
+			genericInputsRegistryInstance = GenericInputs_TypeRegistry::Instance();
 			stateMachinesRegistryInstance = StateMachines_TypeRegistry::Instance();
 			stateMachinesEditorRegistryInstance = StateMachinesEditor_TypeRegistry::Instance();
 			registryInstance = SampleGame_TypeRegistry::Instance();
@@ -18,6 +21,7 @@ namespace Jimara {
 			registryInstance = nullptr;
 			stateMachinesEditorRegistryInstance = nullptr;
 			stateMachinesRegistryInstance = nullptr;
+			genericInputsRegistryInstance = nullptr;
 		}
 	}
 }
