d76cc0660f18a395e4203e28e59bca63e97dfcfc
TheDonsky <nikadon.net@gmail.com>
2024-04-01 22:00:51 +0400

    Expose Label drawer

--- __Source__/Jimara-Editor/GUI/ImGuiWrappers.cpp
+++ __Source__/Jimara-Editor/GUI/ImGuiWrappers.cpp
@@ -6,5 +6,9 @@ namespace Jimara {
 		bool Button(const std::string_view& label, const Vector2& size) {
 			return ImGui::Button(label.data(), ImVec2(size.x, size.y));
 		}
+
+		void Label(const std::string_view& text) {
+			ImGui::TextUnformatted(text.data(), text.data() + text.size());
+		}
 	}
 }
--- __Source__/Jimara-Editor/GUI/ImGuiWrappers.h
+++ __Source__/Jimara-Editor/GUI/ImGuiWrappers.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "../Environment/JimaraEditorTypeRegistry.h"
 #include <string_view>
+#include <sstream>
 #include <Jimara/Math/Math.h>
 
 
 namespace Jimara {
 	namespace Editor {
 		JIMARA_EDITOR_API bool Button(const std::string_view& label, const Vector2& size = Vector2(0.0f));
+
+		JIMARA_EDITOR_API void Label(const std::string_view& text);
 	}
 }
