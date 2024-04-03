561b997d4526e97c45cd6dc72d05e7e41dac85c5
TheDonsky <nikadon.net@gmail.com>
2024-04-03 17:18:16 +0400

    Improve preffered device selection

--- __Source__/Jimara/Graphics/Rendering/RenderSurface.cpp
+++ __Source__/Jimara/Graphics/Rendering/RenderSurface.cpp
@@ -14,25 +14,58 @@ namespace Jimara {
 					&& surface->DeviceCompatible(info);
 			};
 
+			struct DeviceRank {
+				size_t deviceId = ~size_t(0u);
+				size_t typeRank = 0u;
+				size_t featureRank = 0u;
+				size_t vramCapacity = 0u;
+
+				inline bool operator<(const DeviceRank& other) const {
+					return 
+						(typeRank < other.typeRank) ? true : (typeRank > other.typeRank) ? false :
+						(featureRank < other.featureRank) ? true : (featureRank > other.featureRank) ? false :
+						(vramCapacity < other.vramCapacity);
+				}
+			};
+
 			const size_t deviceCount = GraphicsInstance()->PhysicalDeviceCount();
-			size_t bestId = deviceCount;
-			uint8_t bestIsDescrete = 0;
-			uint64_t bestVRAM = 0;
+			DeviceRank bestRank;
 			for (size_t deviceId = 0; deviceId < deviceCount; deviceId++) {
 				const Graphics::PhysicalDevice* info = GraphicsInstance()->GetPhysicalDevice(deviceId);
 				if (!deviceViable(info, this)) continue;
 				
-				uint8_t isDescrete = ((info->Type() == Graphics::PhysicalDevice::DeviceType::DESCRETE) ? 1 : 0);
-				if (deviceId > 0) {
-					if (bestIsDescrete > isDescrete) continue;
-					else if ((bestIsDescrete == isDescrete) && bestVRAM >= info->VramCapacity()) continue;
+				DeviceRank rank = {};
+				{
+					rank.deviceId = deviceId;
+					switch (info->Type()) {
+					case Graphics::PhysicalDevice::DeviceType::OTHER:
+						rank.typeRank = 2u;
+						break;
+					case Graphics::PhysicalDevice::DeviceType::DESCRETE:
+						rank.typeRank = 32u;
+						break;
+					case Graphics::PhysicalDevice::DeviceType::INTEGRATED:
+						rank.typeRank = 16u;
+						break;
+					case Graphics::PhysicalDevice::DeviceType::VIRTUAL:
+						rank.typeRank = 8u;
+						break;
+					case Graphics::PhysicalDevice::DeviceType::CPU:
+						rank.typeRank = 1u;
+						break;
+					default:
+						break;
+					}
+					rank.featureRank |=
+						((info->Features() & static_cast<uint64_t>(Graphics::PhysicalDevice::DeviceFeature::FRAGMENT_SHADER_INTERLOCK)) != 0) ? 1u : 0u;
+					rank.vramCapacity = info->VramCapacity();
 				}
-				bestId = deviceId;
-				bestIsDescrete = isDescrete;
-				bestVRAM = info->VramCapacity();
+
+				if (bestRank.deviceId >= deviceCount || bestRank < rank)
+					bestRank = rank;
 			}
 
-			return (bestId >= deviceCount) ? nullptr : GraphicsInstance()->GetPhysicalDevice(bestId);
+			return (bestRank.deviceId >= deviceCount) ? nullptr : GraphicsInstance()->GetPhysicalDevice(bestRank.deviceId);
 		}
 
 		RenderSurface::RenderSurface(Graphics::GraphicsInstance* graphicsInstance) : m_graphicsInstance(graphicsInstance) {}
