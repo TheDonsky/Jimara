5ee9098016f1e8a66e2bb48c4a146de3c6daee6d
TheDonsky <nikadon.net@gmail.com>
2024-04-24 22:03:09 +0400

    Define a structure for storing device extension function pointers related to ray-tracing features

---
+++ __Source__/Jimara/Graphics/Vulkan/VulkanRayTracingAPI.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "VulkanInstance.h"
+
+namespace Jimara {
+	namespace Graphics {
+		namespace Vulkan {
+			/// <summary>
+			/// Extension functions for Ray-Tracing functions
+			/// </summary>
+			struct JIMARA_API VulkanRayTracingAPI {
+				PFN_vkGetAccelerationStructureBuildSizesKHR GetAccelerationStructureBuildSizes = nullptr;
+				PFN_vkCreateAccelerationStructureKHR CreateAccelerationStructure = nullptr;
+				PFN_vkDestroyAccelerationStructureKHR DestroyAccelerationStructure = nullptr;
+
+				/// <summary>
+				/// Fills-in the methodss
+				/// </summary>
+				/// <param name="device"> Logical device handle </param>
+				inline void FindAPIMethods(VkDevice device);
+			};
+
+
+			/// <summary>
+			/// Fills-in the methodss
+			/// </summary>
+			/// <param name="device"> Logical device handle </param>
+			inline void VulkanRayTracingAPI::FindAPIMethods(VkDevice device) {
+				if (device == VK_NULL_HANDLE)
+					return;
+
+				auto find = [&](auto& address, const char* name) {
+					address = reinterpret_cast<std::remove_reference_t<decltype(address)>>(vkGetDeviceProcAddr(device, name));
+				};
+
+				// Acceleration structure:
+				{
+					find(GetAccelerationStructureBuildSizes, "vkGetAccelerationStructureBuildSizesKHR");
+					find(CreateAccelerationStructure, "vkCreateAccelerationStructureKHR");
+					find(DestroyAccelerationStructure, "vkDestroyAccelerationStructureKHR");
+				}
+			}
+		}
+	}
+}
