f6bf23292c00ceafbbeec007445cf295e69d9850
TheDonsky <nikadon.net@gmail.com>
2024-05-26 18:13:56 +0400

    Add a test for Ray-Queries from Compute pipelines

--- __Source__/Jimara-Tests/Graphics/RayTracing/RayTracingAPITest.cpp
+++ __Source__/Jimara-Tests/Graphics/RayTracing/RayTracingAPITest.cpp
@@ -89,6 +89,8 @@ namespace Jimara {
 				log->Warning("No RT-Capable GPU was found!");
 		}
 
+
+
 		namespace {
 			template<typename DataType>
 			using RayTracingAPITest_DataCreateFn = Function<Reference<DataType>, const RenderEngineInfo*>;
@@ -162,6 +164,8 @@ namespace Jimara {
 				window->OnUpdate() -= updateFn;
 			}
 		}
+
+
 
 		TEST(RayTracingAPITest, InlineRayTracing_Fragment) {
 			const Reference<Jimara::Test::CountingLogger> log = Object::Instantiate<Jimara::Test::CountingLogger>();
@@ -363,6 +367,205 @@ namespace Jimara {
 					RayTracingAPITest_RenderFunction<RendererData>::FromCall(&renderImage));
 				ASSERT_NE(engine, nullptr);
 				
+				{
+					std::stringstream stream;
+					stream << window->Name() << " - " << physDevice->Name();
+					const std::string name = stream.str();
+					RayTracingAPITest_RenderLoop(engine, window, name);
+				}
+			}
+
+			EXPECT_EQ(warningCount, log->NumWarning());
+			EXPECT_EQ(failureCount, log->Numfailures());
+
+			if (!deviceFound)
+				log->Warning("No RT-Capable GPU was found!");
+		}
+
+
+
+		TEST(RayTracingAPITest, InlineRayTracing_Compute) {
+			const Reference<Jimara::Test::CountingLogger> log = Object::Instantiate<Jimara::Test::CountingLogger>();
+			const Reference<Application::AppInformation> appInfo = Object::Instantiate<Application::AppInformation>("RayTracingAPITest");
+
+			const Reference<GraphicsInstance> graphicsInstance = GraphicsInstance::Create(log, appInfo);
+			ASSERT_NE(graphicsInstance, nullptr);
+
+			auto getShader = [&](const char* stage) {
+				static const std::string path = "Shaders/47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU/Jimara-Tests/Graphics/RayTracing/InlineRayTracing.";
+				Reference<SPIRV_Binary> binary = SPIRV_Binary::FromSPVCached(path + stage + ".spv", log);
+				if (binary == nullptr)
+					log->Fatal("BindlessRenderer::EngineData - Failed to load ", stage, " shader!");
+				return binary;
+			};
+			const Reference<SPIRV_Binary> shader = getShader("comp");
+			ASSERT_NE(shader, nullptr);
+
+			const Reference<TriMesh> sphere = MeshConstants::Tri::Sphere();
+			ASSERT_NE(sphere, nullptr);
+
+			const size_t warningCount = log->NumWarning();
+			const size_t failureCount = log->Numfailures();
+
+			bool deviceFound = false;
+			for (size_t deviceId = 0u; deviceId < graphicsInstance->PhysicalDeviceCount(); deviceId++) {
+				// Filter device and create window:
+				PhysicalDevice* const physDevice = graphicsInstance->GetPhysicalDevice(deviceId);
+				if (!physDevice->HasFeatures(PhysicalDevice::DeviceFeatures::RAY_TRACING))
+					continue;
+				const Reference<OS::Window> window = OS::Window::Create(log, "InlineRayTracing_Compute");
+				ASSERT_NE(window, nullptr);
+				const Reference<RenderSurface> surface = graphicsInstance->CreateRenderSurface(window);
+				ASSERT_NE(surface, nullptr);
+				if (!surface->DeviceCompatible(physDevice))
+					continue;
+				deviceFound = true;
+
+				// Create device:
+				const Reference<GraphicsDevice> device = physDevice->CreateLogicalDevice();
+				ASSERT_NE(device, nullptr);
+
+				// Prepare resources for BLAS:
+				const Reference<GraphicsMesh> graphicsMesh = GraphicsMesh::Cached(device, sphere, GraphicsPipeline::IndexType::TRIANGLE);
+				ASSERT_NE(graphicsMesh, nullptr);
+
+				ArrayBufferReference<MeshVertex> vertexBuffer;
+				ArrayBufferReference<uint32_t> indexBuffer;
+				graphicsMesh->GetBuffers(vertexBuffer, indexBuffer);
+				ASSERT_NE(vertexBuffer, nullptr);
+				ASSERT_NE(indexBuffer, nullptr);
+
+				BottomLevelAccelerationStructure::Properties blasProps;
+				blasProps.maxVertexCount = static_cast<uint32_t>(vertexBuffer->ObjectCount());
+				blasProps.maxTriangleCount = static_cast<uint32_t>(indexBuffer->ObjectCount() / 3u);
+				const Reference<BottomLevelAccelerationStructure> blas = device->CreateBottomLevelAccelerationStructure(blasProps);
+				ASSERT_NE(blas, nullptr);
+				bool blasBuilt = false;
+
+				// Prepare resources for TLAS:
+				const ArrayBufferReference<AccelerationStructureInstanceDesc> instanceDesc =
+					device->CreateArrayBuffer<AccelerationStructureInstanceDesc>(1u, ArrayBuffer::CPUAccess::CPU_WRITE_ONLY);
+				ASSERT_NE(instanceDesc, nullptr);
+
+				TopLevelAccelerationStructure::Properties tlasProps;
+				tlasProps.maxBottomLevelInstances = 1u;
+				tlasProps.flags = AccelerationStructure::Flags::ALLOW_UPDATES | AccelerationStructure::Flags::PREFER_FAST_BUILD;
+				const Reference<TopLevelAccelerationStructure> tlas = device->CreateTopLevelAccelerationStructure(tlasProps);
+				ASSERT_NE(tlas, nullptr);
+
+				// Create constant buffer:
+				struct Settings {
+					alignas(16) Vector3 right;
+					alignas(16) Vector3 up;
+					alignas(16) Vector3 forward;
+					alignas(16) Vector3 position;
+				};
+				const BufferReference<Settings> settingsBuffer = device->CreateConstantBuffer<Settings>();
+				ASSERT_NE(settingsBuffer, nullptr);
+
+				// Create renderer:
+				struct RendererData : public virtual Object {
+					float aspectRatio = 1.0f;
+					Reference<ComputePipeline> pipeline;
+					Reference<BindingSet> bindings;
+					Reference<TextureView> frameBufer;
+					Reference<const RenderEngineInfo> engineInfo;
+				};
+
+				auto dataCreate = [&](const RenderEngineInfo* engineInfo) -> Reference<RendererData> {
+					const Reference<RendererData> data = Object::Instantiate<RendererData>();
+					{
+						data->aspectRatio = float(engineInfo->ImageSize().x) / float(Math::Max(engineInfo->ImageSize().y, 1u));
+						data->engineInfo = engineInfo;
+					}
+					{
+						data->pipeline = device->GetComputePipeline(shader);
+						assert(data->pipeline != nullptr);
+					}
+					{
+						data->frameBufer = device->CreateTexture(
+							Texture::TextureType::TEXTURE_2D, Texture::PixelFormat::R16G16B16A16_SFLOAT,
+							Size3(engineInfo->ImageSize(), 1u), 1u, false, ImageTexture::AccessFlags::SHADER_WRITE)
+							->CreateView(TextureView::ViewType::VIEW_2D);
+						assert(data->frameBufer != nullptr);
+					}
+					{
+						const Reference<BindingPool> bindingPool = device->CreateBindingPool(engineInfo->ImageCount());
+						assert(bindingPool != nullptr);
+						BindingSet::Descriptor desc = {};
+						desc.pipeline = data->pipeline;
+
+						const Reference<const ResourceBinding<Buffer>> cbufferBinding =
+							Object::Instantiate<ResourceBinding<Buffer>>(settingsBuffer);
+						auto findConstantBuffer = [&](const auto&) { return cbufferBinding; };
+						desc.find.constantBuffer = &findConstantBuffer;
+
+						const Reference<const ResourceBinding<TopLevelAccelerationStructure>> tlasBinding =
+							Object::Instantiate<ResourceBinding<TopLevelAccelerationStructure>>(tlas);
+						auto findTlas = [&](const auto&) { return tlasBinding; };
+						desc.find.accelerationStructure = &findTlas;
+
+						const Reference<const ResourceBinding<TextureView>> imageBinding = 
+							Object::Instantiate<ResourceBinding<TextureView>>(data->frameBufer);
+						auto findImage = [&](const auto&) { return imageBinding; };
+						desc.find.textureView = &findImage;
+
+						data->bindings = bindingPool->AllocateBindingSet(desc);
+						assert(data->bindings != nullptr);
+					}
+					return data;
+				};
+
+				const Stopwatch elapsed;
+				auto renderImage = [&](RendererData* data, const InFlightBufferInfo& commands) {
+					{
+						AccelerationStructureInstanceDesc& desc = *instanceDesc.Map();
+						desc.transform[0u] = Vector4(1.0f, 0.0f, 0.0f, 0.0f);
+						desc.transform[1u] = Vector4(0.0f, 1.0f, 0.0f, std::sin(elapsed.Elapsed()));
+						desc.transform[2u] = Vector4(0.0f, 0.0f, 1.0f, 0.0f);
+						desc.instanceCustomIndex = 0u;
+						desc.visibilityMask = ~uint8_t(0u);
+						desc.shaderBindingTableRecordOffset = 0u;
+						desc.instanceFlags = 0u;
+						desc.blasDeviceAddress = blas->DeviceAddress();
+						instanceDesc->Unmap(true);
+					}
+
+					if (!blasBuilt) {
+						blas->Build(commands, vertexBuffer, sizeof(MeshVertex), offsetof(MeshVertex, position), indexBuffer);
+						tlas->Build(commands, instanceDesc);
+						blasBuilt = true;
+					}
+					else tlas->Build(commands, instanceDesc, tlas);
+
+					{
+						Settings& settings = settingsBuffer.Map();
+						const float angle = elapsed.Elapsed() * 0.5f;
+						settings.right = Math::Right();
+						settings.position = (Math::Back() * std::cos(angle) + Math::Right() * std::sin(angle)) * 5.0f;
+						settings.forward = Math::Normalize(-settings.position);
+						settings.up = Math::Up();
+						settings.right = Math::Normalize(Math::Cross(settings.up, settings.forward)) * data->aspectRatio;
+						settingsBuffer->Unmap(true);
+					}
+
+					data->bindings->Update(commands);
+					
+					{
+						const constexpr uint32_t BLOCK_SIZE = 16u;
+						data->bindings->Bind(commands);
+						data->pipeline->Dispatch(commands, Size3(
+							(Size2(data->frameBufer->TargetTexture()->Size()) + BLOCK_SIZE - 1u) / BLOCK_SIZE, 1u));
+					}
+
+					data->engineInfo->Image(commands)->Blit(commands, data->frameBufer->TargetTexture());
+				};
+
+				const Reference<RenderEngine> engine = RayTracingAPITest_CreateRenderEngine(device, surface,
+					RayTracingAPITest_DataCreateFn<RendererData>::FromCall(&dataCreate),
+					RayTracingAPITest_RenderFunction<RendererData>::FromCall(&renderImage));
+				ASSERT_NE(engine, nullptr);
+
 				{
 					std::stringstream stream;
 					stream << window->Name() << " - " << physDevice->Name();
