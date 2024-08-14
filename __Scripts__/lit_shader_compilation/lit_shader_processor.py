4fd971b796c391a25baf4fed7dd2de036a6612d1
TheDonsky <nikadon.net@gmail.com>
2024-08-15 00:20:18 +0400

    Define blend mode and flag options

--- __Scripts__/lit_shader_compilation/lit_shader_processor.py
+++ __Scripts__/lit_shader_compilation/lit_shader_processor.py
@@ -2,7 +2,7 @@ import sys, os
 if __name__ == "__main__":
 	sys.path.insert(0, os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + "/../"))
 from code_analysis.source_cache import source_cache
-from code_analysis.preprocessor import preporocessor_state, source_line, macro_definition
+from code_analysis.preprocessor import preporocessor_state, source_line, macro_definition, evaluate_statement
 
 
 JM_BlendMode_pragma_name = "JM_BlendMode"
@@ -37,20 +37,34 @@ built_in_primitive_typenames = {
 }
 
 
+JM_Blend_Opaque_name =		'JM_Blend_Opaque'
+JM_Blend_Opaque_value =		0
+JM_Blend_Alpha_name =		'JM_Blend_Alpha'
+JM_Blend_Alpha_value =		1
+JM_Blend_Additive_name =	'JM_Blend_Additive'
+JM_Blend_Additive_value =	2
 blend_mode_options: dict[str, int]
 blend_mode_options = {
-	'JM_Blend_Opaque':		0,
-	'JM_Blend_Alpha':		1,
-	'JM_Blend_Additive':	2
+	JM_Blend_Opaque_name:	JM_Blend_Opaque_value,
+	JM_Blend_Alpha_name:	JM_Blend_Alpha_value,
+	JM_Blend_Additive_name:	JM_Blend_Additive_value
 }
 
 
+JM_CanDiscard_name =					'JM_CanDiscard'
+JM_CanDiscard_value =					(1 << 0)
+JM_UseObjectId_name =					'JM_UseObjectId'
+JM_UseObjectId_value =					(1 << 1)
+JM_UsePerVertexTilingAndOffset_name =	'JM_UsePerVertexTilingAndOffset'
+JM_UsePerVertexTilingAndOffset_value =	(1 << 2)
+JM_UseVertexColor_name =				'JM_UseVertexColor'
+JM_UseVertexColor_value =				(1 << 3)
 material_flags: dict[str, int]
 material_flags = {
-	'JM_CanDiscard':					(1 << 0),
-	'JM_UseObjectId':					(1 << 1),
-	'JM_UsePerVertexTilingAndOffset':	(1 << 2),
-	'JM_UseVertexColor':				(1 << 3)
+	JM_CanDiscard_name:						JM_CanDiscard_value,
+	JM_UseObjectId_name:					JM_UseObjectId_value,
+	JM_UsePerVertexTilingAndOffset_name:	JM_UsePerVertexTilingAndOffset_value,
+	JM_UseVertexColor_name:					JM_UseVertexColor_value
 }
 
 
@@ -71,10 +85,13 @@ class fragment_field:
 
 class lit_shader_data:
 	def __init__(self) -> None:
+		self.blend_mode = JM_Blend_Opaque_name
+		self.material_flags = 0
 		self.material_properties: list[material_property]
 		self.material_properties = []
 		self.fragment_fields: list[fragment_field]
 		self.fragment_fields = []
+		self.shading_state_size = 0
 
 
 def parse_lit_shader(src_cache: source_cache, jls_path: str) -> lit_shader_data:
@@ -88,12 +105,22 @@ def parse_lit_shader(src_cache: source_cache, jls_path: str) -> lit_shader_data:
 
 	def process_blend_mode_pragma(args: source_line):
 		assert(isinstance(args, source_line))
-		print(JM_BlendMode_pragma_name + " " + args.processed_text)
+		try:
+			result.blend_mode = evaluate_statement(args.processed_text)
+			print('Blend Mode: ' + str(result.blend_mode))
+		except:
+			print('Failed to evaluate blend mode: ' + repr(args.processed_text) + '! ' + str(args))
+			assert(False)
 	preprocessor.pragma_handlers[JM_BlendMode_pragma_name] = process_blend_mode_pragma
 
 	def process_material_flags_pragma(args: source_line):
 		assert(isinstance(args, source_line))
-		print(JM_MaterialFlags_pragma_name + " " + args.processed_text)
+		try:
+			result.material_flags = evaluate_statement(args.processed_text)
+			print('Material Flags: ' + str(result.material_flags))
+		except:
+			print('Failed to evaluate material flags: ' + repr(args.processed_text) + '!' + str(args))
+			assert(False)
 	preprocessor.pragma_handlers[JM_MaterialFlags_pragma_name] = process_material_flags_pragma
 
 	def process_materialProperty_pragma(args: source_line):
