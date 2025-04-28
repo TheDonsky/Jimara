df4cf15da80637fb46a1c697b8c3c5a5868c0c13
TheDonsky <nikadon.net@gmail.com>
2025-04-28 15:08:47 +0400

    Start implementing unit-tests for SerializedActions

---
+++ __Source__/Jimara-Tests/Data/SerializedActionTest.cpp
@@ -0,0 +1,314 @@
+#include "../GtestHeaders.h"
+#include "../Memory.h"
+#include "../CountingLogger.h"
+#include "Data/Serialization/SerializedAction.h"
+#include <tuple>
+
+
+namespace Jimara {
+	namespace Serialization {
+		TEST(SerializedActionTest, NoArguments) {
+			int callCount = 0;
+			auto call = [&]() { callCount++; };
+			const Callback<> callback = Callback<>::FromCall(&call);
+
+			SerializedAction action = SerializedCallback::Create<>::From("Call", callback);
+
+			ASSERT_EQ(action.Name(), "Call");
+			ASSERT_EQ(callCount, 0);
+
+			Reference<SerializedCallback::Instance> instance = action.CreateInstance();
+			ASSERT_NE(instance, nullptr);
+			ASSERT_EQ(instance->ArgumentCount(), 0u);
+			ASSERT_EQ(callCount, 0);
+
+			instance->Invoke();
+			ASSERT_EQ(callCount, 1);
+
+			instance->Invoke();
+			ASSERT_EQ(callCount, 2);
+
+			size_t fieldCount = 0u;
+			auto examineField = [&](const SerializedObject& item) { fieldCount++; };
+			instance->GetFields(Callback<SerializedObject>::FromCall(&examineField));
+			ASSERT_EQ(fieldCount, 0u);
+
+			instance->Invoke();
+			ASSERT_EQ(callCount, 3);
+		}
+
+		TEST(SerializedActionTest, OneArgumentUnnamed) {
+			int counter = 0;
+			auto call = [&](int count) { counter += count; };
+			const Callback<int> callback = Callback<int>::FromCall(&call);
+
+			SerializedAction action = SerializedCallback::Create<int>::From("Call", callback);
+
+			ASSERT_EQ(action.Name(), "Call");
+			ASSERT_EQ(counter, 0);
+
+			Reference<SerializedCallback::Instance> instance = action.CreateInstance();
+			ASSERT_NE(instance, nullptr);
+			ASSERT_EQ(instance->ArgumentCount(), 1u);
+			ASSERT_EQ(counter, 0);
+
+			instance->Invoke();
+			ASSERT_EQ(counter, 0);
+
+			instance->Invoke();
+			ASSERT_EQ(counter, 0);
+
+			{
+				bool found = false;
+				bool nonIntFound = false;
+				bool nonEmptyNameFound = false;
+				size_t fieldCount = 0u;
+				auto examineField = [&](const SerializedObject& item) {
+					fieldCount++;
+					if (item.Serializer()->TargetName() != "")
+						nonEmptyNameFound = true;
+					if (item.Serializer()->GetType() != ItemSerializer::Type::INT_VALUE) {
+						nonIntFound = true;
+						return;
+					}
+					found = true;
+					item = 2;
+				};
+				instance->GetFields(Callback<SerializedObject>::FromCall(&examineField));
+				ASSERT_TRUE(found);
+				ASSERT_FALSE(nonIntFound);
+				ASSERT_EQ(fieldCount, 1u);
+				ASSERT_FALSE(nonEmptyNameFound);
+			}
+			ASSERT_EQ(counter, 0);
+			instance->Invoke();
+			ASSERT_EQ(counter, 2);
+
+			{
+				bool found = false;
+				bool nonIntFound = false;
+				bool nonEmptyNameFound = false;
+				size_t fieldCount = 0u;
+				auto examineField = [&](const SerializedObject& item) {
+					fieldCount++;
+					if (item.Serializer()->TargetName() != "")
+						nonEmptyNameFound = true;
+					if (item.Serializer()->GetType() != ItemSerializer::Type::INT_VALUE) {
+						nonIntFound = true;
+						return;
+					}
+					found = true;
+					item = 5;
+				};
+				instance->GetFields(Callback<SerializedObject>::FromCall(&examineField));
+				ASSERT_TRUE(found);
+				ASSERT_FALSE(nonIntFound);
+				ASSERT_EQ(fieldCount, 1u);
+				ASSERT_FALSE(nonEmptyNameFound);
+			}
+			ASSERT_EQ(counter, 2);
+			instance->Invoke();
+			ASSERT_EQ(counter, 7);
+		}
+
+		TEST(SerializedActionTest, OneArgumentNamed) {
+			int counter = 0;
+			auto call = [&](int count) { counter += count; };
+			const Callback<int> callback = Callback<int>::FromCall(&call);
+			const std::string argName = "Count";
+
+			SerializedAction action = SerializedCallback::Create<int>::From("Call", callback, argName);
+
+			ASSERT_EQ(action.Name(), "Call");
+			ASSERT_EQ(counter, 0);
+
+			Reference<SerializedCallback::Instance> instance = action.CreateInstance();
+			ASSERT_NE(instance, nullptr);
+			ASSERT_EQ(instance->ArgumentCount(), 1u);
+			ASSERT_EQ(counter, 0);
+
+			instance->Invoke();
+			ASSERT_EQ(counter, 0);
+
+			instance->Invoke();
+			ASSERT_EQ(counter, 0);
+
+			{
+				bool found = false;
+				bool nonIntFound = false;
+				bool incorrectNameFound = false;
+				size_t fieldCount = 0u;
+				auto examineField = [&](const SerializedObject& item) {
+					fieldCount++;
+					if (item.Serializer()->TargetName() != argName)
+						incorrectNameFound = true;
+					if (item.Serializer()->GetType() != ItemSerializer::Type::INT_VALUE) {
+						nonIntFound = true;
+						return;
+					}
+					found = true;
+					item = 2;
+				};
+				instance->GetFields(Callback<SerializedObject>::FromCall(&examineField));
+				ASSERT_TRUE(found);
+				ASSERT_FALSE(nonIntFound);
+				ASSERT_EQ(fieldCount, 1u);
+				ASSERT_FALSE(incorrectNameFound);
+			}
+			ASSERT_EQ(counter, 0);
+			instance->Invoke();
+			ASSERT_EQ(counter, 2);
+
+			{
+				bool found = false;
+				bool nonIntFound = false;
+				bool incorrectNameFound = false;
+				size_t fieldCount = 0u;
+				auto examineField = [&](const SerializedObject& item) {
+					fieldCount++;
+					if (item.Serializer()->TargetName() != argName)
+						incorrectNameFound = true;
+					if (item.Serializer()->GetType() != ItemSerializer::Type::INT_VALUE) {
+						nonIntFound = true;
+						return;
+					}
+					found = true;
+					item = 5;
+				};
+				instance->GetFields(Callback<SerializedObject>::FromCall(&examineField));
+				ASSERT_TRUE(found);
+				ASSERT_FALSE(nonIntFound);
+				ASSERT_EQ(fieldCount, 1u);
+				ASSERT_FALSE(incorrectNameFound);
+			}
+			ASSERT_EQ(counter, 2);
+			instance->Invoke();
+			ASSERT_EQ(counter, 7);
+		}
+
+		TEST(SerializedActionTest, OneArgumentCustomSerializer) {
+			int counter = 0;
+			auto call = [&](int count) { counter += count; };
+			const Callback<int> callback = Callback<int>::FromCall(&call);
+			const Reference<const ValueSerializer<int>> serializer = ValueSerializer<int>::Create("Count!!!");
+
+			SerializedAction action = SerializedCallback::Create<int>::From("Call", callback, serializer);
+
+			ASSERT_EQ(action.Name(), "Call");
+			ASSERT_EQ(counter, 0);
+
+			Reference<SerializedCallback::Instance> instance = action.CreateInstance();
+			ASSERT_NE(instance, nullptr);
+			ASSERT_EQ(instance->ArgumentCount(), 1u);
+			ASSERT_EQ(counter, 0);
+
+			instance->Invoke();
+			ASSERT_EQ(counter, 0);
+
+			instance->Invoke();
+			ASSERT_EQ(counter, 0);
+
+			{
+				bool found = false;
+				bool nonIntFound = false;
+				bool incorrectSerializerFound = false;
+				size_t fieldCount = 0u;
+				auto examineField = [&](const SerializedObject& item) {
+					fieldCount++;
+					if (item.Serializer() != serializer)
+						incorrectSerializerFound = true;
+					if (item.Serializer()->GetType() != ItemSerializer::Type::INT_VALUE) {
+						nonIntFound = true;
+						return;
+					}
+					found = true;
+					item = 2;
+				};
+				instance->GetFields(Callback<SerializedObject>::FromCall(&examineField));
+				ASSERT_TRUE(found);
+				ASSERT_FALSE(nonIntFound);
+				ASSERT_EQ(fieldCount, 1u);
+				ASSERT_FALSE(incorrectSerializerFound);
+			}
+			ASSERT_EQ(counter, 0);
+			instance->Invoke();
+			ASSERT_EQ(counter, 2);
+
+			{
+				bool found = false;
+				bool nonIntFound = false;
+				bool incorrectSerializerFound = false;
+				size_t fieldCount = 0u;
+				auto examineField = [&](const SerializedObject& item) {
+					fieldCount++;
+					if (item.Serializer() != serializer)
+						incorrectSerializerFound = true;
+					if (item.Serializer()->GetType() != ItemSerializer::Type::INT_VALUE) {
+						nonIntFound = true;
+						return;
+					}
+					found = true;
+					item = 5;
+				};
+				instance->GetFields(Callback<SerializedObject>::FromCall(&examineField));
+				ASSERT_TRUE(found);
+				ASSERT_FALSE(nonIntFound);
+				ASSERT_EQ(fieldCount, 1u);
+				ASSERT_FALSE(incorrectSerializerFound);
+			}
+			ASSERT_EQ(counter, 2);
+			instance->Invoke();
+			ASSERT_EQ(counter, 7);
+		}
+
+		TEST(SerializedActionTest, TwoArguments_Unnamed) {
+			int sumA = 0u;
+			float sumB = 0u;
+			auto call = [&](int a, float b) { sumA += a; sumB += b; };
+			const Callback<int, float> callback = Callback<int, float>::FromCall(&call);
+
+			SerializedAction action = SerializedCallback::Create<int, float>::From("Call", callback);
+
+			ASSERT_EQ(action.Name(), "Call");
+			ASSERT_EQ(sumA, 0);
+			ASSERT_EQ(sumB, 0.0f);
+
+			Reference<SerializedCallback::Instance> instance = action.CreateInstance();
+			ASSERT_NE(instance, nullptr);
+			ASSERT_EQ(instance->ArgumentCount(), 2u);
+			ASSERT_EQ(sumA, 0);
+			ASSERT_EQ(sumB, 0.0f);
+
+			instance->Invoke();
+			ASSERT_EQ(sumA, 0);
+			ASSERT_EQ(sumB, 0.0f);
+
+			std::vector<SerializedObject> args = {};
+			{
+				auto examineField = [&](const SerializedObject& item) { args.push_back(item); };
+				instance->GetFields(Callback<SerializedObject>::FromCall(&examineField));
+			}
+			ASSERT_EQ(args.size(), 2u);
+			ASSERT_TRUE(args[0u].Serializer()->GetType() == ItemSerializer::Type::INT_VALUE);
+			ASSERT_TRUE(args[0u].Serializer()->TargetName() == "");
+			ASSERT_TRUE(args[1u].Serializer()->GetType() == ItemSerializer::Type::FLOAT_VALUE);
+			ASSERT_TRUE(args[1u].Serializer()->TargetName() == "");
+
+			args[0u] = 1;
+			instance->Invoke();
+			ASSERT_EQ(sumA, 1);
+			ASSERT_EQ(sumB, 0.0f);
+
+			args[1u] = 4.0f;
+			instance->Invoke();
+			ASSERT_EQ(sumA, 2);
+			ASSERT_EQ(sumB, 4.0f);
+
+			args[0u] = 2;
+			args[1u] = 7.0f;
+			instance->Invoke();
+			ASSERT_EQ(sumA, 4);
+			ASSERT_LT(std::abs(sumB - 11.0f), std::numeric_limits<float>::epsilon());
+		}
+	}
+}
