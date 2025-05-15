ef5c854f5ad6a21194152194e3dd71cc0bff8e41
TheDonsky <nikadon.net@gmail.com>
2025-05-16 00:04:43 +0400

    Add a generic input that performs raycasts to evaluate results

---
+++ __Source__/Jimara-GenericInputs/Physical/RaycastInput.cpp
@@ -0,0 +1,243 @@
+#include "RaycastInput.h"
+#include <Jimara/Environment/Layers.h>
+#include <Jimara/Data/Serialization/Helpers/SerializerMacros.h>
+
+
+namespace Jimara {
+	struct RaycastInput::Helpers {
+		static void Update(RaycastInput* self) {
+			const uint64_t frameId = self->Context()->FrameIndex();
+			if ((frameId == self->m_lastUpdateFrame) && 
+				((self->m_flags & Flags::DISABLE_FRAME_CACHING) == Flags::NONE))
+				return;
+			self->m_lastUpdateFrame = frameId;
+			self->m_lastResult.collider = nullptr;
+
+			const float inputMaxDistance = InputProvider<float>::GetInput(self->m_maxDistance, std::numeric_limits<float>::epsilon());
+			if (std::isnan(inputMaxDistance) || inputMaxDistance <= 0.0f)
+				return;
+
+			const Vector3 inputOrigin = InputProvider<Vector3>::GetInput(self->m_originInput, Vector3(0.0f, 0.0f, 0.0f));
+			const Vector3 inputDirection = InputProvider<Vector3>::GetInput(self->m_directionInput, Math::Forward());
+
+			const bool originLocalSpace = (self->m_flags & Flags::QUERY_ORIGIN_WORLD_SPACE) == Flags::NONE;
+			const bool directionLocalSpace = (self->m_flags & Flags::QUERY_DIRECTION_WORLD_SPACE) == Flags::NONE;
+			const bool scaleMaxDistance = (self->m_flags & Flags::SCALE_MAX_DISTANCE_BY_LOSSY_SCALE) != Flags::NONE;
+			
+			const bool scaleSweepShape =
+				((self->m_flags & Flags::SCALE_SWEEP_SHAPE_BY_LOSSY_SCALE) != Flags::NONE) && (self->m_queryType != QueryType::RAY);
+			const bool rotateSweepShape =
+				((self->m_flags & Flags::DO_NOT_ROTATE_SWEEP_SHAPE) == Flags::NONE) && (self->m_queryType != QueryType::RAY);
+
+			const bool transformNeeded = (originLocalSpace || directionLocalSpace || scaleMaxDistance || scaleSweepShape || rotateSweepShape);
+			const bool rotationMatrixNeeded = (scaleMaxDistance || scaleSweepShape || rotateSweepShape);
+			const bool scaleNeeded = (scaleMaxDistance || scaleSweepShape);
+
+			const Transform* transform = (transformNeeded) ? self->GetTransfrom() : nullptr;
+			const Matrix4 worldMatrix = (transform != nullptr) ? transform->WorldMatrix() : Math::Identity();
+			const Matrix4 worldRotationMatrix = (transform != nullptr && rotationMatrixNeeded) ? transform->WorldRotationMatrix() : Math::Identity();
+			const Vector3 lossyScaleRaw = scaleNeeded ? Math::LossyScale(worldMatrix, worldRotationMatrix) : Vector3(1.0f);
+			const Vector3 lossyScale = Vector3(std::abs(lossyScaleRaw.x), std::abs(lossyScaleRaw.y), std::abs(lossyScaleRaw.z));
+			
+			const Vector3 origin = originLocalSpace ? Vector3(worldMatrix * Vector4(inputOrigin, 1.0f)) : inputOrigin;
+			const Vector3 direction = Math::Normalize(directionLocalSpace ? Vector3(worldMatrix * Vector4(inputDirection, 0.0f)) : inputDirection);
+
+			const float maxDistance = scaleMaxDistance
+				? Math::Magnitude(
+					lossyScale * inputMaxDistance *
+					Math::Normalize(directionLocalSpace
+						? inputDirection
+						: Vector3(Math::Inverse(worldMatrix) * Vector4(inputDirection, 0.0f))))
+				: inputMaxDistance;
+
+			auto filterFn = [&](auto filter, auto data) -> Physics::PhysicsScene::QueryFilterFlag {
+				if (filter == nullptr)
+					return Physics::PhysicsScene::QueryFilterFlag::REPORT;
+				std::optional<bool> result = filter->GetInput(data);
+				if ((!result.has_value()) || result.value())
+					return Physics::PhysicsScene::QueryFilterFlag::DISCARD;
+				return Physics::PhysicsScene::QueryFilterFlag::REPORT;
+			};
+
+			Reference<ColliderFilterInput> colliderFilter = self->m_colliderFilter;
+			auto filterCollider = [&](Collider* collider) { return filterFn(colliderFilter, collider); };
+			const Function<Physics::PhysicsScene::QueryFilterFlag, Collider*> preFilterFn =
+				Function<Physics::PhysicsScene::QueryFilterFlag, Collider*>::FromCall(&filterCollider);
+			const Function<Physics::PhysicsScene::QueryFilterFlag, Collider*>* preFilter = (colliderFilter != nullptr) ? &preFilterFn : nullptr;
+
+			Reference<RayHitFilterInput> rayHitFilter = self->m_rayHitFilter;
+			auto filterRayHit = [&](const RaycastHit& hit) { return filterFn(rayHitFilter, hit); };
+			const Function<Physics::PhysicsScene::QueryFilterFlag, const RaycastHit&> postFilterFn =
+				Function<Physics::PhysicsScene::QueryFilterFlag, const RaycastHit&>::FromCall(&filterRayHit);
+			const Function<Physics::PhysicsScene::QueryFilterFlag, const RaycastHit&>* postFilter = (rayHitFilter != nullptr) ? &postFilterFn : nullptr;
+
+			static_assert(static_cast<uint64_t>(Flags::EXCLUDE_DYNAMIC_BODIES) == static_cast<uint64_t>(Physics::PhysicsScene::QueryFlag::EXCLUDE_DYNAMIC_BODIES));
+			static_assert(static_cast<uint64_t>(Flags::EXCLUDE_STATIC_BODIES) == static_cast<uint64_t>(Physics::PhysicsScene::QueryFlag::EXCLUDE_STATIC_BODIES));
+			const Physics::PhysicsScene::QueryFlags queryFlags = static_cast<Physics::PhysicsScene::QueryFlags>(
+				self->m_flags & (Flags::EXCLUDE_DYNAMIC_BODIES | Flags::EXCLUDE_STATIC_BODIES));
+
+			bool found = false;
+			auto onHitFound = [&](const RaycastHit& hit) {
+				assert(hit.collider != nullptr);
+				if (found)
+					self->Context()->Log()->Error("RaycastInput - Internal Error: More than one hit reported! [File", __FILE__, "; Line: ", __LINE__, "]");
+				found = true;
+				self->m_lastResult.collider = hit.collider;
+				self->m_lastResult.point = hit.point;
+				self->m_lastResult.normal = hit.normal;
+				self->m_lastResult.distance = hit.distance;
+			};
+
+			auto query = [&](auto queryFn) {
+				queryFn(direction, maxDistance,
+					Callback<const RaycastHit&>::FromCall(&onHitFound),
+					self->m_layerMask, queryFlags,
+					preFilter, postFilter);
+			};
+			auto sweep = [&](const auto& shape) {
+				Matrix4 poseMatrix = rotateSweepShape ? worldRotationMatrix : Math::Identity();
+				poseMatrix[3u] = Vector4(origin, 1.0f);
+				query([&](const auto&... args) { self->Context()->Physics()->Sweep(shape, poseMatrix, args...); });
+			};
+
+			switch (self->m_queryType) {
+			case QueryType::RAY:
+				query([&](auto... args) { self->Context()->Physics()->Raycast(origin, args...); });
+				break;
+			case QueryType::SPHERE:
+			{
+				const float rawRadius = self->QueryShapeRadius();
+				const float radius = scaleSweepShape
+					? (Math::Max(lossyScale.x, lossyScale.y, lossyScale.z) * rawRadius)
+					: rawRadius;
+				Physics::SphereShape shape(radius);
+				sweep(shape);
+				break;
+			}
+			case QueryType::CAPSULE:
+			{
+				const float rawRadius = self->QueryShapeRadius();
+				const float rawHeight = self->QueryCapsuleHeight();
+				const float scale = scaleSweepShape ? Math::Max(lossyScale.x, lossyScale.y, lossyScale.z) : 1.0f;
+				const float radius = rawRadius * scale;
+				const float height = rawHeight * scale;
+				Physics::CapsuleShape shape(radius, height);
+				sweep(shape);
+				break;
+			}
+			case QueryType::BOX:
+			{
+				const Vector3 rawBoxSize = self->QueryBoxSize();
+				const Vector3 boxSize = rawBoxSize * lossyScale;
+				Physics::BoxShape shape(boxSize);
+				sweep(shape);
+				break;
+			}
+			default:
+				break;
+			}
+		}
+	};
+
+
+	const Object* RaycastInput::QuertTypeEnumerationAttribute() {
+		static const auto attribute = Object::Instantiate<Serialization::EnumAttribute<std::underlying_type_t<QueryType>>>(false,
+			"RAY", QueryType::RAY,
+			"SPHERE", QueryType::SPHERE,
+			"CAPSULE", QueryType::CAPSULE,
+			"BOX", QueryType::BOX);
+		return attribute;
+	}
+
+	const Object* RaycastInput::FlagOptionsEnumerationAttribute() {
+		static const auto attribute = Object::Instantiate<Serialization::EnumAttribute<std::underlying_type_t<Flags>>>(true,
+			"QUERY_WHEN_DISABLED", Flags::QUERY_WHEN_DISABLED,
+			"EXCLUDE_DYNAMIC_BODIES", Flags::EXCLUDE_DYNAMIC_BODIES,
+			"EXCLUDE_STATIC_BODIES", Flags::EXCLUDE_STATIC_BODIES,
+			"QUERY_ORIGIN_WORLD_SPACE", Flags::QUERY_ORIGIN_WORLD_SPACE,
+			"QUERY_DIRECTION_WORLD_SPACE", Flags::QUERY_DIRECTION_WORLD_SPACE,
+			"SCALE_MAX_DISTANCE_BY_LOSSY_SCALE", Flags::SCALE_MAX_DISTANCE_BY_LOSSY_SCALE,
+			"SCALE_SWEEP_SHAPE_BY_LOSSY_SCALE", Flags::SCALE_SWEEP_SHAPE_BY_LOSSY_SCALE,
+			"DO_NOT_ROTATE_SWEEP_SHAPE", Flags::DO_NOT_ROTATE_SWEEP_SHAPE,
+			"DISABLE_FRAME_CACHING", Flags::DISABLE_FRAME_CACHING);
+		return attribute;
+	}
+
+
+	RaycastInput::RaycastInput(Component* parent, const std::string_view& name)
+		: Component(parent, name)
+		, m_lastUpdateFrame(parent->Context()->FrameIndex() - 1u) {}
+
+	std::optional<float> RaycastInput::EvaluateInput() {
+		std::optional<RaycastHit> res = EvaluateRaycastHitResult();
+		if (res == std::nullopt)
+			return std::nullopt;
+		else return res->distance;
+	}
+
+	std::optional<RaycastHit> RaycastInput_Base_RaycastHitInput::GetInput() {
+		RaycastInput* self = dynamic_cast<RaycastInput*>(this);
+		assert(self != nullptr);
+		return self->EvaluateRaycastHitResult();
+	}
+
+	std::optional<RaycastHit> RaycastInput::EvaluateRaycastHitResult() {
+		if (((m_flags & Flags::QUERY_WHEN_DISABLED) == Flags::NONE) && (!ActiveInHierarchy()))
+			return std::nullopt;
+		Helpers::Update(this);
+		Reference<Collider> collider = m_lastResult.collider;
+		if (collider == nullptr)
+			return std::nullopt;
+		RaycastHit hit = {};
+		hit.collider = collider;
+		hit.point = m_lastResult.point;
+		hit.normal = m_lastResult.normal;
+		hit.distance = m_lastResult.distance;
+		return hit;
+	}
+
+	void RaycastInput::GetFields(Callback<Jimara::Serialization::SerializedObject> recordElement) {
+		Jimara::Component::GetFields(recordElement);
+		JIMARA_SERIALIZE_FIELDS(this, recordElement) {
+			JIMARA_SERIALIZE_FIELD_GET_SET(QueryMode, SetQueryMode, "Query Mode", "Type of the raycast or a sweep", QuertTypeEnumerationAttribute());
+			switch (QueryMode()) {
+			case QueryType::RAY:
+				break;
+			case QueryType::SPHERE:
+				JIMARA_SERIALIZE_FIELD_GET_SET(QueryShapeRadius, SetQueryShapeRadius, "Sphere Radius", "Radius of the swept sphere");
+				break;
+			case QueryType::CAPSULE:
+				JIMARA_SERIALIZE_FIELD_GET_SET(QueryShapeRadius, SetQueryShapeRadius, "Capsule Radius", "Radius of the swept capsule");
+				JIMARA_SERIALIZE_FIELD_GET_SET(QueryCapsuleHeight, SetQueryCapsuleHeight, "Capsule Height", "Height of the swept capsule");
+				break;
+			case QueryType::BOX:
+				JIMARA_SERIALIZE_FIELD_GET_SET(QueryBoxSize, SetQueryBoxSize, "Box Size", "Size of the swept box");
+				break;
+			default:
+				break;
+			}
+			JIMARA_SERIALIZE_FIELD(m_layerMask, "Layer Mask", "Layer Mask for filtering colliders", Layers::LayerMaskAttribute::Instance());
+			JIMARA_SERIALIZE_WRAPPED_FIELD(m_maxDistance, Reference<InputProvider<float>>, "Max Distance", "Generic input provider for max raycast/sweep distance");
+			JIMARA_SERIALIZE_WRAPPED_FIELD(m_originInput, Reference<InputProvider<Vector3>>, "Origin Offset", "Input provider for raycast/sweep origin point offset");
+			JIMARA_SERIALIZE_WRAPPED_FIELD(m_directionInput, Reference<InputProvider<Vector3>>, "Direction input", 
+				"Input provider for raycast/sweep direction\n"
+				"If this is set to nullptr, 'forward' direction will be picked by default.");
+			JIMARA_SERIALIZE_WRAPPED_FIELD(m_colliderFilter, Reference<ColliderFilterInput>, "Collider Filter", 
+				"Filter-input for filtering which colliders to ignore\n"
+				"Input value will be used as keep/discard value in the raycast/sweep pre-filtering function.");
+			JIMARA_SERIALIZE_WRAPPED_FIELD(m_rayHitFilter, Reference<RayHitFilterInput>, "Ray-Hit Filter", 
+				"Filter-input for filtering which hit-events to ignore\n"
+				"Input value will be used as keep/discard value in the raycast/sweep post-filtering function.");
+			JIMARA_SERIALIZE_FIELD_GET_SET(QueryFlags, SetQueryFlags, "Query Flags", "Flags and options for the query");
+		};
+	}
+
+	template<> void TypeIdDetails::GetTypeAttributesOf<RaycastInput>(const Callback<const Object*>& report) {
+		static const Reference<ComponentFactory> factory = ComponentFactory::Create<RaycastInput>(
+			"Raycast Input", "Jimara/Input/Physical/RaycastInput",
+			"An input provider that performs a raycast/sweep and returns RaycastHit and/or hit-distance\n"
+			"Floating-point-type Vector-input evaluates hit distance (nullopt if there's no hit);\n"
+			"RaycastHit input provider interface evaluates the hit itself.");
+		report(factory);
+	}
+}
---
+++ __Source__/Jimara-GenericInputs/Physical/RaycastInput.h
@@ -0,0 +1,293 @@
+#pragma once
+#include "../Base/VectorInput.h"
+#include <Jimara/Components/Physics/Collider.h>
+
+
+namespace Jimara {
+	/// <summary> Let the system know about the type </summary>
+	JIMARA_REGISTER_TYPE(Jimara::RaycastInput);
+
+	/// <summary>
+	/// Base class of RaycastInput; Not intended to be used by end-user;
+	/// <para/> Intent of this one is to expose InputProvider<RaycastHit> interface through RaycastInput and nothing more..
+	/// </summary>
+	class JIMARA_GENERIC_INPUTS_API RaycastInput_Base_RaycastHitInput : public virtual InputProvider<RaycastHit> {
+	public:
+		/// <summary>
+		/// Evaluates RaycastHit input
+		/// </summary>
+		/// <returns> dynamic_cast&lt;RaycastInput&gt;(this)->EvaluateRaycastHitResult() </returns>
+		virtual std::optional<RaycastHit> GetInput()final override;
+
+	private:
+		// Constructor can only be accessed through RaycastInput
+		inline RaycastInput_Base_RaycastHitInput() {}
+		friend class RaycastInput;
+	};
+
+
+	/// <summary>
+	/// An input provider that performs a raycast/sweep and returns RaycastHit and/or hit-distance
+	/// <para/> Floating-point-type Vector-input evaluates hit distance (nullopt if there's no hit);
+	/// <para/> RaycastHit input provider interface evaluates the hit itself.
+	/// </summary>
+	class JIMARA_GENERIC_INPUTS_API RaycastInput
+		: public virtual VectorInput::ComponentFrom<float>
+		, public RaycastInput_Base_RaycastHitInput {
+	public:
+		/// <summary> Type of the raycast or a sweep </summary>
+		enum class QueryType : uint8_t {
+			/// <summary> Query will be a Raycast </summary>
+			RAY = 0,
+
+			/// <summary> Query will be a Sweep with a sphere as the shape </summary>
+			SPHERE = 1,
+
+			/// <summary> Query will be a Sweep with a capsule as the shape </summary>
+			CAPSULE = 2,
+
+			/// <summary> Query will be a Sweep with a box as the shape </summary>
+			BOX = 3,
+
+			/// <summary> Basically disables the query </summary>
+			NONE = 4
+		};
+
+		/// <summary> Enum-attribute for query-type </summary>
+		static const Object* QuertTypeEnumerationAttribute();
+
+		/// <summary> Flags and options for the query </summary>
+		enum class Flags : uint16_t {
+			/// <summary> Empty bitmask </summary>
+			NONE = 0u,
+
+			/// <summary> Will update input even when the RaycastInput component is disabled; by default, disabling the component will also disable the input </summary>
+			QUERY_WHEN_DISABLED = (1 << 0),
+
+			/// <summary> Excludes dynamic bodies from the query </summary>
+			EXCLUDE_DYNAMIC_BODIES = (1 << 1),
+
+			/// <summary> Excludes static bodies from the query </summary>
+			EXCLUDE_STATIC_BODIES = (1 << 2),
+
+			/// <summary> If set, this flag will cause the query to originate in the world-space instead of transform's local space </summary>
+			QUERY_ORIGIN_WORLD_SPACE = (1 << 3),
+
+			/// <summary> If set, this flag will cause the query direction will be in the world-space instead of transform's local space </summary>
+			QUERY_DIRECTION_WORLD_SPACE = (1 << 4),
+
+			/// <summary> If this flag is set, max distance will be scaled by lossy scale of the transform </summary>
+			SCALE_MAX_DISTANCE_BY_LOSSY_SCALE = (1 << 5),
+
+			/// <summary> If this flag is set, sweep shape will be scaled by lossy scale of the transform for SPHERE CAPSULE and BOX QueryType </summary>
+			SCALE_SWEEP_SHAPE_BY_LOSSY_SCALE = (1 << 6),
+
+			/// <summary> If set, sweep-shape will not be rotated with the transform for SPHERE CAPSULE and BOX QueryType </summary>
+			DO_NOT_ROTATE_SWEEP_SHAPE = (1 << 7),
+
+			/// <summary> 
+			/// If set, the input provider will perform a new raycast/sweep each time the input is queried 
+			/// instead of caching the 'current frame result' 
+			/// </summary>
+			DISABLE_FRAME_CACHING = (1 << 8)
+		};
+
+		/// <summary> Enum-attribute for Flags </summary>
+		static const Object* FlagOptionsEnumerationAttribute();
+
+
+		/// <summary> Filter input for filtering-out Colliders </summary>
+		using ColliderFilterInput = InputProvider<bool, Collider*>;
+
+		/// <summary> Filter input for filtering-out candidate RaycastHit results </summary>
+		using RayHitFilterInput = InputProvider<bool, const RaycastHit&>;
+
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="parent"> Parent component </param>
+		/// <param name="name"> Component name </param>
+		RaycastInput(Component* parent, const std::string_view& name = "RaycastInput");
+
+		/// <summary> Type of the raycast or a sweep </summary>
+		inline QueryType QueryMode()const { return m_queryType; }
+
+		/// <summary>
+		/// Sets ray/sweep type
+		/// </summary>
+		/// <param name="mode"> Type of the raycast or a sweep </param>
+		inline void SetQueryMode(const QueryType mode) { m_queryType = Math::Min(mode, QueryType::NONE); }
+
+		/// <summary> Radius for QueryType SPHERE and/or CAPSULE </summary>
+		inline float QueryShapeRadius()const { return m_queryShapeSize.x; }
+
+		/// <summary>
+		/// Sets radius for QueryType SPHERE and/or CAPSULE
+		/// </summary>
+		/// <param name="radius"> Shape radius </param>
+		inline void SetQueryShapeRadius(float radius) { m_queryShapeSize.x = std::abs(radius); }
+
+		/// <summary> Height for QueryType CAPSULE </summary>
+		inline float QueryCapsuleHeight()const { return m_queryShapeSize.y; }
+
+		/// <summary>
+		/// Sets height for QueryType CAPSULE
+		/// </summary>
+		/// <param name="height"> Shape height </param>
+		inline void SetQueryCapsuleHeight(float height) { m_queryShapeSize.y = std::abs(height); }
+
+		/// <summary> Size of QueryType BOX </summary>
+		inline Vector3 QueryBoxSize()const { return m_queryShapeSize; }
+
+		/// <summary>
+		/// Sets QueryType BOX size
+		/// </summary>
+		/// <param name="size"> Shape size </param>
+		inline void SetQueryBoxSize(const Vector3& size) { m_queryShapeSize = Vector3(std::abs(size.x), std::abs(size.y), std::abs(size.z)); }
+
+
+		/// <summary> Layer-mask for collider filtering </summary>
+		inline const Physics::PhysicsCollider::LayerMask& LayerMask()const { return m_layerMask; }
+
+		/// <summary>
+		/// Sets Layer-mask for collider filtering
+		/// </summary>
+		/// <param name="mask"> LayerMask </param>
+		inline void SetLayerMask(const Physics::PhysicsCollider::LayerMask& mask) { m_layerMask = mask; }
+
+		/// <summary> Generic input provider for max raycast/sweep distance </summary>
+		inline Reference<InputProvider<float>> MaxDistanceInput()const { return m_maxDistance; }
+
+		/// <summary>
+		/// Sets input source for the maximal raycast/sweep distance
+		/// </summary>
+		/// <param name="input"> Input provider </param>
+		inline void SetMaxDistanceInput(InputProvider<float>* input) { m_maxDistance = input; }
+
+		/// <summary> Input provider for raycast/sweep origin point offset </summary>
+		Reference<InputProvider<Vector3>> OriginOffsetInput()const { return m_originInput; }
+
+		/// <summary>
+		/// Sets input provider for raycast/sweep origin point offset
+		/// </summary>
+		/// <param name="input"> Input provider </param>
+		void SetOriginOffsetInput(InputProvider<Vector3>* input) { m_originInput = input; }
+
+		/// <summary> 
+		/// Input provider for raycast/sweep direction 
+		/// <para/> If this is set to nullptr, 'forward' direction will be picked by default.
+		/// </summary>
+		Reference<InputProvider<Vector3>> DirectionInput()const { return m_directionInput; }
+
+		/// <summary>
+		/// Sets input provider for raycast/sweep direction
+		/// </summary>
+		/// <param name="input"> Input provider </param>
+		void SetDirectionInput(InputProvider<Vector3>* input) { m_directionInput = input; }
+
+		/// <summary>
+		/// Filter-input for filtering which colliders to ignore
+		/// <para/> Input value will be used as keep/discard value in the raycast/sweep pre-filtering function
+		/// </summary>
+		Reference<ColliderFilterInput> ColliderFilter()const { return m_colliderFilter; }
+
+		/// <summary>
+		/// Sets filter-input for filtering which colliders to ignore
+		/// </summary>
+		/// <param name="input"> Input provider </param>
+		void SetColliderFilterInput(ColliderFilterInput* input) { m_colliderFilter = input; }
+
+		/// <summary>
+		/// Filter-input for filtering which hit-events to ignore
+		/// <para/> Input value will be used as keep/discard value in the raycast/sweep post-filtering function
+		/// </summary>
+		Reference<RayHitFilterInput> RayHitFilter()const { return m_rayHitFilter; }
+
+		/// <summary>
+		/// Sets filter-input for filtering which hit-events to ignore
+		/// </summary>
+		/// <param name="input"> Input provider </param>
+		void SetRayHitFilter(RayHitFilterInput* input) { m_rayHitFilter = input; }
+
+		/// <summary> Flags and options for the query </summary>
+		inline Flags QueryFlags()const { return m_flags; }
+
+		/// <summary>
+		/// Sets query flags
+		/// </summary>
+		/// <param name="flags"> Flags and options for the query </param>
+		inline void SetQueryFlags(const Flags flags) { m_flags = flags; }
+
+
+		/// <summary> Evaluates input </summary>
+		virtual std::optional<float> EvaluateInput()override;
+
+		/// <summary> Evaluates RaycastHit input </summary>
+		std::optional<RaycastHit> EvaluateRaycastHitResult();
+
+		/// <summary>
+		/// Exposes fields
+		/// </summary>
+		/// <param name="recordElement"> Fields will be exposed through this callback </param>
+		virtual void GetFields(Callback<Jimara::Serialization::SerializedObject> recordElement)override;
+
+
+		/// <summary>
+		/// [Only intended to be used by WeakReference<>; not safe for general use] Fills WeakReferenceHolder with a StrongReferenceProvider, 
+		/// that will return this WeaklyReferenceable back upon request (as long as it still exists)
+		/// <para/> Note that this is not thread-safe!
+		/// </summary>
+		/// <param name="holder"> Reference to a reference of a StrongReferenceProvider </param>
+		inline virtual void FillWeakReferenceHolder(WeaklyReferenceable::WeakReferenceHolder& holder)override {
+			Component::FillWeakReferenceHolder(holder);
+		}
+
+		/// <summary>
+		/// [Only intended to be used by WeakReference<>; not safe for general use] Should clear the link to the StrongReferenceProvider;
+		/// <para/> Address of the holder has to be the same as the one, previously passed to FillWeakReferenceHolder() method
+		/// <para/> Note that this is not thread-safe!
+		/// </summary>
+		/// <param name="holder"> Reference to a reference of a StrongReferenceProvider </param>
+		inline virtual void ClearWeakReferenceHolder(WeaklyReferenceable::WeakReferenceHolder& holder)override {
+			Component::ClearWeakReferenceHolder(holder);
+		}
+
+
+	private:
+		QueryType m_queryType = QueryType::RAY;
+		Physics::PhysicsCollider::LayerMask m_layerMask = Physics::PhysicsCollider::LayerMask::All();
+		WeakReference<InputProvider<float>> m_maxDistance;
+		WeakReference<InputProvider<Vector3>> m_originInput;
+		WeakReference<InputProvider<Vector3>> m_directionInput;
+		WeakReference<ColliderFilterInput> m_colliderFilter;
+		WeakReference<RayHitFilterInput> m_rayHitFilter;
+		Flags m_flags = Flags::NONE;
+
+		Vector3 m_queryShapeSize = Vector3(1.0f);
+
+		uint64_t m_lastUpdateFrame;
+		struct JIMARA_GENERIC_INPUTS_API RaycastInformation {
+			WeakReference<Collider> collider;
+			Vector3 point = Vector3(0.0f, 0.0f, 0.0f);
+			Vector3 normal = Vector3(0.0f, 0.0f, 0.0f);
+			float distance = 0.0f;
+		} m_lastResult;
+
+		struct Helpers;
+	};
+
+	// Define boolean operators for RaycastInput::Flags
+	JIMARA_DEFINE_ENUMERATION_BOOLEAN_OPERATIONS(::Jimara::RaycastInput::Flags);
+
+
+	// Expose type details:
+	template<> inline void TypeIdDetails::GetParentTypesOf<RaycastInput_Base_RaycastHitInput>(const Callback<TypeId>& report) {
+		report(TypeId::Of<InputProvider<RaycastHit>>());
+	}
+	template<> inline void TypeIdDetails::GetParentTypesOf<RaycastInput>(const Callback<TypeId>& report) {
+		report(TypeId::Of<VectorInput::ComponentFrom<float>>());
+		report(TypeId::Of<RaycastInput_Base_RaycastHitInput>());
+	}
+	template<> JIMARA_GENERIC_INPUTS_API void TypeIdDetails::GetTypeAttributesOf<RaycastInput>(const Callback<const Object*>& report);
+}
