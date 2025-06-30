7250c13e6d577ee6ee170cbf66484b277fe29dd3
TheDonsky <nikadon.net@gmail.com>
2025-06-30 12:21:17 +0400

    Add a standalone runner that performs all the necessary boilerplate to run the game without editor.

---
+++ __Source__/Jimara/Environment/StandaloneRunner.cpp
@@ -0,0 +1,254 @@
+#include "StandaloneRunner.h"
+#include "../Core/TypeRegistration/TypeRegistration.h"
+#include "../OS/Logging/StreamLogger.h"
+#include "../OS/System/DynamicLibrary.h"
+#include "../Environment/Scene/Scene.h"
+#include "../Data/AssetDatabase/FileSystemDatabase/FileSystemDatabase.h"
+#include "../Data/ComponentHierarchySpowner.h"
+#include "../Environment/Rendering/RenderStack.h"
+#include "../Core/Stopwatch.h"
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+
+namespace Jimara {
+	StandaloneRunner::Error StandaloneRunner::RunGame(const Args& args) {
+		class Logger : public virtual OS::StreamLogger {
+		private:
+			const Flags m_flags;
+
+		public:
+			inline Logger(Flags flags) : m_flags(flags) {}
+
+			inline virtual ~Logger() {};
+
+		protected:
+			inline virtual void Log(const LogInfo& info) override {
+#ifdef _WIN32
+				if (((m_flags & Flags::SHOW_CONSOLE_ON_LOG_INFOS) != Flags::NONE && info.level == OS::Logger::LogLevel::LOG_INFO) ||
+					((m_flags & Flags::SHOW_CONSOLE_ON_LOG_WARNINGS) != Flags::NONE && info.level == OS::Logger::LogLevel::LOG_WARNING) ||
+					((m_flags & Flags::SHOW_CONSOLE_ON_LOG_ERRORS) != Flags::NONE && info.level >= OS::Logger::LogLevel::LOG_ERROR)) {
+					HWND wnd = ::GetConsoleWindow();
+					if (wnd != static_cast<HWND>(0u))
+						::ShowWindow(wnd, SW_SHOW);
+				}
+#endif
+				OS::StreamLogger::Log(info);
+			}
+		};
+
+#ifdef _WIN32
+		{
+			HWND wnd = ::GetConsoleWindow();
+			if (wnd == static_cast<HWND>(0u)) {
+				AllocConsole();
+				::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+				FILE* fp;
+				freopen_s(&fp, "CONOUT$", "w", stdout);
+			}
+		}
+#endif
+
+		const Reference<OS::Logger> logger = Object::Instantiate<Logger>(args.flags);
+		auto error = [&](Error code, const auto&... message) -> Error {
+#ifdef _WIN32
+			HWND wnd = ::GetConsoleWindow();
+			if (wnd != static_cast<HWND>(0u))
+				::ShowWindow(wnd, SW_SHOW);
+#endif
+			logger->Error(message...);
+			return code;
+			};
+
+		const Reference<BuiltInTypeRegistrator> builtInTypes = BuiltInTypeRegistrator::Instance();
+
+		std::vector<Reference<OS::DynamicLibrary>> gameCode;
+		OS::Path::IterateDirectory(args.gameDLLPath, [&](const OS::Path& path) {
+			std::string extension = path.extension().string();
+			for (size_t i = 0u; i < extension.length(); i++)
+				extension[i] = std::toupper(extension[i]);
+			if (extension != ".DLL")
+				return true;
+			const Reference<OS::DynamicLibrary> library = OS::DynamicLibrary::Load(path, nullptr);
+			if (library != nullptr)
+				gameCode.push_back(library);
+			return true;
+			});
+
+		const Reference<const Application::AppInformation> appInfo = Object::Instantiate<Application::AppInformation>();
+
+		const Reference<Graphics::GraphicsInstance> graphicsAPI = Graphics::GraphicsInstance::Create(logger, appInfo);
+		if (graphicsAPI == nullptr)
+			return error(Error::GRAPHICS_INSTANCE_CREATION_FAILED, "Graphics instance could not be created!");
+
+		const Reference<OS::Window> window = Jimara::OS::Window::Create(logger, args.windowName);
+		if (window == nullptr)
+			return error(Error::WINDOW_CREATION_FAILED, "Window could not be created!");
+		else window->SetFullscreen((args.flags & Flags::FULLSCREEN_ON_START) != Flags::NONE);
+
+		const Reference<Graphics::RenderSurface> surface = graphicsAPI->CreateRenderSurface(window);
+		if (surface == nullptr)
+			return error(Error::RENDER_SURFACE_CREATION_FAILED, "Render surface could not be created!");
+
+		Graphics::PhysicalDevice* const physicalGPU = surface->PrefferedDevice();
+		if (physicalGPU == nullptr)
+			return error(Error::COMPATIBLE_GRAPOHICS_DEVICE_NOT_FOUND, "Compatible device not found!");
+
+		const Reference<Graphics::GraphicsDevice> graphicsDevice = physicalGPU->CreateLogicalDevice();
+		if (graphicsDevice == nullptr)
+			return error(Error::GRAPHICS_DEVICE_CREATION_FAILED, "Graphics device could not be created!");
+
+		const Reference<Graphics::BindlessSet<Graphics::ArrayBuffer>> bindlessBuffers = graphicsDevice->CreateArrayBufferBindlessSet();
+		if (bindlessBuffers == nullptr)
+			return error(Error::BINDLESS_BUFFER_SET_CREATION_FAILED, "Bindless buffer set could not be created!");
+
+		const Reference<Graphics::BindlessSet<Graphics::TextureSampler>> bindlessSamplers = graphicsDevice->CreateTextureSamplerBindlessSet();
+		if (bindlessSamplers == nullptr)
+			return error(Error::BINDLESS_SAMPLER_SET_CREATION_FAILED, "Bindless texture sampler set could not be created!");
+
+		const Reference<Graphics::RenderEngine> surfaceRenderEngine = graphicsDevice->CreateRenderEngine(surface);
+		if (surfaceRenderEngine == nullptr)
+			return error(Error::RENDER_SURFACE_ENGINE_CREATION_FAILED, "Surface render engine could not be created!");
+
+		const Reference<ShaderLibrary> shaderLibrary = FileSystemShaderLibrary::Create(args.shaderPath, logger);
+		if (shaderLibrary == nullptr)
+			return error(Error::SHADER_LIBRARY_CREATION_FAILED, "Shader library could not be created!");
+
+		const Reference<Physics::PhysicsInstance> physicsAPI = Physics::PhysicsInstance::Create(logger);
+		if (physicsAPI == nullptr)
+			return error(Error::PHYSICS_API_CREATION_FAILED, "Physics API could not be created!");
+
+		const Reference<Audio::AudioInstance> audioAPI = Audio::AudioInstance::Create(logger);
+		if (audioAPI == nullptr)
+			return error(Error::AUDIO_API_CREATION_FAILED, "Audio API could not be created!");
+
+		const Reference<Audio::PhysicalAudioDevice> physicalAudioDevice = audioAPI->DefaultDevice();
+		if (physicalAudioDevice == nullptr)
+			return error(Error::DEFAULT_AUDIO_DEVICE_NOT_FOUND, "Default audio device could not found!");
+
+		const Reference<Audio::AudioDevice> audioDevice = physicalAudioDevice->CreateLogicalDevice();
+		if (audioDevice == nullptr)
+			return error(Error::AUDIO_DEVICE_CREATION_FAILED, "Audio device could not be created!");
+
+		FileSystemDatabase::CreateArgs databaseCreateArgs = {};
+		{
+			databaseCreateArgs.logger = logger;
+			databaseCreateArgs.graphicsDevice = graphicsDevice;
+			databaseCreateArgs.bindlessBuffers = bindlessBuffers;
+			databaseCreateArgs.bindlessSamplers = bindlessSamplers;
+			databaseCreateArgs.shaderLibrary = shaderLibrary;
+			databaseCreateArgs.physicsInstance = physicsAPI;
+			databaseCreateArgs.audioDevice = audioDevice;
+			databaseCreateArgs.assetDirectory = OS::Path(args.assetDirectory);
+			databaseCreateArgs.previousImportDataCache = OS::Path("JimaraDatabaseCache.json");
+		}
+		const Reference<FileSystemDatabase> assetDatabase = FileSystemDatabase::Create(databaseCreateArgs);
+		if (assetDatabase == nullptr)
+			return error(Error::ASSET_DATABASE_CREATION_FAILED, "Asset database could not be created!");
+
+		const Reference<OS::Input> inputModule = window->CreateInputModule();
+		if (inputModule == nullptr)
+			return error(Error::INPUT_MODULE_CREATION_FAILED, "Input module could not be created!");
+		else if ((args.flags & Flags::LOCK_MOUSE_INSIDE_WINDOW_WHEN_FOCUSED) != Flags::NONE)
+			inputModule->SetCursorLockMode(OS::Input::CursorLock::LOCK_INSIDE);
+
+		Scene::CreateArgs sceneCreateArgs = {};
+		{
+			sceneCreateArgs.logic.logger = logger;
+			sceneCreateArgs.logic.input = inputModule;
+			sceneCreateArgs.logic.assetDatabase = assetDatabase;
+			sceneCreateArgs.graphics.graphicsDevice = graphicsDevice;
+			sceneCreateArgs.graphics.shaderLibrary = shaderLibrary;
+			sceneCreateArgs.graphics.bindlessResources.bindlessArrays = bindlessBuffers;
+			sceneCreateArgs.graphics.bindlessResources.bindlessSamplers = bindlessSamplers;
+			sceneCreateArgs.physics.physicsInstance = physicsAPI;
+			sceneCreateArgs.audio.audioDevice = audioDevice;
+		}
+		const Reference<Scene> scene = Scene::Create(sceneCreateArgs);
+		if (scene == nullptr)
+			return error(Error::SCENE_CREATION_FAILED, "Scene could not be created!");
+
+		{
+			Reference<Asset> mainSceneAsset;
+			assetDatabase->GetAssetsFromFile<ComponentHierarchySpowner>(
+				args.assetDirectory / args.mainScenePath,
+				[&](const FileSystemDatabase::AssetInformation& assetInfo) {
+					mainSceneAsset = assetInfo.AssetRecord();
+				});
+			if (mainSceneAsset == nullptr)
+				return error(Error::MAIN_SCENE_ASSET_NOT_FOUND, "Main scene could not be found!");
+			const Reference<ComponentHierarchySpowner> mainScene = mainSceneAsset->LoadAs<ComponentHierarchySpowner>();
+			if (mainScene == nullptr)
+				return error(Error::MAIN_SCENE_COULD_LOAD_FAILED, "Main scene could not be loaded!");
+			mainScene->SpownHierarchy(scene->RootObject());
+		}
+
+		struct ImageRenderer : public virtual Graphics::ImageRenderer {
+			const Reference<RenderStack> renderStack;
+
+			inline ImageRenderer(SceneContext* context) : renderStack(RenderStack::Main(context)) {}
+
+			virtual Reference<Object> CreateEngineData(Graphics::RenderEngineInfo* engineInfo) override { return engineInfo; }
+
+			virtual void Render(Object* engineData, Graphics::InFlightBufferInfo bufferInfo) override {
+				Graphics::RenderEngineInfo* engineInfo = dynamic_cast<Graphics::RenderEngineInfo*>(engineData);
+				const Reference<RenderImages> images = (renderStack != nullptr) ? renderStack->Images() : nullptr;
+				const RenderImages::Image* const image = (images != nullptr) ? images->GetImage(RenderImages::MainColor()) : nullptr;
+				if (image != nullptr && image->Resolve() != nullptr)
+					engineInfo->Image(bufferInfo)->Blit(bufferInfo, image->Resolve()->TargetTexture());
+				renderStack->SetResolution(engineInfo->ImageSize());
+			}
+		};
+		const Reference<ImageRenderer> renderer = Object::Instantiate<ImageRenderer>(scene->Context());
+		surfaceRenderEngine->AddRenderer(renderer);
+
+
+		Stopwatch frameTimer;
+		float smoothFrameTime = 0.0f;
+		auto updateScene = [&](auto*) {
+			// Frame time:
+			const float frameTime = frameTimer.Reset();
+			smoothFrameTime = Math::Lerp(smoothFrameTime, frameTime, 0.01f);
+
+			// Update:
+			scene->Update(frameTime);
+			surfaceRenderEngine->Update();
+
+			// Name:
+			if ((args.flags & (Flags::SHOW_SMOOTH_FRAMERATE_IN_WINDOW_NAME | Flags::SHOW_SMOOTH_FRAMETIME_IN_WINDOW_NAME)) != Flags::NONE) {
+				std::stringstream name_stream;
+				name_stream << "Game ";
+				std::string startSym = "[";
+				if ((args.flags & Flags::SHOW_SMOOTH_FRAMERATE_IN_WINDOW_NAME) != Flags::NONE) {
+					name_stream << startSym << std::setprecision(4u) << (1.0f / smoothFrameTime) << " fps";
+					startSym = "; ";
+				}
+				if ((args.flags & Flags::SHOW_SMOOTH_FRAMETIME_IN_WINDOW_NAME) != Flags::NONE) {
+					name_stream << startSym << std::setprecision(4u) << (smoothFrameTime * 1000.0f) << " ms";
+					startSym = "; ";
+				}
+				if (startSym != "[")
+					name_stream << "]";
+				window->SetName(name_stream.str());
+			}
+
+			// Fullscreen:
+			if (((args.flags & Flags::TOGGLE_FULLSCREEN_ON_F11) != Flags::NONE) && inputModule->KeyDown(OS::Input::KeyCode::F11))
+				window->SetFullscreen(!window->IsFullscreen());
+
+#ifdef _WIN32
+			// Console:
+			if (((args.flags & Flags::TOGGLE_CONSOLE_ON_F10) != Flags::NONE) && inputModule->KeyDown(OS::Input::KeyCode::F10)) {
+				HWND wnd = ::GetConsoleWindow();
+				if (wnd != static_cast<HWND>(0u))
+					::ShowWindow(wnd, ::IsWindowVisible(wnd) ? SW_HIDE : SW_SHOW);
+			}
+#endif
+			};
+		window->OnUpdate() += Callback<OS::Window*>::FromCall(&updateScene);
+
+		window->WaitTillClosed();
+		return Error::NONE;
+	}
+}
---
+++ __Source__/Jimara/Environment/StandaloneRunner.h
@@ -0,0 +1,165 @@
+#pragma once
+#include "../Core/JimaraApi.h"
+#include "../OS/IO/Path.h"
+#include "../Core/EnumClassBooleanOperands.h"
+
+
+namespace Jimara {
+	/// <summary> Standalone runner, containing the necessary boilerplate to run the game without editor </summary>
+	class JIMARA_API StandaloneRunner {
+	public:
+		/// <summary> Runner flags </summary>
+		enum class JIMARA_API Flags : uint32_t {
+			/// <summary> Empty bitmask </summary>
+			NONE = 0u,
+
+			/// <summary> If set, this flag forces console window creation if it does not exist </summary>
+			CREATE_CONSOLE_IF_NOT_PRESENT = (1 << 0),
+
+			/// <summary> If set, this flag makes console visible if any initialization error occures </summary>
+			SHOW_CONSOLE_ON_INITIALIZATION_ERRORS = (1 << 1),
+
+			/// <summary> If set, this flag makes console visible if anything gets logged with LogLevel::LOG_INFO </summary>
+			SHOW_CONSOLE_ON_LOG_INFOS = (1 << 2),
+
+			/// <summary> If set, this flag makes console visible if anything gets logged with LogLevel::LOG_WARNING </summary>
+			SHOW_CONSOLE_ON_LOG_WARNINGS = (1 << 3),
+
+			/// <summary> If set, this flag makes console visible if anything gets logged with LogLevel::LOG_ERROR or LogLevel::LOG_FATAL </summary>
+			SHOW_CONSOLE_ON_LOG_ERRORS = (1 << 4),
+
+			/// <summary> If set, this flag will enable toggling console window on and off by clicking F10 button </summary>
+			TOGGLE_CONSOLE_ON_F10 = (1 << 5),
+
+			/// <summary> If set, this flag will cause the game window to start in full-screen mode </summary>
+			FULLSCREEN_ON_START = (1 << 8),
+
+			/// <summary> If set, this flag will enable toggling window full-screen mode on and off by clicking F10 button </summary>
+			TOGGLE_FULLSCREEN_ON_F11 = (1 << 9),
+
+			/// <summary> If set, this flag will lock cursor inside window right out of the gate (Input::SetCursorLockMode(OS::Input::CursorLock::LOCK_INSIDE)) </summary>
+			LOCK_MOUSE_INSIDE_WINDOW_WHEN_FOCUSED = (1 << 16),
+
+			/// <summary> If set, this flag will cause smooth framerate to be shown as a part of the window title </summary>
+			SHOW_SMOOTH_FRAMERATE_IN_WINDOW_NAME = (1 << 24),
+
+			/// <summary> If set, this flag will cause smooth frame-time to be shown as a part of the window title </summary>
+			SHOW_SMOOTH_FRAMETIME_IN_WINDOW_NAME = (1 << 25),
+
+
+			/// <summary> Default flags </summary>
+			DEFAULT = (
+				CREATE_CONSOLE_IF_NOT_PRESENT |
+				SHOW_CONSOLE_ON_INITIALIZATION_ERRORS |
+				TOGGLE_CONSOLE_ON_F10 |
+				FULLSCREEN_ON_START |
+				TOGGLE_FULLSCREEN_ON_F11 |
+				LOCK_MOUSE_INSIDE_WINDOW_WHEN_FOCUSED |
+				SHOW_SMOOTH_FRAMERATE_IN_WINDOW_NAME |
+				SHOW_SMOOTH_FRAMETIME_IN_WINDOW_NAME)
+		};
+
+
+		/// <summary> Arguments for running the game </summary>
+		struct JIMARA_API Args {
+			/// <summary> Name of the window </summary>
+			std::string windowName = "Game";
+
+			/// <summary> Path to the compiled SPIR-V directory (shader output path) </summary>
+			OS::Path shaderPath = OS::Path("Game/Shaders/");
+
+			/// <summary> Path to the game dll files </summary>
+			OS::Path gameDLLPath = OS::Path("Game/");
+
+			/// <summary> Path to the asset directory </summary>
+			OS::Path assetDirectory = OS::Path("Assets/");
+
+			/// <summary> Path to the main scene to load (relative to the asset directory) </summary>
+			OS::Path mainScenePath = OS::Path("Scenes/Main.jimara");
+
+			/// <summary> Runner flags </summary>
+			Flags flags = Flags::DEFAULT;
+		};
+
+
+		/// <summary> Error codes </summary>
+		enum class JIMARA_API Error : int {
+			/// <summary> No error </summary>
+			NONE = 0,
+
+			/// <summary> Graphics::GraphicsInstance::Create() == nullptr </summary>
+			GRAPHICS_INSTANCE_CREATION_FAILED = 1,
+
+			/// <summary> Returned if no display-capable graphics devie was found </summary>
+			COMPATIBLE_GRAPOHICS_DEVICE_NOT_FOUND = 2,
+
+			/// <summary> Graphics::PhysicalDevice::CreateLogicalDevice() == nullptr </summary>
+			GRAPHICS_DEVICE_CREATION_FAILED = 3,
+
+			/// <summary> OS::Window::Create() == nullptr </summary>
+			WINDOW_CREATION_FAILED = 4,
+
+			/// <summary> Graphics::GraphicsInstance::CreateRenderSurface() == nullptr </summary>
+			RENDER_SURFACE_CREATION_FAILED = 5,
+
+			/// <summary> Graphics::GraphicsDevice::CreateRenderEngine() == nullptr </summary>
+			RENDER_SURFACE_ENGINE_CREATION_FAILED = 6,
+
+			/// <summary> FileSystemShaderLibrary::Create() == nullptr </summary>
+			SHADER_LIBRARY_CREATION_FAILED = 7,
+
+			/// <summary> Graphics::GraphicsDevice::CreateArrayBufferBindlessSet() == nullptr </summary>
+			BINDLESS_BUFFER_SET_CREATION_FAILED = 8,
+
+			/// <summary> Graphics::GraphicsDevice::CreateTextureSamplerBindlessSet() == nullptr </summary>
+			BINDLESS_SAMPLER_SET_CREATION_FAILED = 9,
+
+			/// <summary> Physics::PhysicsInstance::Create() == nullptr </summary>
+			PHYSICS_API_CREATION_FAILED = 10,
+
+			/// <summary> Audio::AudioInstance::Create() == nullptr </summary>
+			AUDIO_API_CREATION_FAILED = 11,
+
+			/// <summary> Audio::AudioInstance::DefaultDevice() == nullptr </summary>
+			DEFAULT_AUDIO_DEVICE_NOT_FOUND = 12,
+
+			/// <summary> Audio::PhysicalAudioDevice::CreateLogicalDevice() == nullptr  </summary>
+			AUDIO_DEVICE_CREATION_FAILED = 13,
+
+			/// <summary> FileSystemDatabase::Create() == nullptr </summary>
+			ASSET_DATABASE_CREATION_FAILED = 14,
+
+			/// <summary> OS::Window::CreateInputModule() == nullptr </summary>
+			INPUT_MODULE_CREATION_FAILED = 15,
+
+			/// <summary> Scene::Create() == nullptr </summary>
+			SCENE_CREATION_FAILED = 16,
+
+			/// <summary> Main scene (Args::mainScenePath) not found </summary>
+			MAIN_SCENE_ASSET_NOT_FOUND = 17,
+
+			/// <summary> Main scene (Args::mainScenePath) resource could not be loaded </summary>
+			MAIN_SCENE_COULD_LOAD_FAILED = 18,
+
+			/// <summary> Unknown error </summary>
+			UNKNOWN = ~0
+		};
+
+
+		/// <summary>
+		/// Performs necessary boilerplate to create context and runs the game
+		/// <para/> This function retunrs once the window gets closed.
+		/// </summary>
+		/// <param name="args"> Arguments for running the game </param>
+		/// <returns> Error code if something failed, Error::NONE otherwise. </returns>
+		static Error RunGame(const Args& args);
+
+
+	private:
+		// Standalone runner is not a class that we can create an object of..
+		inline StandaloneRunner() {}
+	};
+
+	// Define boolean operands for the flags:
+	JIMARA_DEFINE_ENUMERATION_BOOLEAN_OPERATIONS(StandaloneRunner::Flags);
+}
